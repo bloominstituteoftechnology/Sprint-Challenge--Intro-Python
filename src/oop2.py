56d076773587756a58dc0d5c21b66ffad59c266f
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2018-07-27 10:42:19 -0500

    Made progress on oop2.py

--- src/oop2.py
+++ src/oop2.py
@@ -2,11 +2,11 @@
 #
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
-
+v = vehicles
 class GroundVehicle():
     def __init__(self, num_wheels):
         self.num_wheels = num_wheels
-
+num_wheels = 4
     # TODO
 
 
@@ -16,8 +16,20 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it prints "BRAAAP!!"
+class Motorcycle(Vehicle):
+    def __init__(self):
+    super()__init__("num_wheels")
+
+    def call(self):
+        print("BRAAAP!!")
+    
+    def num_wheels(self)
+        print(2)
 
 # TODO
+v = vehicles()
+v.drive()
+v.sound() #generic sound
 
 vehicles = [
     GroundVehicle(),
@@ -30,3 +42,5 @@ vehicles = [
 # Go through the vehicles list and call drive() on each.
 
 # TODO
+for v in vehicles:
+    v.drive()
