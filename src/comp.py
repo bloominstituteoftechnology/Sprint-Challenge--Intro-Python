7aad74c2fa51a72fc8c4b64e357e8a4b2e905a1a
Henry K <mykim1891@hotmail.com>
2018-07-27 09:27:11 -0700

    list comprehension halfway done

--- src/comp.py
+++ src/comp.py
@@ -23,32 +23,32 @@ humans = [
 # whose name starts with 'D':
 
 print("Starts with D:")
-r = []  # TODO
+r = [x.name for x in humans if x.name[0] == "D"]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 
 print("Ends with e:")
-r = []  # TODO
+r = [x.name for x in humans if x.name[len(x.name) - 1] == "e"]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 
 print("Starts between C and G, inclusive:")
-r = []  # TODO
+r = [any(x.name[0] == letters for letters in "cdefg") for x in humans]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-r = []  # TODO
+r = [x.name for x in humans if x.age >= 10]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-r = []  # TODO
+r = [x.name + '-' + str(x.age) for x in humans ]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of tuples containing name and
