1d9e17413c1ed1625b38b190e6013711718926b9
Nedu Robert <nedumrobert@gmail.com>
2018-07-27 17:26:02 +0100

    Implemented list comprehension for all ages plus 10

--- src/comp.py
+++ src/comp.py
@@ -42,7 +42,7 @@ print(r)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-r = []  # TODO
+r = [human.age + 10 for human in humans]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of strings which are the name
