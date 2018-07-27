a741756c12fd757a30b127527387c53d4ec1b458
jeffrey <jeffreyneilflynn@gmail.com>
2018-07-27 15:23:33 -0400

    stretch

--- src/cityreader.py
+++ src/cityreader.py
@@ -1,4 +1,6 @@
 import csv
+import re
+# from string import split
 
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
@@ -32,7 +34,7 @@ with open("cities.csv") as csvfile:
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 
-[print(x.name, x.latitude, x.longitude) for x in cities]
+# [print(x.name, x.latitude, x.longitude) for x in cities]
 
 # *** STRETCH GOAL! ***
 #
@@ -58,4 +60,29 @@ with open("cities.csv") as csvfile:
 # Tucson: (32.1558,-110.8777)
 # Salt Lake City: (40.7774,-111.9301)
 
-# TODO
+print("\nSeperate lat/long numbers with [, ] (comma + space)")
+
+user_input_1 = input("\n\tEnter first set of latitude/longitude coordinates (lat, lng): ")
+user_input_2 = input("\n\tEnter second set of latitude/longitude coordinates (lat, lng): ")
+
+
+parse_input_1 = user_input_1.split(", ")
+parse_input_2 = user_input_2.split(", ")
+
+
+lats = [parse_input_1[0], parse_input_2[0]]
+longs = [parse_input_1[1], parse_input_2[1]]
+
+
+def validateData(city):
+  if city.latitude > max(lats) or city.latitude < min(lats): None
+  
+  elif city.longitude > max(longs) or city.longitude < min(longs): None
+
+  else: print(city.name, city.latitude, city.longitude)
+
+
+print() # for terminal aesthetics
+
+
+[validateData(city) for city in cities]
