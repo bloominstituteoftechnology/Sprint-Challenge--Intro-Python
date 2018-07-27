82c928772f34e61c31054bc6a845cf9702a46d43
ajf412 <aaronjfaulkner@yahoo.com>
2018-07-27 14:03:04 -0400

    Aaron Faulkner - CS9:  MVP complete.  Working on stretch goals.

--- src/oop2.py
+++ src/oop2.py
@@ -3,10 +3,11 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
-class GroundVehicle():
-    def __init__(self, num_wheels):
+class GroundVehicle:
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
-
+    def drive(self):
+        print("vroooom")
     # TODO
 
 
@@ -16,6 +17,12 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it prints "BRAAAP!!"
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(2)
+    def drive(self):
+        print("BRAAAP!!")
 
 # TODO
 
@@ -28,5 +35,7 @@ vehicles = [
 ]
 
 # Go through the vehicles list and call drive() on each.
+for v in vehicles:
+    v.drive()
 
 # TODO
