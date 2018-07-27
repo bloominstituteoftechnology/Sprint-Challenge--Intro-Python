6631b889cf755fade91c9f3a716684b26578a4d3
JustinSHong <jhong2009@gmail.com>
2018-07-27 08:31:28 -0700

    comp.py: added comp that creates a list of strings that are name-age

--- src/comp.py
+++ src/comp.py
@@ -48,7 +48,7 @@ print(r)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-r = []  # TODO
+r = ["{0}-{1}".format(h.name, h.age) for h in humans]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of tuples containing name and
