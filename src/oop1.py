77cec0a1e1db0b2b116cd6e1219935f833746fec
totsuya <supertatortots@gmail.com>
2018-07-27 12:54:08 -0400

    oop1 done

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,34 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+  def __init__:(self, name, description)
+    self.name = name 
+    self.description = description
+  #base class for FlightVehicle and Starship
+
+class FlightVehicle(Vehicle):
+  pass
+  #takes in from Vehicle
+
+class Starship(Vehicle):
+  pass
+  #takes in from Vehicle
+
+class GroundVehicle(Vehicle):
+  pass
+  #base of Car and Motorcycle
+
+class Car(Ground Vehicle):
+  pass
+  #takes in from GroundVehicle
+
+class Motorcycle(GroundVehicle):
+  pass
+  #takes in from GroundVehicle
+
+class Airplane(FlightVehicle):
+  pass
+  #takes in from FlightVehicle
