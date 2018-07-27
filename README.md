99b949a53b418b4828a070f04b0c92ffe8ce49e5
Beej Jorgensen <beej@lambdaschool.com>
2018-07-26 18:46:02 -0700

    added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Sprint-Challenge--Intro-Python
-Intro to Python Sprint Challenge
+#  Sprint Challenge: Intro to Python
+
+* Complete the programs in the `src/` directory in any order.
+
+  * `oop1.py`: class hierarchies
+  * `oop2.py`: subclassing and method overriding
+  * `comp.py`: list comprehensions
+  * `cityreader.py`: modules and CSV file reading
---
+++ src/oop1.py
@@ -0,0 +1,19 @@
+# Write classes for the following class hierarchy:
+#
+#  [Vehicle]->[FlightVehicle]->[Starship]
+#      |                |
+#      v                v
+# [GroundVehicle]      [Airplane]
+#   |       |
+#   v       v
+# [Car]  [Motorcycle]
+#
+# Each class can simply "pass" for its body. The exercise is about setting up
+# the hierarchy.
+#
+# e.g.
+#
+# class Vehicle:
+#     pass
+#
+# Put a comment noting which class is the base class
