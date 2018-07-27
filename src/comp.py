4d95c005617650956a5561f1a3a8e8c34cd02283
Eric Andrade <eric.andrade@gmail.com>
2018-07-27 16:47:09 -0400

    refactored comp

--- src/comp.py
+++ src/comp.py
@@ -23,69 +23,48 @@ humans = [
 # whose name starts with 'D':
 
 print("Starts with D:")
-r = [] 
-for person in humans:
-    if person.name[0] == 'D':
-        r.append(person.name)
+r = [each for each in humans if each.name[0] == "D"] 
 print(r)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 
 print("Ends with e:")
-r = []  
-for person in humans:
-    if person.name[len(person.name) - 1] == 'e':
-        r.append(person.name)
+r = [human for human in humans if (human.name[-1] in ['e'])]
 print(r)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
-import re
-
 print("Starts between C and G, inclusive:")
-r = []  
-for person in humans:
-    pattern = re.compile("[C-G]")
-    if pattern.search(person.name[0]):
-        r.append(person.name)
+r = [h.name for h in humans if (h.name[0] >= "C" and h.name[0] <= "G")]
 print(r)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-r = []  
-for person in humans:
-    r.append(person.age + 10)
+r = [ human.age+10 for human in humans]
 print(r)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-r = []  
-for person in humans:
-    r.append("%s-%s" % (person.name, person.age))
+r = ["-".join([each.name, str(each.age)]) for each in humans]
 print(r)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-r = []  # TODO
+r = [each for each in humans if each.age >= 27 and each.age <= 32]
 print(r)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names capitalized and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names capitalized:")
-r = []  
-for person in humans:
-    r.append((person.name.upper(), person.age + 5))
+r = [Human(human.name.upper(), human.age+5) for human in humans]
 print(r)
 
 # Write a list comprehension that contains the square root of all the ages.
-import math
 print("Square root of ages:")
-r = [] 
-for person in humans:
-    r.append(math.sqrt(person.age))
+r = [each.age**0.5 for each in humans]
 print(r)
