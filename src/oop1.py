96ae0af0a94eb55f866e24fb1f968cb7327c8edf
Anthony <anthony@Anthonys-MacBook-Pro.local>
2018-07-27 08:46:42 -0700

    oop1.py:
    Completed the class hierarchy for all vehicles.

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,32 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base Class - All other classes are a form of vehicle
+class Vehicle:
+    pass
+
+# child class of vehicle 
+class FlightVehicle(Vehicle):
+    pass
+
+# child class of flight vehicle which is a child class of vehicle
+class Starship(FlightVehicle):
+    pass
+
+# child class of flight vehicle which is a child class of vehicle
+class Airplane(FlightVehicle):
+    pass
+
+# child class of Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+# child class of Ground Vehicle
+class Car(GroundVehicle):
+    pass
+    
+# child class of Ground Vehicle
+class Motorcycle(GroundVehicle):
+    pass
+
