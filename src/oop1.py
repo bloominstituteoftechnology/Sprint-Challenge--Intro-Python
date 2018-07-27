c6e9f8e788c9aad6cf0104e5d4e3a30ccee237b7
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2018-07-27 10:25:36 -0500

    First commit -- Started working on oop1.py

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,16 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+Class Vehicle(Vehicle, FlightVehicle, Starship): #base class/Super class
+    def __init__(self, type)
+        self.type = typeOf # type of vehicle
+        self.FlightVehicle = flight 
+        self.Starship = starship
+        self.Vehicle = ground
+
+Class GroundVehicle(Vehicle)
+    def __init__ (self, type)
+        self.type
+        super().__init__("ground") #ask ? about this one.. 
+
+
