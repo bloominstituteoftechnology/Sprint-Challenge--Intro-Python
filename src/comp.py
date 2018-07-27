804dc4276601092c315cb0393e3cb1dab1dab99f
Nedu Robert <nedumrobert@gmail.com>
2018-07-27 17:19:03 +0100

    Implemented List comprehension for names starting with 'D

--- src/comp.py
+++ src/comp.py
@@ -23,7 +23,7 @@ humans = [
 # whose name starts with 'D':
 
 print("Starts with D:")
-r = []  # TODO
+r = [human.name for human in humans if human.name[0] == 'D']  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of names of everyone
