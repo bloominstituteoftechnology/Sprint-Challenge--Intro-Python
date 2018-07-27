fd3267eca0c73876ae62f79777eeda9fcb68e82f
Beej Jorgensen <beej@lambdaschool.com>
2018-07-26 18:46:02 -0700

    added

---
+++ src/oop2.py
@@ -0,0 +1,32 @@
+# To the GroundVehicle class, add method drive() that prints "vroooom".
+#
+# Also change it so the num_wheels defaults to 4 if not specified when the
+# object is constructed.
+
+class GroundVehicle():
+    def __init__(self, num_wheels):
+        self.num_wheels = num_wheels
+
+    # TODO
+
+
+# Subclass Motorcycle from Vehicle.
+#
+# Make it so when you instantiate a Motorcycle, it automatically sets the number
+# of wheels to 2 by passing that to the constructor of its superclass.
+#
+# Override the drive() method in Motorcycle so that it prints "BRAAAP!!"
+
+# TODO
+
+vehicles = [
+    GroundVehicle(),
+    GroundVehicle(),
+    Motorcycle(),
+    GroundVehicle(),
+    Motorcycle(),
+]
+
+# Go through the vechicles list and call drive() on each.
+
+# TODO
