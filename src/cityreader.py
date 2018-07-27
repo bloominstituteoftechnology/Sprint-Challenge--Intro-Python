78ab2005c02d0de3cb3cfad302b53f784d9131f5
Anthony <anthony@Anthonys-MacBook-Pro.local>
2018-07-27 15:40:43 -0700

    Changed comp.py for loops to list comprehensions and finished cityreader.py

--- src/cityreader.py
+++ src/cityreader.py
@@ -1,27 +1,14 @@
-# Create a class to hold a city location. Call the class "City". It should have
-# fields for name, latitude, and longitude.
-
-# TODO
-# We have a collection of US cities with population over 750,000 stored in the
-# file "cities.csv". (CSV stands for "comma-separated values".)
-#
-# Use Python's built-in "csv" module to read this file so that each record is
-# imported into a City instance. (You're free to add more attributes to the City
-# class if you wish, but this is not necessary.) Google "python 3 csv" for
-# references and use your Google-fu for other examples.
-#
-# Store the instances in the "cities" list, below.
-#
-# Note that the first line of the CSV is header that describes the fields--this
-# should not be loaded into a City object.
 import csv
+
 class City:
-    def __init__(name, latitude, longitude):
+    def __init__(self, name, latitude, longitude):
         self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
 
-cities = []
+with open('cities.csv') as file:
+    reader = csv.reader(file)
+    city = [City(i[0], i[3], i[4]) for i in reader]
 
-file = open('cities.csv', newline='')
-reader = csv.reader(file)
-header = next(reader)
-data = [row for row in ]
+for i in city:
+    print('{}, {}, {}'.format(i.name, i.latitude, i.longitude))
--- src/comp.py
+++ src/comp.py
@@ -23,51 +23,51 @@ humans = [
 # whose name starts with 'D':
 
 print("Starts with D:")
-r = []  
+r = [i for i in humans if i.name[0] == "D"]  
 # TODO
-for human in humans:
-    if (human.name[0] == 'D'):
-        r.append(human.name)
+# for human in humans:
+#     if (human.name[0] == 'D'):
+#         r.append(human.name)
 print(r)
 
 # # Write a list comprehension that creates a list of names of everyone
 # # whose name ends in "e".
 
 print("Ends with e:")
-r = []  # TODO
-for human in humans:
-    if (human.name[-1] =='e'):
-        r.append(human.name)
+r = [i for i in humans if i.name[-1] == "e"]  # TODO
+# for human in humans:
+#     if (human.name[-1] =='e'):
+#         r.append(human.name)
 print(r)
 
 # # Write a list comprehension that creates a list of names of everyone
 # # whose name starts with any letter between 'C' and 'G' inclusive.
 
 # print("Starts between C and G, inclusive:")
-r = []  # TODO
-for human in humans:
-    if (human.name[0] == 'C'):
-        r.append(human.name)
-    if (human.name[0] == 'D'):
-        r.append(human.name)
-    if (human.name[0] == 'E'):
-        r.append(human.name)
-    if (human.name[0] == 'F'):
-        r.append(human.name)
-    if (human.name[0] == 'G'):
-        r.append(human.name)
+r = [i for i in humans if i.name[0] >= 'C' and i.name[0] <= 'G']  # TODO
+# for human in humans:
+#     if (human.name[0] == 'C'):
+#         r.append(human.name)
+#     if (human.name[0] == 'D'):
+#         r.append(human.name)
+#     if (human.name[0] == 'E'):
+#         r.append(human.name)
+#     if (human.name[0] == 'F'):
+#         r.append(human.name)
+#     if (human.name[0] == 'G'):
+#         r.append(human.name)
 print(r)
 
 
 # # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-r = []  # TODO
-age = 10
-for human in humans:
-    r.append(human.age)
-for number in r:
-    age += number
-print(age) 
+r = [i.age + 10 for i in humans]  # TODO
+# age = 10
+# for human in humans:
+#     r.append(human.age)
+# for number in r:
+#     age += number
+print(r) 
 
 # # Write a list comprehension that creates a list of strings which are the name
 # # joined to the age with a hyphen, for example "David-31", for all humans.
@@ -86,20 +86,19 @@ print(r)
 # # list, except with all the names capitalized and the ages with 5 added to them.
 # # The "humans" list should be unmodified.
 print("All names capitalized:")
-r = []  # TODO
-for human in humans:
-    r.append(human.name.upper())
-    r.append(human.age + 5)
+r = [(i.name.upper(), i.age + 5) for i in humans]  # TODO
+# for human in humans:
+#     r.append(human.name.upper())
+#     r.append(human.age + 5)
 print(r)
 
 # # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
-r = []  # TODO
-sqrt = []
-for human in humans:
-    r.append(human.age)
-for number in r:
-    x = round(number ** (.5), 1)
-    sqrt.append(x)
-        
-print(sqrt)
+r = ["{0:.2f}".format(i.age**.5) for i in humans]  # TODO
+# sqrt = []
+# for human in humans:
+#     r.append(human.age)
+# for number in r:
+#     x = round(number ** (.5), 1)
+#     sqrt.append(x)
+print(r)
