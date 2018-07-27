0c7448923cb9d7a230e7fe8eec2034df7220a1f5
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-07-27 05:06:27 -0400

    Robert Hogan. Completed oop1, some progress on cityReader.

--- src/cityreader.py
+++ src/cityreader.py
@@ -1,7 +1,15 @@
+#bring in csv
+import csv
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
 
 # TODO
+class City:
+    def __init__(self, name, latitude, longitude):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -16,13 +24,24 @@
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 
+#path: /Users/roberthogan/Desktop/CS9/python/Sprint-Challenge--Intro-Python/src
+
 cities = []
 
 # TODO
+path = "/Users/roberthogan/Desktop/CS9/python/Sprint-Challenge--Intro-Python/src/cities.csv"
+file = open(path)
+reader = csv.reader(file)
+
+#ignore header
+header = next(reader)
+lines = [line for line in file]
+cities.append(lines)
+
 
 # Print the list of cities (name, lat, lon), 1 record per line.
-
 # TODO
+print(f'{cities}\n')
 
 # *** STRETCH GOAL! ***
 #
--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,50 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#main class named Vehicle
+class Vehicle(object):
+    def __init__(self, type, terrain):
+        self.type = type
+        self.terrain = terrain
+
+    def getType(self):
+        return self.type
+
+    def getTerrain(self):
+        return self.terrain
+
+    def __str__(self):
+        return "%s is a %s vehicle" % (self.type, self.terrain)
+
+#Ground vehicle subclass of vehicle class
+class GroundVehicle:
+    def __init__(self, type, color):
+        self.type = type
+        self.color = color
+
+        #instance of ground vehicle called car
+        GroundVehicle("car", "Blue")
+        #instance of ground vehicle called motorcycle
+        GroundVehicle("motorcycle", "Orange")
+
+#flightVehicle sub class of vehicle class
+class FlightVehicle:
+    def __init__(self, type, wingsAmount):
+        self.type = type
+        self.wingsAmount = wingsAmount
+
+        #instance of flight vehicle called bi plane
+        FlightVehicle("Bi-plane", 4)
+                #instance of flight vehicle called helicopter
+        FlightVehicle("Helicopter", 0)
+
+#star ship sub class of vehicle class. 
+class StarShip:
+    def __init__(self, type, capacity):
+        self.type = type
+        self.capacity = capacity
+
+        #transit instance of starship subclass
+        StarShip("transit", 1000)
+        StarShip("battleship", 900)
