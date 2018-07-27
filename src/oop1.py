bd7f23c7ff914bb72b2482ee5ba5c1165d29dadc
Yasin Shuman <email@yasin.io>
2018-07-27 10:09:55 -0700

    welp, i should stop playing with vim and get back to vscode for now...

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,24 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#this is the base class
+class Vehicle:
+    
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+    def getName(self):
+        return self.name
+
+    def getSType(self):
+        return self.type
+
+    def __str__(self):
+        return "%s is a %s" % (self.name, self.type)
+
+class FlightVehicle(Vehicle):
+    
+    def __init__(self):
+        super().__init__("flight")
