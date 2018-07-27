b926fcfbf8e75ec6e110d26a31ad1cd7ea5778aa
JustinSHong <jhong2009@gmail.com>
2018-07-27 08:30:07 -0700

    comp.py: added comp that captures all ages > 10

--- src/comp.py
+++ src/comp.py
@@ -42,7 +42,7 @@ print(r)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-r = []  # TODO
+r = [h.age for h in humans if h.age > 10]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of strings which are the name
