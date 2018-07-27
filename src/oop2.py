679ec08e4b72f5e0c60b830723e2e98b4233824b
David Alexander Morales <damorales89@gmail.com>
2018-07-27 12:26:55 -0400

    Corrections to OOP2

--- src/oop2.py
+++ src/oop2.py
@@ -4,7 +4,7 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
     # TODO
@@ -21,7 +21,7 @@ class GroundVehicle():
 
 # TODO
 
-class Motorcycle(Vehicle):
+class Motorcycle(GroundVehicle):
     def __init__(self):
         super().__init__(2)
 
