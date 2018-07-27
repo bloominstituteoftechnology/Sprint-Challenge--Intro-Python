03bc1df6b03bfb0ea03927f56314872d8ad8eb5e
sadrak105 <sadrak10@gmail.com>
2018-07-27 12:39:34 -0400

    motorcycle class

--- src/oop2.py
+++ src/oop2.py
@@ -21,6 +21,13 @@ class GroundVehicle():
 
 # TODO
 
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(num_wheels=2)
+
+    def drive(self):
+        print('BRAAAP!!')
+
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
