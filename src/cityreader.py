e85284801ef72db96f34cab2871d29ae31f8794c
Finlie <dfinley2917@gmail.com>
2018-07-27 13:31:34 -0400

    comp.py and cityreader complete

--- src/cityreader.py
+++ src/cityreader.py
@@ -2,6 +2,15 @@
 # fields for name, latitude, and longitude.
 
 # TODO
+import csv
+
+
+class City:
+    def__init__(self, name, latitude, longitude):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -16,13 +25,25 @@
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 
+
 cities = []
 
 # TODO
+with open('cities.csv'), newline = '\n' as csvfile:
+    spamreader = csv.reader(csvfile, delimter=',')
+    cities_arr = [row for row in spamreader]
+
+    cities_arr.pop(0)
+    for city in cities_arr:
+        new_city = City(city[0], city[2], city[4])
+        cities.append(new_city)
+
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 
 # TODO
+for city in cities:
+    print(city.name, city.latitude, city.longitude)
 
 # *** STRETCH GOAL! ***
 #
