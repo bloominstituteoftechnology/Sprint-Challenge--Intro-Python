0306528f42892dae606764c3ac5fbfa6a61f63cc
Eric Gomez <eric_gomez_1@hotmail.com>
2018-07-30 10:05:35 -0700

    django project started, migrated all data to mongodb

--- src/comp.py
+++ src/comp.py
@@ -1,5 +1,5 @@
 import binascii
-
+import dis
 
 class Human:
     def __init__(self, name, age):
@@ -24,51 +24,52 @@ humans = [
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
-
-print("Starts with D:")
-r = [i for i in humans if i.name[0] == "D"]  # TODO
-print(r)
-
-# Write a list comprehension that creates a list of names of everyone
-# whose name ends in "e".
-
-print("Ends with e:")
-r = [i for i in humans if i.name[len(i.name) - 1] == "e"]  # TODO
-print(r)
+#
+# print("Starts with D:")
+# r = [i for i in humans if i.name[0] == "D"]  # TODO
+# print(r)
+#
+# # Write a list comprehension that creates a list of names of everyone
+# # whose name ends in "e".
+#
+# print("Ends with e:")
+# r = [i for i in humans if i.name[len(i.name) - 1] == "e"]  # TODO
+# print(r)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
+def compare_str():
+    global humans
+    x = [i for i in humans if 47 >= binascii.hexlify(bytes(i.name[0], 'utf-8')) >= 43]  # TODO
+    print(x)
 
-print("Starts between C and G, inclusive:")
-r = [i for i in humans if 71 >= int(binascii.hexlify(bytes(i.name[0], 'utf-8')), 16) >= 67]  # TODO
-print(r)
-
-# Write a list comprehension that creates a list of all the ages plus 10.
-print("Ages plus 10:")
-r = [i.age + 10 for i in humans]  # TODO
-print(r)
-
-# Write a list comprehension that creates a list of strings which are the name
-# joined to the age with a hyphen, for example "David-31", for all humans.
-print("Name hyphen age:")
-r = [i.name + "-" + str(i.age) for i in humans]  # TODO
-print(r)
-
-# Write a list comprehension that creates a list of tuples containing name and
-# age, for example ("David", 31), for everyone between the ages of 27 and 32,
-# inclusive.
-print("Names and ages between 27 and 32:")
-r = [(i.name, i.age) for i in humans if 32 >= i.age >= 27]  # TODO
-print(r)
-
-# Write a list comprehension that creates a list of new Humans like the old
-# list, except with all the names capitalized and the ages with 5 added to them.
-# The "humans" list should be unmodified.
-print("All names capitalized:")
-r = [Human(i.name.upper(), i.age + 5) for i in humans]  # TODO
-print(r)
-
-# Write a list comprehension that contains the square root of all the ages.
-print("Square root of ages:")
-r = [i.age**(1.0/2) for i in humans]  # TODO
-print(r)
+dis.dis(compare_str)
+# # Write a list comprehension that creates a list of all the ages plus 10.
+# print("Ages plus 10:")
+# r = [i.age + 10 for i in humans]  # TODO
+# print(r)
+#
+# # Write a list comprehension that creates a list of strings which are the name
+# # joined to the age with a hyphen, for example "David-31", for all humans.
+# print("Name hyphen age:")
+# r = [i.name + "-" + str(i.age) for i in humans]  # TODO
+# print(r)
+#
+# # Write a list comprehension that creates a list of tuples containing name and
+# # age, for example ("David", 31), for everyone between the ages of 27 and 32,
+# # inclusive.
+# print("Names and ages between 27 and 32:")
+# r = [(i.name, i.age) for i in humans if 32 >= i.age >= 27]  # TODO
+# print(r)
+#
+# # Write a list comprehension that creates a list of new Humans like the old
+# # list, except with all the names capitalized and the ages with 5 added to them.
+# # The "humans" list should be unmodified.
+# print("All names capitalized:")
+# r = [Human(i.name.upper(), i.age + 5) for i in humans]  # TODO
+# print(r)
+#
+# # Write a list comprehension that contains the square root of all the ages.
+# print("Square root of ages:")
+# r = [i.age**(1.0/2) for i in humans]  # TODO
+# print(r)
