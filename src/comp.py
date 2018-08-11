57bdc9811c333d85c9b32be1f670ba54663b6ea0
unknown <alexisreyes90@gmail.com>
2018-08-11 13:32:21 -0400

    comp.py completed

--- src/comp.py
+++ src/comp.py
@@ -1,3 +1,5 @@
+import math
+
 class Human:
     def __init__(self, name, age):
         self.name = name
@@ -44,30 +46,34 @@ print(r)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-r = []  # TODO
+r = [ i.age + 10 for i in humans ]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-r = []  # TODO
+r = [ '{0}-{1}'.format(i.name, i.age) for i in humans ]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-r = []  # TODO
+ages = [27, 28, 29, 30, 31, 32]
+r = [ ( i.name, i.age ) for i in humans if i.age in ages ]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names capitalized and the ages with 5 added to them.
 # The "humans" list should be unmodified.
+
+# Create new Humans class then?
+
 print("All names capitalized:")
-r = []  # TODO
+r = [ Human( i.name.upper(), i.age + 5 ) for i in humans ]  # TODO
 print(r)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
-r = []  # TODO
+r = [ math.sqrt(i.age) for i in humans ]  # TODO
 print(r)
