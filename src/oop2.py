0b74a7c8f5ed135587c3970c3074fbd648c04cbb
Matthias Bakken <matthias.bakken@gmail.com>
2018-08-11 09:08:28 -0700

    finished oop2.py

--- src/oop2.py
+++ src/oop2.py
@@ -8,7 +8,8 @@ class GroundVehicle():
         self.num_wheels = num_wheels
 
     # TODO
-
+    def drive(self):
+        print("vroooom")
 
 # Subclass Motorcycle from Vehicle.
 #
@@ -18,15 +19,24 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it prints "BRAAAP!!"
 
 # TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels):
+        super().__init__(num_wheels)
+
+    def drive(self):
+        print("BRAAAP")
+
 
 vehicles = [
-    GroundVehicle(),
-    GroundVehicle(),
-    Motorcycle(),
-    GroundVehicle(),
-    Motorcycle(),
+    GroundVehicle(4),
+    GroundVehicle(4),
+    Motorcycle(2),
+    GroundVehicle(4),
+    Motorcycle(2),
 ]
 
 # Go through the vehicles list and call drive() on each.
 
 # TODO
+for vehicle in vehicles:
+    vehicle.drive()
