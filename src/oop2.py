e318047e3f518413330c398ffb4521a68758d7b3
Frank Faustino <nawzyah@gmail.com>
2018-08-14 17:35:16 -0700

    Fix mistake in oop2.py

--- src/oop2.py
+++ src/oop2.py
@@ -19,7 +19,7 @@ class GroundVehicle:
 
 class Motorcycle(GroundVehicle):
     def __init__(self, num_wheels = 2):
-        GroundVehicle().__init__(num_wheels)
+        GroundVehicle.__init__(self, num_wheels)
 
     def drive(self):
         print('BRAAAP!!')
