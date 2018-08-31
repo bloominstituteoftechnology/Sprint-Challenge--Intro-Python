5082343a5406ee8a00a80b0f14539fe0638b749e
Alec <afroenygam@gmail.com>
2018-08-31 11:27:18 -0400

    oop1.py updated

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,46 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#Base class
+class Vehicle:
+    """Vehicle creates a generic Vehicle object."""
+    pass
+
+class FlightVehicle(Vehicle):
+    """FlightVehicle inherits from Vehicle"""
+    def __init__(self):
+        super(Vehicle, self).__init__()
+        self.arg = arg
+    pass
+
+class Airplane(FlightVehicle):
+    """Airplane inherits from FlightVehicle which inherits from Vehicle"""
+    def __init__(self, arg):
+        super(Airplane, self).__init__()
+        self.arg = arg
+
+
+class Starship(FlightVehicle):
+    """Starship inherits from FlightVehicle class which inherits from Vehicle"""
+    def __init__(self, arg):
+        super(FlightVehicle, self).__init__()
+        self.arg = arg
+
+class GroundVehicle(Vehicle):
+    """GroundVehicle inherits from Vehicle"""
+    def __init__(self, arg):
+        super(Vehicle, self).__init__()
+        self.arg = arg
+
+class Motorcycle(GroundVehicle):
+    """Motorcycle inherits from GroundVehicle"""
+    def __init__(self, arg):
+        super(GroundVehicle, self).__init__()
+        self.arg = arg
+
+class Car(GroundVehicle):
+    """Car inherits from GroundVehicle which inherits from Vehicle"""
+    def __init__(self, arg):
+        super(GroundVehicle, self).__init__()
+        self.arg = arg
