defb606a9abfa809f3197656474e57a806f2ecd1
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-31 09:30:27 -0600

    Completed all tasks for oop1.py

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base Class Vehicle
+class Vehicle:
+    pass
+
+# Ground Vehicles
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+# Flight Vehicles
+class FlightVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
