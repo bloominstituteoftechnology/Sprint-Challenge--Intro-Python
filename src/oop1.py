adccff71b7382b4bdfc9db196c7f583396aa2e63
Mark Hong <m4rkh0ng@gmail.com>
2018-08-31 12:10:05 -0400

    complete oop1

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,36 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle is the base class
+
+class Vehicle:
+  def __init__(self, name):
+    self.name = name
+
+class GroundVehicle(Vehicle):
+  def __init__(self, name):
+    Vehicle.__init__(self, name)
+
+class Car(GroundVehicle):
+  def __init__(self, name):
+    GroundVehicle.__init__(self, name)
+
+class Motorcycle(GroundVehicle):
+  def __init__(self, name):
+    GroundVehicle.__init__(self, name)
+
+# Vehicle is again the base class, here
+
+class FlightVehicle(Vehicle):
+  def __init__(self, name):
+    Vehicle.__init__(self, name)
+
+class Airplane(FlightVehicle):
+  def __init__(self, name):
+    FlightVehicle.__init__(self, name)
+  
+class Starship(FlightVehicle):
+  def __init__(self, name):
+    FlightVehicle.__init__(self, name)
+
