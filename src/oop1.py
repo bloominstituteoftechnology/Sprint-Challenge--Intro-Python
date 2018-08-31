f3c7f2884d31ed2f3032911a13154d205a87d05c
Austin Merando <ajmerando1@yahoo.com>
2018-08-31 11:43:58 -0700

    missed a class

--- src/oop1.py
+++ src/oop1.py
@@ -18,14 +18,18 @@
 #
 # Put a comment noting which class is the base class
 
-class Vehicle:      #Base Class
+
+class Vehicle:  # Base Class
     pass
+
 
 class GroundVehicle(Vehicle):
     pass
 
+
 class Car(GroundVehicle):
     pass
+
 
 class Motorcycle(GroundVehicle):
     pass
@@ -33,6 +37,11 @@ class Motorcycle(GroundVehicle):
 
 class FlightVehicle(Vehicle):
     pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
 
 class Starship(FlightVehicle):
     pass
