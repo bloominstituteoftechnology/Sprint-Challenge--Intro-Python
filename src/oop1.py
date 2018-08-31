06099252457eb86982a79ba22d3a19185b435927
CLN <cln@Sags-MacBook-Pro.local>
2018-08-31 11:27:12 -0400

    Setup base class,.

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,13 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle: #base class here
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    def __repr__(self):
+        return f"{self.name}"
+    def __str__(self):
+        return f"{self.name}"     
+
+
