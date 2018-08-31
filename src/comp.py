09c35e5070225ba1ac685c02e0f06d5be3e875d7
M.Real <mreal126@protonmail.com>
2018-08-31 11:32:06 -0400

    Almost done comp.py

--- src/comp.py
+++ src/comp.py
@@ -1,3 +1,4 @@
+import math
 class Human:
     def __init__(self, name, age):
         self.name = name
@@ -23,49 +24,56 @@ humans = [
 # whose name starts with 'D':
 
 print("Starts with D:")
-r = []  # TODO
+r = [human.name for human in humans if human.name[0] == 'D']
 print(r)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 
 print("Ends with e:")
-r = []  # TODO
+r = [human.name for human in humans if human.name[:-1] == 'e']
 print(r)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 
 print("Starts between C and G, inclusive:")
-r = []  # TODO
+list = ['C', 'D', 'E', 'F', 'G']
+r = [human.name for human in humans if human.name[0] in list]
 print(r)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-r = []  # TODO
+age_plus_ten = []
+r = [human.age + 10 for human in humans]
+age_plus_ten.append(r)
 print(r)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-r = []  # TODO
+r = [f'{human.name}-{human.age}' for human in humans]
 print(r)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-r = []  # TODO
+r = [(human.name, human.age) for human in humans if human.age >= 27 and human.age <= 32]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names capitalized and the ages with 5 added to them.
 # The "humans" list should be unmodified.
+def capitalize_plus_five(human):
+    human.name = human.name.capitalize()
+    human.age += 5
+
 print("All names capitalized:")
-r = []  # TODO
+r = [capitalize_plus_five(human) for human in humans]
 print(r)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
-r = []  # TODO
+r = [math.sqrt(human.age) for human in humans]
 print(r)
