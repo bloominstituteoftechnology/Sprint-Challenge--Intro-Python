dc48cfce5168fd02351703ff57865c25b1ac3d18
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-30 17:33:05 -0700

    oop1.py

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,18 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle: # base
+    pass
+class FlightVehicle(Vehicle):
+    pass
+class Airplane(FlightVehicle):
+    pass
+class Starship(FlightVehicle):
+    pass
+class GroundVehicle(Vehicle):
+    pass
+class Car(GroundVehicle):
+    pass
+class Motorcycle(GroundVehicle):
+    pass
