aee93d45f8a5778e7b44569e2f39144da8c503d9
Gabriel Duquette <gabrielduquette@gmail.com>
2018-08-31 11:25:05 -0400

    addoop1.py

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,41 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle: #base class
+    def __init__(self, *args, **kwargs):
+        pass
+
+class FlightVehicle(Vehicle):
+    def __init__(self, *args, **kwargs):
+        Vehicle.init(self, *args, **kwargs)
+        pass
+
+class Airplane(Vehicle, FlightVehicle):
+    def __init__(self, *args, **kwargs):
+        Vehicle.init(self, *args, **kwargs)
+        FlightVehicle.init(self, *args, **kwargs)
+        pass
+
+class Starship(Vehicle, FlightVehicle):
+    def __init__(self, *args, **kwargs):
+        Vehicle.init(self, *args, **kwargs)
+        FlightVehicle.init(self, *args, **kwargs)
+        pass
+
+class GroundVehicle(Vehicle):
+    def __init__(self, *args, **kwargs):
+        Vehicle.init(self, *args, **kwargs)
+        pass
+
+class Car(Vehicle, GroundVehicle):
+    def __init__(self, *args, **kwargs):
+        Vehicle.init(self, *args, **kwargs)
+        GroundVehicle(self, *args, **kwargs)
+        pass
+
+class Motorcycle(Vehicle, GroundVehicle):
+    def __init__(self, *args, **kwargs):
+        Vehicle.init(self, *args, **kwargs)
+        GroundVehicle(self, *args, **kwargs)
+        pass
