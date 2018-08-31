3b2350f875440ce30396c048de570dfede8733cf
-tristan- <tristan.linehan@gmail.com>
2018-08-31 12:52:07 -0700

    completed oop1.py

--- src/oop1.py
+++ src/oop1.py
@@ -1,4 +1,3 @@
-# START
 # Write classes for the following class hierarchy:
 #
 #  [Vehicle]->[FlightVehicle]->[Starship]
@@ -18,3 +17,40 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    """Base Class"""
+    def __init__(self, name):
+        self.name = name
+        self.base = "vehicle"
+
+
+
+class GroundVehicle(Vehicle):
+    """secondary base"""
+    def __init__(self, name):
+        super().__init__(name)
+        self.type = "ground"
+
+class Car(GroundVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class FlightVehicle(Vehicle):
+    """secondary base"""
+    def __init__(self, name):
+        super().__init__(name)
+        self.type = "air"
+
+class Starship(FlightVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Airplane(FlightVehicle):
+    def __init__(self, name):
+        super().__init__(name)
