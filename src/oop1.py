96ba175721385fa3338160c6d8c3cd81828db4c5
markdavidwilliams <markwilliams.aus@gmail.com>
2018-08-31 10:18:06 -0500

    completed OOP1

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,45 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+######
+# Classes
+#####
+
+# Base Classes
+
+class Vehicle:
+  def __init__(self, name, description):
+    pass
+
+class FlightVehicle:
+ def __init__(self, name, description):
+    pass
+
+class Starship:
+  def __init__(self, name, description):
+    pass
+
+# Inherited from base classes
+
+class GroundVehicle(Vehicle):
+  def __init__(self, name, description, type):
+    super().__init__(name, description)
+    pass
+
+class Airplane(FlightVehicle):
+  def __init__(self, name, description, type):
+    super().__init__(name, description)
+    pass
+
+# Inherited from Ground Vehicle
+
+class Car(GroundVehicle):
+  def __init__(self, name, description, type, make, model):
+    super().__init__(name, description, type)
+    pass
+
+class Motorcycle(GroundVehicle):
+  def __init__(self, name, description, type, make, model):
+    super().__init__(name, description, type)
+    pass
