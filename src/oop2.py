42d58ebb57ce6c0eddddd8f9bd398793138bbbca
Adrian Adames <adrianadames@gmail.com>
2018-08-31 11:51:54 -0400

    Mostly done with oop2.py. Need to address the 'None' string that prints after the drive sounds print. Need to find better way to default Motorcyle number_wheel to 2.

--- src/oop2.py
+++ src/oop2.py
@@ -12,12 +12,31 @@ class GroundVehicle():
     def drive(self):
         print('vrooom')
 
+# vehicle1 = GroundVehicle()
+# print(vehicle1.drive())
+
+
+
+
+
 # Subclass Motorcycle from Vehicle.
 #
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it prints "BRAAAP!!"
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        if num_wheels == 2:
+            num_wheels = 2
+        self.num_wheels = num_wheels
+
+    def drive(self):
+        print('BRAAAP!!')
+
+vehicle2 = Motorcycle()
+print(vehicle2.drive())
 
 
 
@@ -32,4 +51,8 @@ vehicles = [
 
 # Go through the vehicles list and call drive() on each.
 
-# TODO
+for vehicle in vehicles:
+    print(vehicle.drive())
+
+
+
