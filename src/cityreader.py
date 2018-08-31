33c9bf38374d53f7722421ff14fab43d0ec2b830
chainchompa <chainchompa@gmail.com>
2018-08-31 13:32:11 -0400

    completed cityreader, not including stretch

--- src/cityreader.py
+++ src/cityreader.py
@@ -1,7 +1,14 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
 
-# TODO
+class City():
+    def __init__(self, name, latitude, longitude):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+    def __str__(self):
+        return "<name: {}\n location: {},{}>".format(self.name, self.latitude, self.longitude)
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -18,12 +25,22 @@
 
 cities = []
 
-# TODO
+with open('cities.csv') as csvfile:
+    readCSV = csv.reader(csvfile)
+
+    for row in readCSV:
+        cities.append(row)
+del cities[0]
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 
-# TODO
+cityLocations = []
 
+for city in cities:
+    cityLocations.append(City(city[0], city[3], city[4]))
+
+for city in cityLocations:
+    print(city)
 # *** STRETCH GOAL! ***
 #
 # Allow the user to input two points, each specified by latitude and longitude.
