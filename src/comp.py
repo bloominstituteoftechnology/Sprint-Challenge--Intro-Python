4e3c423b0eadf94b8979ad04f4d00a01deb15506
JacobLayton <jlayjlay3@gmail.com>
2018-08-31 11:02:45 -0700

    Comp nearly complete

--- src/comp.py
+++ src/comp.py
@@ -23,14 +23,14 @@ humans = [
 # whose name starts with 'D':
 
 print("Starts with D:")
-r = []  # TODO
+r = [l for l in humans if l.name[0] == "D"]
 print(r)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 
 print("Ends with e:")
-r = []  # TODO
+r = [l for l in humans if l.name[-1] == "e"]
 print(r)
 
 # Write a list comprehension that creates a list of names of everyone
@@ -42,20 +42,20 @@ print(r)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-r = []  # TODO
+r = [n.age + 10 for n in humans]
 print(r)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-r = []  # TODO
+r = []
 print(r)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-r = []  # TODO
+r = [n for n in humans if n.age >= 27 and n.age <= 32]
 print(r)
 
 # Write a list comprehension that creates a list of new Humans like the old
@@ -67,5 +67,5 @@ print(r)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
-r = []  # TODO
+r = [n.age ** 0.5 for n in humans]
 print(r)
