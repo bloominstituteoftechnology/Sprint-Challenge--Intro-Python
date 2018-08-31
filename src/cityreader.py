279418a631a9103f06f7acfe2c82084e7deacedf
JJ Ashcraft <jj@jjashcraft.com>
2018-08-31 11:25:23 -0400

    CS11-JJAshcraft-Sprint-Challenge--Intro-Python Initial Commit *Starting cityreader.py

--- src/cityreader.py
+++ src/cityreader.py
@@ -1,7 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
+import csv
+cites = open('cities.csv')
 
 # TODO
+
+class City:
+  def __init__(self, name, latitude, longitude):
+    self.name = name
+    self.latitude = latitude
+    self.longitude = longitude
+  
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
