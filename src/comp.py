30a589e21e5a20fae69d938dfaf7ee9fcc04b900
Daniel Chamorro <dan@dcitllc.com>
2018-09-04 11:16:06 -0400

    Comp: All names capitalized

--- src/comp.py
+++ src/comp.py
@@ -64,7 +64,7 @@ print(r)
 # list, except with all the names capitalized and the ages with 5 added to them.
 # The `humans` list should be unmodified.
 print("All names capitalized:")
-r = []  # TODO
+r = [Human(h.name.capitalize(), h.age + 5) for h in humans]  # TODO
 print(r)
 
 # Write a list comprehension that contains the square root of all the ages.
