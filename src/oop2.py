c2164643ea65ffbac0946f1f3c8441cac7c9b981
Daniel Chamorro <dan@dcitllc.com>
2018-09-04 10:14:01 -0400

    GroundVehicle

--- src/oop2.py
+++ src/oop2.py
@@ -3,9 +3,13 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
+
+    def drive(self):
+        print("vroooom")
 
     # TODO
 
