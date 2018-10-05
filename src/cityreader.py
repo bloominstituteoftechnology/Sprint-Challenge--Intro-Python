7f435ff7ff23b6f475597792336dccfdbe09d7e0
Fred <sohnf90@yahoo.com>
2018-10-05 09:34:17 -0700

    Completed comp

--- src/cityreader.py
+++ src/cityreader.py
@@ -1,6 +1,11 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
-
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+        
 # TODO
 
 # We have a collection of US cities with population over 750,000 stored in the
