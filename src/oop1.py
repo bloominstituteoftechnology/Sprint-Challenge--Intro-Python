fc4cfcbe853266f53b6c3e71debed8080d15e4bd
Naaz Shaikh <naazhome@Naazs-MBP-3.hsd1.or.comcast.net>
2018-10-05 16:07:43 -0700

    oop1 done

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:  # base class
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
