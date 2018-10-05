32057cdf389b3a2a5d8c68fab56db0022ab0d731
jordan-massingill <jordan_massingill@hotmail.com>
2018-10-05 10:39:52 -0500

    Jordan Massingill
    Initial PR
    Completed OOP1
    Working through OOP2

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,40 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:          #Base Class
+    def __init__(self):
+        self.description = 'I am a basic vehicle'
+        pass
+
+class FlightVehicle(Vehicle):   # One level deep
+    def __init__(self):
+        Vehicle.__init__(self)
+        self.description = 'I am a flying vehicle'
+        pass
+
+class Starship(FlightVehicle):  #Two levels deep
+    def __init__(self):
+        FlightVehicle.__init__(self)
+        self.description = 'I am a starship, your argument is invalid'
+        pass
+
+class GroundVehicle(Vehicle):   #One level deep
+    def __init__(self):
+        Vehicle.__init__(self)
+        pass
+
+class Airplane(FlightVehicle):  #Two levels deep
+    def __init__(self):
+        FlightVehicle.__init__(self)
+        pass
+
+class Car(GroundVehicle):   #Two levels deep
+    def __init__(self):
+        GroundVehicle.__init__(self)
+        pass
+
+class Motorcycle(GroundVehicle):    #Two levels deep
+    def __init__(self):
+        GroundVehicle.__init__(self)
+        pass
