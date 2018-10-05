dcefd18814140cc05e1fd64105195d38552cc575
cubsfan46350 <cubsfan.46350@msn.com>
2018-10-05 10:32:16 -0500

    completed oop1

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#Vehicle is the base class
+class Vehicle:
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class FlightVehicle:
+    pass
+
+class Starship:
+    pass
+
+#FlightVehicle is the base class
+class Airplane(FlightVehicle):
+    pass
+
