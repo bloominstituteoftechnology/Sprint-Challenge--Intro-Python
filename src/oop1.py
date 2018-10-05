1dfcf46b0948a14b9bd8f9b4c6abe6632b4e85f6
Erik Kimsey <erik@erikkimsey.com>
2018-10-05 11:19:45 -0400

    Completed oop1.py

--- src/oop1.py
+++ src/oop1.py
@@ -7,7 +7,56 @@
 #   |       |
 #   v       v
 # [Car]  [Motorcycle]
+
+
+# Parent/Base Super Class
+class Vehicle():
+    def __init__(self):
+        pass
+
+
+# Parent Class to inheriting FlightVehicle Classes
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        Vehicle.__init__(self)
+        pass
+
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        FlightVehicle.__init__(self)
+        pass
+
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        FlightVehicle.__init__(self)
+        pass
+
 #
+# GroundVehicles
+#
+# Parent Class to inheriting GroundVehicle Classes
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        Vehicle.__init__(self)
+        pass
+
+
+class Car(GroundVehicle):
+    def __init__(self):
+        GroundVehicle.__init__(self)
+        pass
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        GroundVehicle.__init__(self)
+        pass
+
+
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
 #
@@ -17,3 +66,4 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+``
