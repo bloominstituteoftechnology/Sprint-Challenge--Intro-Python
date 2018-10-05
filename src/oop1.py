5eb0c1ac556d449ef780dfe212f58ba090ad2d63
tulf <timzulf@gmail.com>
2018-10-05 11:20:25 -0400

    Tim Zulf -- Sprint Challenge WIP
    oop1.py complete

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,24 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+#Vehicle is the base class
+class Vehicle(object):
+    pass
+class GroundVehicle(Vehicle):
+    pass
+class Car(GroundVehicle):
+    pass
+class Motorcycle(GroundVehicle):
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+class Starship(FlightVehicle):
+    pass
+class Airplane(FlightVehicle):
+    pass
+
+#test
+mitsubishi = Car()
+PJ = Airplane()
+print mitsubishi,  PJ
