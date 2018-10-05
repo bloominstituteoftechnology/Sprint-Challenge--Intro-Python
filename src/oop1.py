2c53ce1a1abd2222e8d1d129c525efa8969d093d
AI2EJAY <JHammett1314@gmail.com>
2018-10-05 10:37:30 -0500

    OOP 1 & 2 done

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,43 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle (Base Class):
+class Vehicle():
+    def __init__(self, name):
+        pass
+        
+# Ground        
+class GroundVehicle(Vehicle): 
+    def __init__(self, name):
+        super().__init__(name)      
+        pass
+
+class Car(GroundVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+        pass
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+        pass
+
+#==========================================
+# Air
+#  
+class FlightVehicle(Vehicle): 
+    def __init__(self, name):
+        super().__init__(name)      
+        pass
+
+
+class Airplane(FlightVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+        pass
+
+class Starship(FlightVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+        pass
