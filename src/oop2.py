df2a2912c72da826db3034fe1f7bdcb1593a8da4
AI2EJAY <JHammett1314@gmail.com>
2018-10-05 15:22:00 -0500

    fixed oop2 from solution. It might have worked the way I had it but I like it the way he did it

--- src/oop2.py
+++ src/oop2.py
@@ -20,8 +20,8 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it prints "BRAAAP!!"
 class Motorcycle(GroundVehicle):
-    def __init__(self, num_wheels=2):
-        super().__init__(num_wheels)
+    def __init__(self):
+        super().__init__(2)
 
     def drive(self):
         print("BRAAAP!!")  
