1f39eb19813699c281ba74a73c3cc477503c1b4f
Charlie Winslow <cwinslow22@yahoo.com>
2018-10-05 10:31:35 -0500

    oop1 completed

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,30 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle():
+    pass
+
+class FlightVehicle(Vehicle):
+    # base class is Vehicle
+    pass
+
+class Starship(FlightVehicle):
+    # base class is FlightVehicle
+    pass
+
+class Airplane(FlightVehicle):
+    # base class is FlightVehicle
+    pass
+
+class GroundVehicle(Vehicle):
+    # base class is Vehicle
+    pass
+
+class Car(GroundVehicle):
+    # base class is GroundVehicle
+    pass
+
+class Motorcycle(GroundVehicle):
+    # base class is GroundVehicle
+    pass
