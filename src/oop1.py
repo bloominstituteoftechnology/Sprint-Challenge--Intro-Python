7b572b30b88c2388c1f994d87bcda8516ac51d53
bri79319 <bri79319@gmail.com>
2018-10-05 13:14:54 -0400

    oop1.py done

--- src/oop1.py
+++ src/oop1.py
@@ -8,6 +8,7 @@
 #   v       v
 # [Car]  [Motorcycle]
 #
+#
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
 #
@@ -17,3 +18,30 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    # base class
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
