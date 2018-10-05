ccdddff29cb2006e14063c9a078baa835d3af213
Erik Kimsey <erik@erikkimsey.com>
2018-10-05 12:13:55 -0400

    cityreader.py MVP completed

--- src/cityreader.py
+++ src/cityreader.py
@@ -1,22 +1,47 @@
+import csv
+
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
 
+class City:
+    def __init__(self, name, latitude, longitude):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+
 # TODO
 
-# We have a collection of US cities with population over 750,000 stored in the
-# file "cities.csv". (CSV stands for "comma-separated values".)
-#
-# Use Python's built-in "csv" module to read this file so that each record is
-# imported into a City instance. (You're free to add more attributes to the City
-# class if you wish, but this is not necessary.) Google "python 3 csv" for
-# references and use your Google-fu for other examples.
-#
-# Store the instances in the "cities" list, below.
-#
-# Note that the first line of the CSV is header that describes the fields--this
-# should not be loaded into a City object.
 
 cities = []
+# rows: city row[0], lat row[3], lon row[4]
+with open('cities.csv') as citiescsv:
+    readCSV = csv.reader(citiescsv, delimiter=',')
+    for city in readCSV:
+        name = city[0]
+        lat = city[3]
+        lon = city[4]
+        cities.append(City(name, lat, lon))
+
+cities.pop(0)
+
+for city in cities:
+    print(f"\n{city.name}, {city.latitude}, {city.longitude}\n")
+    print("---------------------------------------------")
+
+    # We have a collection of US cities with population over 750,000 stored in the
+    # file "cities.csv". (CSV stands for "comma-separated values".)
+    #
+    # Use Python's built-in "csv" module to read this file so that each record is
+    # imported into a City instance. (You're free to add more attributes to the City
+    # class if you wish, but this is not necessary.) Google "python 3 csv" for
+    # references and use your Google-fu for other examples.
+    #
+    # Store the instances in the "cities" list, below.
+    #
+    # Note that the first line of the CSV is header that describes the fields--this
+    # should not be loaded into a City object.
+
 
 # TODO
 
