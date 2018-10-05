dee57d2c81a2a2f029f9ae3e3c87ad4cbb8a2fbe
codejoncode <jonathanjholloway@gmail.com>
2018-10-05 10:55:22 -0500

    oop2.py has been completed

--- src/oop2.py
+++ src/oop2.py
@@ -4,14 +4,24 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
     # TODO
+    def drive(self):
+        print("vroooom")
 
 
 # Subclass Motorcycle from GroundVehicle.
 #
+class Motorcycle (GroundVehicle):
+    def __init__(self, num_wheels =2):
+        GroundVehicle.__init__(self, num_wheels)
+    
+    def drive(self):
+        print("BRAAAP!!")
+
+    
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
@@ -28,5 +38,6 @@ vehicles = [
 ]
 
 # Go through the vehicles list and call drive() on each.
-
+for vehicle in vehicles:
+    vehicle.drive()
 # TODO
