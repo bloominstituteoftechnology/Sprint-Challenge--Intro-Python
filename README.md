16e4fc7effca4d2fcbbcf43e94659ad3a46137bd
vanceleon <vanceleon44@gmail.com>
2018-10-05 14:22:03 -0400

    cityreader

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 * Complete the programs in the `src/` directory in any order.
 
-  * `oop1.py`: class hierarchies
-  * `oop2.py`: subclassing and method overriding
-  * `comp.py`: list comprehensions
-  * `cityreader.py`: modules and CSV file reading
+    * `oop1.py`: class hierarchies
+    * `oop2.py`: subclassing and method overriding
+    * `comp.py`: list comprehensions
+    * `cityreader.py`: modules and CSV file reading
--- src/cityreader.py
+++ src/cityreader.py
@@ -1,7 +1,14 @@
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
 
 # TODO
+
+class City():
+    def __init__(self, name, latitude, longitude):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -10,7 +17,14 @@
 # imported into a City instance. (You're free to add more attributes to the City
 # class if you wish, but this is not necessary.) Google "python 3 csv" for
 # references and use your Google-fu for other examples.
-#
+
+file = open("cities.csv", "r")
+
+# for row in file:
+#     print(row)
+
+cityList = file.read().split("\n")
+print(cityList)
 # Store the instances in the "cities" list, below.
 #
 # Note that the first line of the CSV is header that describes the fields--this
@@ -21,6 +35,20 @@ cities = []
 # TODO
 
 # Print the list of cities (name, lat, lon), 1 record per line.
+
+for city in cityList: 
+    print(City(name = str(city[0]), latitude = city[3], longitude = city[4]))
+    
+
+# print(cities)
+
+
+
+
+
+
+
+# above is MVP
 
 # TODO
 
