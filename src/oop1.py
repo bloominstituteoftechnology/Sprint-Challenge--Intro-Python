1be2b6ca6db7c923cb1c2b7894b84a5c4e8a7fd9
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-05 10:22:59 -0500

    oop1
    Finished with oop1 setting up a heirarchy system.

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,39 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#base class
+
+class Vehicle:
+    def __init__(self, name):
+        self.name = name
+#secondary classes
+
+class GroundVehicle(Vehicle):
+    def __init__(self, name):
+        self.name = name
+
+class FlightVehicle(Vehicle):
+    def __init__(self, name):
+        self.name = name
+
+#children of the secondary classes
+#Ground Vehicle Children
+
+class Car(GroundVehicle):
+    def __init__(self, name):
+        self.name = name
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name):
+        self.name = name
+
+#Flight Vehicle Children
+
+class Airplane(FlightVehicle):
+    def __init__(self, name):
+        self.name = name
+
+class Starship(FlightVehicle):
+    def __init__(self, name):
+        self.name = name
