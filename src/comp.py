45a0ee35d11e93ced6e87593659addf6e549bdb9
adamcglee <adamcglee@gmail.com>
2018-10-05 12:25:59 -0700

    fixed incorrect code

--- src/comp.py
+++ src/comp.py
@@ -58,7 +58,7 @@ print(r)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-r = [(human.name, human.age) for human in humans]  # TODO
+r = [(human.name, human.age) for human in humans if human.age >= 27 and human.age <= 32]  # TODO
 print(r)
 
 # Write a list comprehension that creates a list of new Humans like the old
