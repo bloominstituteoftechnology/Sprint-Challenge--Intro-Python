f08284f4701e77b134119bfbbc1f3cf3b02a3211
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-10-05 10:22:50 -0600

    OOP1 complete

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,40 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle: #base class
+    def __init__(self, wheels, steering):
+        self.wheels = wheels
+        self.steering = steering
+
+class FlightVehicle: # base class
+    def __init__(self, engineType):
+        self.engineType = engineType
+
+class Starship: # base class
+    def __init__(self, typeOf, speed):
+        self.typeOf = typeOf
+        self.speed = speed
+
+class GroundVehicle(Vehicle): 
+    def __init__(self, engine):
+        self.engine = engine
+        super().__init__(wheels, steering)
+
+class Car(GroundVehicle):
+    def __init__(self, brand, mpg):
+        self.brand = brand
+        self.mpg = mpg
+        super().__init__(wheels, steering, engine)
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, brand, mpg):
+        self.brand = brand
+        self.mpg = mpg
+        super().__init__(wheels, steering, engine)
+
+class Airplane(FlightVehicle):
+    def __init__(self, capacity)
+        self.capacity = capacity
+        super().__init__(engineType)
+
