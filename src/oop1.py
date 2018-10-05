63dbefeff41ce7c9df60125043db28969e9aed19
adamcglee <adamcglee@gmail.com>
2018-10-05 08:07:44 -0700

    Adam Lee: Sprint-Challenge-Intro-Python
    first commit and completed oop1 file code

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,25 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#This is the Vehicle base class
+class Vehicle:
+    pass
+
+class GoundVehicle(Vehicle):
+    pass
+
+class Car(GoundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(Vehicle):
+    pass
