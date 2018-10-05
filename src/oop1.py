6b10dc161dad7662a58da181a31ba89b916d9958
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-10-05 08:47:36 -0700

    OOP1

--- src/oop1.py
+++ src/oop1.py
@@ -20,7 +20,36 @@
 
 # Vehicle is the base class
 class Vehicle():
-    def __init__(self, name):
+    def __init__(self, name, color):
         self.name = name
+        self.color = color
 
+class GroundVehicle(Vehicle):
+    def __init__(self, name, color, numberOfTires):
+        Vehicle.__init__(self, name, color)
+        self.numberOfTires = numberOfTires
 
+class Car(GroundVehicle:)
+    def __init__(self, name, color, numberOfTires, numberOfDoors):
+        GroundVehicle.__init__(self, name, color, numberOfTires)
+        self.numberOfDoors = numberOfDoors
+
+class Motorcycle(GroundVehicle:)
+    def __init__(self, name, color, numberOfTires, numberOfDoors, typeOfMotorcycle):
+        GroundVehicle.__init__(self, name, color, numberOfTires)
+        self.typeOfMotorcycle = typeOfMotorcycle
+
+class FlightVehicle(Vehicle):
+    def __init__(self, name, color, propulsionSystem):
+        Vehicle.__init__(self, name, color)
+        self.propulsionSystem = propulsionSystem
+
+class Airplane(FlightVehicle):
+    def __init__(self, name, color, propulsionSystem):
+        FlightVehicle.__init__(self, name, color, propulsionSystem)
+    def __str__(self):
+        return self.name
+
+class Starship(FlightVehicle):
+    def __init__(self, name, color, propulsionSystem):
+        FlightVehicle.__init__(self, name, color, propulsionSystem)
