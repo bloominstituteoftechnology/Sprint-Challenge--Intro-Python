8f835abae23a28f069773f39e7eb0af4c9f8e434
Nicocchi <nicocchilover@gmail.com>
2018-11-09 11:15:56 -0500

    oop1.py completed

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class
+class Vehicle():
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+# Base class
+class FlightVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+# Base class
+class Starship(FlightVehicle):
+    pass
