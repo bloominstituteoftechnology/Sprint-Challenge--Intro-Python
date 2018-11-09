ce44040afbfa28acf172ea0d395907677f82b2c6
Carlos Vargas <cornielleandres@gmail.com>
2018-11-09 10:19:35 -0500

    Complete oop1.py file.

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,44 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# Base class
+class Vehicle:
+	def __init__(self, engine):
+		self.engine = engine
+
+# Inherits from Vehicle class
+class FlightVehicle(Vehicle):
+	def __init__(self, engine, wings):
+		Vehicle.__init__(self, engine)
+		self.wings = wings
+
+# Inherits from FlightVehicle class
+class Airplane(FlightVehicle):
+	def __init__(self, engine, wings, jetFuel):
+		FlightVehicle.__init__(self, engine, wings)
+		self.jetFuel = jetFuel
+
+# Inherits from FlightVehicle class
+class Starship(FlightVehicle):
+	def __init__(self, engine, wings, nuclearFuel):
+		FlightVehicle.__init__(self, engine, wings)
+		self.nuclearFuel = nuclearFuel
+
+# Inherits from Vehicle class
+class GroundVehicle(Vehicle):
+	def __init__(self, engine, gasolineFuel):
+		Vehicle.__init__(self, engine)
+		self.gasolineFuel = gasolineFuel
+
+# Inherits from GroundVehicle class
+class Car(GroundVehicle):
+	def __init__(self, engine, gasolineFuel, fourWheels):
+		GroundVehicle.__init__(self, engine, gasolineFuel)
+		self.fourWheels = fourWheels
+
+# Inherits from GroundVehicle class
+class Motorcycle(GroundVehicle):
+	def __init__(self, engine, gasolineFuel, twoWheels):
+		self.twoWheels = twoWheels
