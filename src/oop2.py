8cb7a18d18ea96dd4970ce852c891ab6dff34939
mparedes003 <michelle.g.paredes@gmail.com>
2018-11-09 06:31:25 -0800

    Created child class Motorcycle which inherits from GroundVehicle but automatically chainging  the num_wheels to 2. and overrid drive method in Motorcycle to print BRAAAP!!.

--- src/oop2.py
+++ src/oop2.py
@@ -15,9 +15,14 @@ def drive(self):
 #
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
 # of wheels to 2 by passing that to the constructor of its superclass.
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels=2)
+        self.num_wheels = num_wheels
 #
 # Override the drive() method in Motorcycle so that it prints "BRAAAP!!"
-
+    def drive(self):
+        print("BRAAAP!!")
 # TODO
 
 vehicles = [
