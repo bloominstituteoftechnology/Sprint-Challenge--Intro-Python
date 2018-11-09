c5aa4c93759de49c46a02ebefc2a88a0871ce62f
jlaluces123 <jlalucescareer123@gmail.com>
2018-11-09 08:18:29 -0800

    Oop1: Oop1 done

--- src/oop1.py
+++ src/oop1.py
@@ -17,3 +17,34 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle: # base
+    def __init__(self, num_wheels):
+      pass
+
+class GroundVehicle(Vehicle):
+  pass
+  
+class Car(GroundVehicle):
+  pass
+  
+class Motorcycle(GroundVehicle):
+  pass
+
+class FlightVehicle(Vehicle):
+  pass
+
+class Airplane(FlightVehicle):
+  pass
+
+class Starship(FlightVehicle):
+  pass
+
+
+
+
+
+
+
+
+
