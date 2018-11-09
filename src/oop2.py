665bc970633447e7e62cd25548c2793f0da335bb
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-11-09 16:56:58 -0500

    Clean up

--- src/oop2.py
+++ src/oop2.py
@@ -4,7 +4,7 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
     def drive(self):
         print("vrooom")
@@ -20,18 +20,18 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it prints "BRAAAP!!"
 
 class Motorcycle(GroundVehicle):
-    def __init__(self):
-        GroundVehicle.__init__(self, 2)
+    def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels)
     def drive(self):
         print ("BRAAAP!!")
 
 # TODO
 
 vehicles = [
-    GroundVehicle(2),
-    GroundVehicle(4),
+    GroundVehicle(),
+    GroundVehicle(),
     Motorcycle(),
-    GroundVehicle(2),
+    GroundVehicle(),
     Motorcycle(),
 ]
 
