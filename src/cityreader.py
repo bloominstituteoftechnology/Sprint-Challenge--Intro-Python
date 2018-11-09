0252bfb0b85fb23ab7fa4004618a2f8966a704aa
Kevin Nguyen <kevinn858@gmail.com>
2018-11-09 10:37:58 -0800

    did stretch goal

--- src/cityreader.py
+++ src/cityreader.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
 
@@ -61,6 +62,46 @@ for city in cities:
 # Phoenix: (33.5722,-112.0891)
 # Tucson: (32.1558,-110.8777)
 # Salt Lake City: (40.7774,-111.9301)
+def get_point(num):
+    if num==1:
+        point = input('Enter a lat1,lon1: ')
+    elif num==2:
+        point = input('Enter a lat2,lon2: ')
+    else:
+        print('check() used incorrectly')
+        sys.exit(0)
+    lon = ''
+    lat = ''
+    found_comma = False
+    for char in point:
+        if char ==',':
+            found_comma = True
+            continue
+        elif not found_comma:
+            lon += char
+        elif found_comma:
+            if(char==' '):
+                continue
+            lat += char
+    if not found_comma:
+        print('Please enter two numbers separated by a comma.')
+        sys.exit(0)
+    else:
+        return (float(lon), float(lat))
 
-# TODO
+def normalize(point1, point2):
+    lat1, lon1 = point1
+    lat2, lon2 = point2
+    latMax = lat1 if lat1>lat2 else lat2
+    latMin = lat1 if latMax==lat2 else lat2
+    lonMax = lon1 if lon1>lon2 else lon2
+    lonMin = lon1 if lonMax==lon2 else lon2
+    return (latMax, latMin, lonMax, lonMin)
 
+point1 = get_point(1)
+point2 = get_point(2)
+points = normalize(point1, point2)
+latMax, latMin, lonMax, lonMin = points
+for city in cities:
+    if latMin<=float(city.latitude)<=latMax and lonMin<=float(city.longitude)<=lonMax:
+        print(f'{city.name}- lat:{city.latitude} lon:{city.longitude}')
