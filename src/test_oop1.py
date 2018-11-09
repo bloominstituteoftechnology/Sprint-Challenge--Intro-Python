dcf5a0135ddd8f8867c581c1b4a62a3e5b14835f
decagondev <decagondevelopment@gmail.com>
2018-11-09 18:14:18 +0000

    (POST STRETCH) : UNIT TEST : added a test to assert that the FlightVehicle class exists

--- src/test_oop1.py
+++ src/test_oop1.py
@@ -5,11 +5,20 @@ import oop1
 # used this as reference: https://docs.python.org/3/library/unittest.html#unittest.TestCase.debug
 class TestOop1(unittest.TestCase):
     def test_vehicle(self):
-        """ Does the class exist? """
+        """ Does the Vehicle class exist? """
         try:
             self.test = oop1.Vehicle()
             self.assertIsInstance(self.test, oop1.Vehicle)
-            print("test passed!")
+            print("\nVehicle Class Exists\n")
+        except NameError as e:
+            print(e)
+
+    def test_flight_vehicle(self):
+        """ Does the FlightVehicle class exist? """
+        try:
+            self.test = oop1.FlightVehicle()
+            self.assertIsInstance(self.test, oop1.FlightVehicle)
+            print("\nFlightVehicle Class Exists\n")
         except NameError as e:
             print(e)
 
