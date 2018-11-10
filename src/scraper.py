9be397224d33216961351cee759b71783b5389ab
decagondev <decagondevelopment@gmail.com>
2018-11-10 14:05:47 +0000

    (EXTRA) : tested craper against a games website leaderboard, built data from it to create a list of dictionaries holding place, username and xp

--- src/scraper.py
+++ src/scraper.py
@@ -18,10 +18,29 @@ def bs_scrape(ADDR):
     return soup
 
 
-print(scrape_emails("https://www.swfc.co.uk/club/club-contacts/"))
+# print(scrape_emails("https://www.swfc.co.uk/club/club-contacts/"))
 
 soup = bs_scrape("https://www.swfc.co.uk/club/club-contacts/")
 
 # grab the tags and print them
-for item in soup.find_all("a"):
-    print(item)
+#for item in soup.find_all("a"):
+#    print(item)
+
+# lets scrape the data from a game leaderboard now
+
+umg = bs_scrape("https://umggaming.com/leaderboards")
+
+leader = umg.find("table", {"id": "leaderboard-table"})
+
+tbody = leader.find("tbody")
+
+players = []
+
+for tr in tbody.find_all("tr"):
+    place = tr.find_all("td")[0].text.strip()
+    username = tr.find_all("td")[1].text.strip()
+    xp = tr.find_all("td")[3].text.strip()
+    # print(place, username, xp)
+    players.append({"place": place, "username": username, "xp": xp})
+
+print(players)
