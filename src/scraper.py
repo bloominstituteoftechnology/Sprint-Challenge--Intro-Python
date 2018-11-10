d719a7c0a9a19282dbdb16a87816407f650dfcb6
decagondev <decagondevelopment@gmail.com>
2018-11-10 13:01:35 +0000

    (EXTRA) : refactored web scraper to have a plain scraper and one that uses bs4

--- src/scraper.py
+++ src/scraper.py
@@ -1,10 +1,27 @@
 import requests
 import re
+from bs4 import BeautifulSoup
 
-# get the data
-data = requests.get('https://www.swfc.co.uk/club/club-contacts/')
 
-# extract all emails on a web page
-emails = re.findall(r'([\d\w\.]+@[\d\w\.\-]+\.\w+)', data.text)
+def scrape_emails(ADDR):
+    # get the data
+    data = requests.get(ADDR)
+    # extract all emails on a web page
+    emails = re.findall(r'([\d\w\.]+@[\d\w\.\-]+\.\w+)', data.text)
 
-print(emails)
+    return emails
+
+
+def bs_scrape(ADDR):
+    data = requests.get(ADDR)
+    soup = BeautifulSoup(data.text, "html.parser")
+    return soup
+
+
+print(scrape_emails("https://www.swfc.co.uk/club/club-contacts/"))
+
+soup = bs_scrape("https://www.swfc.co.uk/club/club-contacts/")
+
+# grab the tags and print them
+for item in soup.find_all("a"):
+    print(item)
