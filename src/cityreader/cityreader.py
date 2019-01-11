bb63c3ddc6987dbd120472f6dd3b34f37060fbc3
Andrew <ghobrial.andrew@gmail.com>
2019-01-11 12:43:20 -0500

    fixed cityreader.py errors

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,13 +1,12 @@
 import csv
-
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
 class City:
     # Initializer / Instance Attributes
-    def __init__(self, name, latitude, longitude):
+    def __init__(self, name, lat, lon):
         self.name = name
-        self.latitude = latitude
-        self.longitude = longitude
+        self.lat = lat
+        self.lon = lon
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -27,14 +26,12 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-  with open(r'''C:\Users\ghobr\OneDrive\Software Dev\Lambda\Sprint-Challenge--Intro-Python\src\cityreader\cities.csv''') as csvfile:
-    cityreader = csv.reader(csvfile)
-    for row in cityreader:
-      # print(row[0])
-      # print(row[3])
-      # print(row[4])
-      newCity = City(row[0], row[3], row[4])
-      print("new city instance: {newCity}")
+  with open(r'''C:\Users\ghobr\OneDrive\Software Dev\Lambda\Sprint-Challenge--Intro-Python\src\cityreader\cities.csv''', newline='') as csvfile:
+    cityreader = csv.reader(csvfile, delimiter=',')
+    for index, row in enumerate(cityreader):
+      if index == 0:
+        continue
+      newCity = City(row[0], float(row[3]), float(row[4]))
       cities.append(newCity)
   return cities
 
@@ -42,7 +39,7 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(f'{c.name}, {c.lat}, {c.lon}')
 
 # STRETCH GOAL!
 #
