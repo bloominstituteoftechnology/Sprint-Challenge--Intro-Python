366410b2fc417deb4ccdb75530333515db5b0a33
Kevin <brackkevin@gmail.com>
2019-01-11 10:15:55 -0600

    Vehicle class inheritence exercise complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -20,3 +20,30 @@
 
 
 # VEHICLE IS THE BASE CLASS
+class Vehicle:
+    def __init__(self):
+        pass
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        pass
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        pass
+
+class Car(GroundVehicle):
+    def __init__(self):
+        pass
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        pass
