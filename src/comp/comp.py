a3b9373896577f68355cb7d9694df9c4929f3b80
Katie Cleary <katiecleary@airmaster.lan>
2019-01-11 11:58:19 -0500

    completed half of comp

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,24 +24,24 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [h.name for h in humans if h.name[0]=="D"]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [h.name for h in humans if h.name[-1]=="e"]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [h.name for h in humans if ord(h.name[0]) in range(ord('C'), ord('G')+1)]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [h.age +10 for h in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
