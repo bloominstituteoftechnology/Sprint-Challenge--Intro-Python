bb2c5b724f523d70ab9a3d832252407936b483ea
Farhan Farooqui <farhan@farooqui.net>
2019-01-11 11:40:30 -0500

    OOP 1 Done and Passed

---
+++ src/oop/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+    class GroundVehicle(Vehicle):
+        def __init__(self):
+            super().__init__()
+
+    class Car(GroundVehicle):
+        def __init__(self):
+            super().__init__()
+
+    class Bike(GroundVehicle):
+        super().__init__()
