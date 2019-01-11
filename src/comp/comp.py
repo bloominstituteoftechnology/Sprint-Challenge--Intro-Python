bf1d2cd146aa969a8f7bdca473f4d39c8643550e
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-01-11 13:00:07 -0500

    finished comp and fixed errors

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -28,19 +28,19 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = [name for name in humans if name.name[0] == "D"]
+a = [name.name for name in humans if name.name[0] == "D"]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = [name for name in humans if name.name[-1] == "e"]
+b = [name.name for name in humans if name.name[-1] == "e"]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = [name for name in humans if name.name[0] == "C" or name.name[0] ==
+c = [name.name for name in humans if name.name[0] == "C" or name.name[0] ==
      "D" or name.name[0] == "E" or name.name[0] == "F" or name.name[0] == "G"]
 print(c)
 
@@ -59,18 +59,18 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = tuple([(human.name, human.age)
-           for human in humans if human.age >= 27 and human.age <= 32])
+f = [(human.name, human.age)
+     for human in humans if human.age >= 27 and human.age <= 32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names capitalized and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names capitalized:")
-g = []
+g = [Human(human.name.upper(), human.age + 5) for human in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
-h = []
+h = [math.sqrt(human.age) for human in humans]
 print(h)
