914bc6e0458ecbeab645cbfd6cb83e1e43a5dc2a
michaelagard <mdagard@gmail.com>
2019-01-11 11:27:04 -0500

    fix copy error

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -18,26 +18,6 @@
 #
 # Put a comment noting which class is the base class
 
-# Write classes for the following class hierarchy:
-#
-#  [Vehicle]->[FlightVehicle]->[Starship]
-#      |                |
-#      v                v
-# [GroundVehicle]      [Airplane]
-#   |       |
-#   v       v
-# [Car]  [Motorcycle]
-#
-# Each class can simply "pass" for its body. The exercise is about setting up
-# the hierarchy.
-#
-# e.g.
-#
-# class Whatever:
-#     pass
-#
-# Put a comment noting which class is the base class
-
 # Base class
 
 
