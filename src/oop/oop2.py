fdd31d20ff89c38ed3c69c5de3727a2c13662ed1
Kat Fries <katherynejf@gmail.com>
2019-01-11 12:07:50 -0500

    oop2 done

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,23 +1,22 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
-#
-# Also change it so the num_wheels defaults to 4 if not specified when the
-# object is constructed.
-
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
+    
+    def __repr__(self):
+        return '{} has {} wheels.'.format(self, self.num_wheels)
 
-    # TODO
+    def drive(self):
+        return 'vrooom'
 
-
-# Subclass Motorcycle from GroundVehicle.
-#
-# Make it so when you instantiate a Motorcycle, it automatically sets the number
-# of wheels to 2 by passing that to the constructor of its superclass.
-#
-# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
-
-# TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
+    
+    def __repr__(self):
+        return '{} has {} wheels.'.format(self, self.num_wheels)
+    
+    def drive(self):
+        return 'BRAAAP!!'
 
 vehicles = [
     GroundVehicle(),
@@ -27,6 +26,21 @@ vehicles = [
     Motorcycle(),
 ]
 
+driven = [el.drive() for el in vehicles]
+print(driven)
+
+# To the GroundVehicle class, add method drive() that returns "vroooom".
+#
+# Also change it so the num_wheels defaults to 4 if not specified when the
+# object is constructed.
+
+# Subclass Motorcycle from GroundVehicle.
+#
+# Make it so when you instantiate a Motorcycle, it automatically sets the number
+# of wheels to 2 by passing that to the constructor of its superclass.
+#
+# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+
