60ff45ff0d69ab19f61a0bbcbe5ed5caa35775c7
lndubose <lndubose13@gmail.com>
2019-01-11 10:48:08 -0600

    One list comprehension left
    The last comprehension I am stuck on is giving me some trouble. Everything else is
    finsihed and passing for the tests.

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -51,25 +51,26 @@ print(d)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f'{human.name}-{human.age}' for human in humans]
 print(e)
-print("^here")
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [tuple([human.name, human.age])
+     for human in humans if human.age in range(27, 33)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names capitalized and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names capitalized:")
-g = []
+g = [human for human in humans]
 print(g)
-
+print(humans)
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
-h = []
+h = [math.sqrt(human.age) for human in humans]
 print(h)
+print("^here")
