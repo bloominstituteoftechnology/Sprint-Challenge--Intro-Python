51280b1502b642c4afdb9c08150e015b378007db
brandongrych <brandongrych@gmail.com>
2019-01-11 11:08:19 -0600

    Oop1 completed

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,23 +1,32 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
+# To the GroundVehicle class, add method drive() that prints "vroooom".
 #
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
-        self.num_wheels = num_wheels
+    def __init__(self, num_wheels=None):
+        self.num_wheels = 4 if num_wheels is None else num_wheels
 
     # TODO
-
+    def drive(self):
+        print('vroooom')
 
 # Subclass Motorcycle from GroundVehicle.
 #
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
-# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+# Override the drive() method in Motorcycle so that it prints "BRAAAP!!"
 
 # TODO
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(num_wheels=2)
+
+    def drive(self):
+        print('BRAAAP!!')
+
 
 vehicles = [
     GroundVehicle(),
@@ -27,6 +36,9 @@ vehicles = [
     Motorcycle(),
 ]
 
-# Go through the vehicles list and print the result of calling drive() on each.
+# Go through the vehicles list and call drive() on each.
 
 # TODO
+
+for v in vehicles:
+    v.drive()
