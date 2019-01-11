2ce602b83c19f2b9a7a8be72beef4c2a8e849ee9
Kevin <brackkevin@gmail.com>
2019-01-11 13:07:37 -0600

    printed list formatted to match example, tests will not pass

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -71,7 +71,8 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c.name, c.lat, c.lon)
+    # print(c.name, c.lat, c.lon)
+    print(f'({c.name}, {c.lat}, {c.lon})')
 
 # STRETCH GOAL!
 #
