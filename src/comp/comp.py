355fa99d223a32f865aff5d8d227132bd41e181b
Drew <drewsup123c@gmail.com>
2019-01-11 10:28:06 -0700

    comp.py passes all tests except 1

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -65,7 +65,8 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = [(item.name, item.age) for item in humans]
+f = [(item.name, item.age) for item in humans if item.age>=27 and item.age<=32]
+# figure out more efficient way later
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
