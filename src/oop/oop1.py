f23b522fde79220ee2821db7a90e01207572d026
Conner Alegre <cga0@pm.me>
2019-01-11 09:09:56 -0700

    Add Starship, Airplane, GroundVehicle, Car, Motorcycle classes

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -23,3 +23,17 @@ class Vehicle:
 class FlightVehicle(Vehicle):
     pass
 
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
