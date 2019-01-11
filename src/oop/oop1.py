669f1f328a58ea0ff3d71df1f39414c075a5280e
Linh Bouniol <paulalinh89@gmail.com>
2019-01-11 08:24:02 -0800

    Set up classes for oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -18,4 +18,23 @@
 #
 # Put a comment noting which class is the base class
 
- 
+class Vehicle:
+	pass
+	
+class FlightVehicle(Vehicle):
+	pass
+	
+class Starship(FlightVehicle):
+	pass
+	
+class GroundVehicle(Vehicle):
+	pass
+	
+class Car(GroundVehicle):
+	pass
+	
+class Motorcycle(GroundVehicle):
+	pass
+	
+class Airplane(FlightVehicle):
+	pass
