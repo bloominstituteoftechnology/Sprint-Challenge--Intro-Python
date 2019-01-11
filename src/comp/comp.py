7f91555c012d41379dff2ec857a64c577ffd1ee0
Drew <drewsup123c@gmail.com>
2019-01-11 10:17:20 -0700

    finished comp.p

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -25,36 +25,47 @@ humans = [
 # whose name starts with 'D':
 print("Starts with D:")
 a = []
+# a.append(i) for i in humans if i.name[0] == 'D'
+for i in humans:
+    if i.name[0] == 'D':
+        a.append(i)
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
 b = []
+for i in humans:
+    if i.name[len(i.name) - 1] == 'e':
+        b.append(i)
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
 c = []
+betcg = ['C', 'D', 'E', 'F', 'G']
+for i in humans:
+    if any(string in i.name[0] for string in betcg):
+        c.append(i)
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [item.age+10 for item in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f'{item.name}-{item.age}' for item in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(item.name, item.age) for item in humans]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
@@ -62,10 +73,14 @@ print(f)
 # The "humans" list should be unmodified.
 print("All names capitalized:")
 g = []
+# item.name.upper() for item in humans
+for item in humans:
+    g.append(item.name.upper())
+    g.append(item.age+5)
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [item.age**2 for item in humans]
 print(h)
