2e7adff034eeb4a9dd1502e3ac1c782f737f0e53
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-01-11 13:19:42 -0500

    basic functionality implemented, working through a bug when calling a new City() instance

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -23,13 +23,21 @@ print(testCity)
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
+
 cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    with open('cities.csv') as csv_file:
+      csv_reader = csv.reader(csv_file, delimiter = ',')
+      line_count = 0
+      for row in csv_reader:
+        row = City()
+        cities.append(row)
+        line_count += 1
     return cities
 
 cityreader(cities)
