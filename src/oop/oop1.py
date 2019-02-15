bf6f2f01ea287136c0b39fec4ea61d38c084a02c
TRIF3X <thewizardofcrypto@gmail.com>
2019-02-15 10:03:24 -0600

    init commit

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,19 +1,5 @@
-# Write classes for the following class hierarchy:
-#
-#  [Vehicle]->[FlightVehicle]->[Starship]
-#      |                |
-#      v                v
-# [GroundVehicle]      [Airplane]
-#   |       |
-#   v       v
-# [Car]  [Motorcycle]
-#
-# Each class can simply "pass" for its body. The exercise is about setting up
-# the hierarchy.
-#
-# e.g.
-#
-# class Whatever:
-#     pass
-#
-# Put a comment noting which class is the base class
+# Base class
+class Vehicle:
+    def __init__(self):
+        pass
+
