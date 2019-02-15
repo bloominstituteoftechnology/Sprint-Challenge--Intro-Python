497478a039005b9cdca11d94fb5b8e81961b3dea
TRIF3X <thewizardofcrypto@gmail.com>
2019-02-15 10:12:23 -0600

    oop1.py done

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -3,3 +3,32 @@ class Vehicle:
     def __init__(self):
         pass
 
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
