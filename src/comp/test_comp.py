0c39f904e3505add1cb183d7e5f248ecc0ba24aa
Brady <Bradyfukumoto@gmail.com>
2019-04-26 13:05:12 -0400

    Update test_comp.py

--- src/comp/test_comp.py
+++ src/comp/test_comp.py
@@ -30,7 +30,7 @@ class CompTests(unittest.TestCase):
   def test_names_ages_between_27_and_32(self):
     self.assertEqual(f, [('Alice', 29), ('Bob', 32), ('Daphne', 30), ('David', 31)])
 
-  def test_all_names_capitalized(self):
+  def test_all_names_uppercase(self):
     expected = [Human("ALICE", 34), Human("BOB", 37), Human("CHARLIE", 42), Human("DAPHNE", 35), Human("EVE", 31), Human("FRANK", 23), Human("GLENN", 47), Human("HARRISON", 17), Human("IGON", 46), Human("DAVID", 36)]
     self.assertTrue(compare_humans(g, expected))
 
