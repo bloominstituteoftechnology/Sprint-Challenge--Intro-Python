35e649af7f830e670489341bd23f0533cff74bd5
audreywelch <welch.audreyl@gmail.com>
2019-05-03 12:08:28 -0500

    Completed cityreader.py challenge

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,15 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
 
+class City:
+  
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __repr__(self):
+    return f'{self.name}, {self.lat},{self.lon}'
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -14,13 +23,26 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
+
 cities = []
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+
+  # Implement the functionality to read from the 'cities.csv' file
+  with open('/Users/audreywelch/Dropbox/Lambda School/Computer_Science/Sprint_01_Python/Sprint_Challenge/Sprint-Challenge--Intro-Python/src/cityreader/cities.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    line_count = 0
+
+    # For each city record, create a new City instance and add it to the 
+    # `cities` list
+    for row in csv_reader:
+      if line_count == 0:
+        line_count += 1
+      else:
+        cities.append(City(str(row[0]), float(row[3]), float(row[4])))
+        line_count += 1
+
     return cities
 
 cityreader(cities)
