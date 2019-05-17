fe792edff097e97cb395d6129afdedefb79a7a17
cocoitali <cmburatto@gmail.com>
2019-05-16 22:37:26 -0600

    Pip update, oop2.py: set default value for num_wheels

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,14 +4,18 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def __repr__(self):
+        return f"This vehicle has {self.num_wheels}"
+    
+   
 
 
 # Subclass Motorcycle from GroundVehicle.
-#
+
+
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
