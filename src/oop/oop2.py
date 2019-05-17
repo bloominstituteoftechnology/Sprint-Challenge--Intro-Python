a2504819a5f28da1800cdcb154abb7ddc74ab8bd
cocoitali <cmburatto@gmail.com>
2019-05-16 23:05:38 -0600

    oop2.py : iterate through list of vehicles

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -8,7 +8,7 @@ class GroundVehicle():
         self.num_wheels = num_wheels
 
     def __repr__(self):
-        return f"This vehicle has {self.num_wheels}"
+        return f"This vehicle has {self.num_wheels} wheels"
     
     def drive(self):
         return "vroooom"
@@ -36,5 +36,5 @@ vehicles = [
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
-
-# TODO
+for i in vehicles :
+    print (i)
