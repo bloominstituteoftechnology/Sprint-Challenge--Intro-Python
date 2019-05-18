940b897d23028dd00ecc45f0f190b98532bd59b1
Jess Harrison <jessdev83@gmail.com>
2019-05-18 13:36:06 -0400

    oop1.py complete and all tests passing

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,8 +3,9 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
     # TODO
