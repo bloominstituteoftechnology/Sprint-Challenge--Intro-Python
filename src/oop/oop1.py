1fcd886673c53fcbadf6029a5c4a657af514dd72
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-05-18 11:33:49 -0500

    initial commit, finished oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,29 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle: 
+    pass
+    #base class
+
+class GroundVehicle(Vehicle):
+    pass 
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class FlightVehicle(Vehicle): 
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+
+
+
