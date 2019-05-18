cd8e2b227a1023672927ab718ffc15537cc880d5
willieino <willieino@hotmail.com>
2019-05-18 09:21:55 -0700

    begin sprint

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,18 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    def __init__(self):
+       pass
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+          
+
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+
+class 
+
