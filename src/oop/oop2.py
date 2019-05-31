0359605345521d3a31f2414ca2664560290e13d2
Mark Oliver <mark.c.oliver@gmail.com>
2019-05-31 11:22:38 -0400

    comp oop1 untested

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,11 +4,18 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return "vroooom"
 
+class Motorcycle(GroundVehicle):
+    def __init___(self, num_wheels=2):
+        super().__init__(num_wheels)
+
+    def drive(self):
+        return "BRAAAP!!"
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -29,4 +36,6 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for v in vehicles:
+    print(v.drive())
+
