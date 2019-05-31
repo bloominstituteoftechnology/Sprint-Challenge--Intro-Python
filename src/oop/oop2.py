0d153965d2c0c685084f28a97659af74e9b3bd60
ed.chin.git <edgardochin@gmail.com>
2019-05-31 11:12:05 -0500

    OOP challenge completed

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,11 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return "vroooom"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -16,8 +17,14 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+class Motorcycle(GroundVehicle):
+    def __init__(num_wheels):
+        super().__init__(num_wheels=2)
+    
+    def drive(self):
+        return "BRAAAP!!"
 
-# TODO
+
 
 vehicles = [
     GroundVehicle(),
@@ -29,4 +36,5 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for v in vehicles:
+    print(v.drive())
