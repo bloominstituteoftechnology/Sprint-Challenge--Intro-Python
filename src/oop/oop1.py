cb48ac95db1917c0825bdeafac9d49b4d33af081
RMH <rory.hennessey@gmail.com>
2019-05-31 11:13:06 -0400

    oop1 complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -19,3 +19,22 @@
 # Put a comment noting which class is the base class
 
 class Vehicle: # base
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
