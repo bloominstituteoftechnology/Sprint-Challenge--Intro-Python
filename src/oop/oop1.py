4b91dd413d36a2942734a9f787cb0ad28f584a1e
griffinshuth <griffinshuth@gmail.com>
2019-05-31 19:49:42 -0600

    finished comp.py and oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,24 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class vehicle:
+    pass
+    # No Base Class
+class FlightVehicle(vehicle):
+    pass
+    # Vehicle is Base Class
+class Starship(FlightVehicle):
+    pass
+    # FlightVehicle is Base Class
+class GroundVehicle(vehicle):
+    pass
+    # Vehicle is Base Class
+class Airplane(FlightVehicle):
+    pass
+    # FlightVehicle is Base Class
+class Car(GroundVehicle):
+    pass
+    # GroundVehicle is Base Class
+class Motorcycle(GroundVehicle):
+    pass
+      # GroundVehicle is Base Class
