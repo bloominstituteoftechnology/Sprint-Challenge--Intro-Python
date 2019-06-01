be8014832f48c3d2180d195a349f4368216496ee
FreeKeyBoy <jjhutton81@gmail.com>
2019-06-01 14:18:53 -0700

    Completed cityreader.py. MVP complete.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __repr__(self):
+        return f"{(self.name, self.lat, self.lon)}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -16,11 +26,43 @@
 # should not be loaded into a City object.
 cities = []
 
+str_to_int_dict = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+}
+
+
+def str_to_int(int_str):
+    is_negative = False
+    if "-" in int_str:
+        is_negative = True
+    int_str_list = int_str.split("")
+    int_list = [str_to_int_dict[str] for str in int_str_list]
+    int = "".join(int_list)
+    if is_negative == True:
+        int *= -1
+    return int / 10000
+
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    with open('cities.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[1] == "state_name":
+                print("true")
+                pass
+        else:
+            cities.append(City(row[0], float(row[3]), float(row[4])))
     return cities
 
 cityreader(cities)
