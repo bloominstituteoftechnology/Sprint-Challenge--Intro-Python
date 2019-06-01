35342f06aac1298e9571d3e9e0d63a5e9350340b
Denise Escobar <descobar1490@gmail.com>
2019-06-01 13:59:59 -0500

    Creating list of cities but not passing test for cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,7 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City():
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
 
+    def __repr__(self):
+      output =""
+      output += self.name + "," + self.lat +"," + self.lon 
+      return output
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -20,7 +29,15 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    import csv 
+    with open ('cities.csv') as cities_csv:
+        csv_reader = csv.reader(cities_csv, delimiter = ',')
+        line_count=0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count+=1
+            else :
+                cities.append(City(row[0], row[3], row[4]))
     return cities
 
 cityreader(cities)
