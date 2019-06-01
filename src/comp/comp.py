ecc2e2a0e8376e3eabd86880330f9dad807d4f23
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-01 13:35:46 -0400

    Adding comp.

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -36,35 +36,35 @@ b = [name for name in humans if name.name[len(name.name) -1 ] == 'e']
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
 c = [name for name in humans if name.name[0] >= 'C' and name.name[0] <= 'G']
-print('c:', c)
+# print('c:', c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
-print(d)
+d = [age.age + 10 for age in humans]
+# print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
-print(e)
+e = [name.name + '-' + str(name.age) for name in humans]
+# print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
-print(f)
+f = [(human.name, human.age) for human in humans if human.age <= 32 and human.age >= 27]
+# print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [[human.name.upper(), human.age + 5] for human in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(human.age) for human in humans]
 print(h)
