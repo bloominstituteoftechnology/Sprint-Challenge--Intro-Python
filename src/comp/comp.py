06cee26a9a39008a727d194a99dc47c49b6f1eee
Denise Escobar <descobar1490@gmail.com>
2019-06-01 12:32:55 -0500

    fixed last error with comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -79,7 +79,7 @@ print(f)
 print("All names uppercase:")
 g = []
 for i in humans:
-    g.append((i.name.upper(), i.age+5))
+    g.append(Human(i.name.upper(), i.age+5))
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
