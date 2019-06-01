727965b08905df839e9c105251559b775a3266de
FreeKeyBoy <jjhutton81@gmail.com>
2019-06-01 13:04:33 -0700

    Passed first two tests in oop files.

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -8,6 +8,8 @@ class GroundVehicle():
         self.num_wheels = num_wheels
 
     # TODO
+    def drive(self):
+        return "Vroom"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -18,6 +20,13 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
+
+    def drive(self):
+        return "BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
@@ -30,3 +39,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for vehicle in vehicles:
+    print(f"{vehicle.drive()}")
