66b0304bdda4fedcc5b25c444937294e05a11e28
Chris Roth <c.roth.lambda@gmail.com>
2019-06-02 22:04:25 -0500

    completed MVP for Python Into Sprint: starting on city stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -14,14 +14,34 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
+class City():
+
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return  f'{self.name}, {self.lat}, {self.lon}'.format(self = self)
+
 cities = []
 
-def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+def cityreader(cities=[]):
+        with open('cities.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter = ',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                   cities.append(City((row[0]), float(row[3]), float(row[4])))
+                   line_count +=1
+
+        return cities
 
 cityreader(cities)
 
