2ca1d80840462724e387e97861e22b4a4a68349c
Jennifer King <jenbellpgh@gmail.com>
2019-06-02 22:05:41 -0400

    Added __str__ to City

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,12 +1,15 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
-
+import csv
 class City():
 
   def __init__(self, name, lat, lon):
     self.name = name
     self.lat = lat
     self.lon = lon
+
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -26,7 +29,11 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    with open('cities.csv', newline='') as csvfile:
+      lines = csv.DictReader(csvfile)
+      for row in lines:
+        cities.append(City(row['city'], float(row['lat']), float(row['lng'])))
+        
     return cities
 
 cityreader(cities)
