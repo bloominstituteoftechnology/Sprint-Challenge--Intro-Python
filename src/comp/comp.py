a73274580ab043049991d7ff843dce7332089f0b
vy3191 <venkyafhs@gmail.com>
2019-06-03 15:35:51 -0400

    Completed comp.py assignments

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -50,25 +50,26 @@ print(d)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [obj.name+"-"+str(obj.age) for obj in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(obj.name,obj.age) for obj in humans if obj.age >=27 and obj.age <=32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [Human(obj.name.upper(), obj.age+5) for obj in humans]
+
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(obj.age) for obj in humans]
 print(h)
