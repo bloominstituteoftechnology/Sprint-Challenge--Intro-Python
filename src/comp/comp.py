de78c902b29667143055d0dd7210d26dee834976
Frulwinn <frulwinn@gmail.com>
2019-06-14 11:03:41 -0500

    next 3 list comprehensions in comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -40,22 +40,22 @@ c = [human.name for human in humans if human.name[0] == "C" or human.name[0] == 
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
-# print("Ages plus 10:")
-# d = []
-# print(d)
+print("Ages plus 10:")
+d = [human.age + 10 for human in humans]
+print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
-# print("Name hyphen age:")
-# e = []
-# print(e)
+print("Name hyphen age:")
+e = [f'{human.name}-{human.age}'for human in humans]
+print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
-# print("Names and ages between 27 and 32:")
-# f = []
-# print(f)
+print("Names and ages between 27 and 32:")
+f = [(human.name, human.age) for human in humans if human.age >= 27 and human.age <= 32]
+print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
