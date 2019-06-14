fafe63fb00fffc87890378426cdd9fac6201ab41
nateyoungren <nathanaelyoungren@gmail.com>
2019-06-14 10:13:21 -0700

    Fix spelling in Motorcyle class

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,14 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -16,12 +24,21 @@
 # should not be loaded into a City object.
 cities = []
 
+import csv
+
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+  file = open('cities.csv', 'r')
+  reader = csv.reader(file)
+  next(reader)
+  for row in reader:
+    new_city = City(row[0], row[3], row[4])
+    cities.append(new_city)
+
+  file.close()
+  return cities
 
 cityreader(cities)
 
