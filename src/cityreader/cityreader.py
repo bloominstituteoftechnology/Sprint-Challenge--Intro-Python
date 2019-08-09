43e06aa66f8c84f0b0110f4e7bfa428f9debd8f1
jkaddu <jkaddu@yahoo.com>
2019-08-09 08:56:15 -0700

    import added

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -19,6 +19,8 @@ class City:
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
+
 cities = []
 
 def cityreader(cities=[]):
