d279b1f89a0848e95920ecd1d5cd7df828206315
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2019-08-17 14:29:12 -0400

    This commit should satisfy all requirements of comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -26,7 +26,7 @@ humans = [
 ]
 
 # Write a list comprehension that creates a list of names of everyone
-# whose name starts with 'D':
+# whose name starts with "D":
 print("Starts with D:")
 a = [human.name for human in humans if human.name[0] == "D"]
 
@@ -45,9 +45,10 @@ b = [human.name for human in humans if human.name[-1] == "e"]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
-# whose name starts with any letter between 'C' and 'G' inclusive.
+# whose name starts with any letter between "C" and "G" inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [human.name for human in humans if (
+    human.name[0] >= "C" and human.name[0] <= "G")]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
@@ -58,21 +59,23 @@ print(d)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = [human.name + '-' + str(human.age) for human in humans]
+e = [human.name + "-" + str(human.age) for human in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(human.name, human.age)
+     for human in humans if (human.age >= 27 and human.age <= 32)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [(human.name.upper(), human.age + 5) for human in humans]
+g = [Human(human.name.upper(), human.age + 5) for human in humans]
+# Had to correct `AttributeError: 'tuple' object has no attribute 'name'`
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
