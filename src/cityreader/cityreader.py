171b63fa87ed1bc29abc3a13ef7994156ae0b52e
jrizza88 <jetorres88@gmail.com>
2019-08-18 23:38:51 -0400

    completed MVP

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,13 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -14,20 +21,33 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv 
+
+
+
 cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  with open('cities.csv', newline='') as csvfile:
+    readCSV = csv.reader(csvfile)
+    for row in readCSV:
+      if row[0] != 'city':
+        city = row[0]
+        lat = row[3]
+        lon = row[4]
+        city_list = City(city, float(lat), float(lon))
+        cities.append(city_list)
+
     return cities
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(c.name, c.lat, c.lon)
 
 # STRETCH GOAL!
 #
--- src/comp/comp.py
+++ src/comp/comp.py
@@ -63,10 +63,8 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-# z = [f"{u.name.upper()} {u.age + 5}" for u in humans]
 g = [Human(u.name.upper(), u.age + 5) for u in humans]
 print(g)
-# print(z)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
