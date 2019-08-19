da8aa6a5da92c23a4f633c684c706f98d7229283
david-chua <dp_chua@hotmail.com>
2019-08-18 21:08:19 -0400

    OOP1 and OOP2 test passing

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,9 +4,11 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
+    def drive(self):
+      return 'vroooom'
     # TODO
 
 
@@ -18,6 +20,12 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        self.num_wheels = 2
+    def drive(self):
+        return "BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
@@ -30,3 +38,5 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for type in vehicles:
+    print(type.drive())
