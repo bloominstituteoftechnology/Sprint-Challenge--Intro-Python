5801b68b3bedc0d497e3a4b1e94191b95f9d7f98
Bao Pham <ryanvuepham@gmail.com>
2019-08-19 21:25:39 -0500

    tested testcityreader, passed

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,17 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
+
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f'{self.name}, {self.lat}, {self.lon}'
+
+
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -20,7 +32,15 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  with open('cities.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    line = 0
+    for row in csv_reader:
+      if line == 0:
+        line += 1
+      else:
+        cities.append(City(row[0], float(row[3]), float(row[4])))
+
     return cities
 
 cityreader(cities)
@@ -67,5 +87,6 @@ def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # TODO Ensure that the lat and lon valuse are all floats
   # Go through each city and check to see if it falls within 
   # the specified coordinates.
+
 
   return within
