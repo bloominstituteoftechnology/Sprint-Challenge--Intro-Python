5ba9665d9005d3336e885fbf9c0a7619f5c0b8b0
Witanday <witanday.cd@gmail.com>
2019-08-20 16:15:22 -0400

    Completed OOP1.PY CLASS HIERARCHY

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,28 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle():
+    pass
+class FlightVehicle(Vehicle):
+    # we pass Vehicle Class as direct base class
+    pass
+
+class Starship(FlightVehicle):
+     # we pass FlightVehicle Class as direct parent class
+    pass
+
+class Airplane(FlightVehicle):
+     # we pass FlightVehicle Class as direct parent class
+    pass
+
+class GroundVehicle(Vehicle):
+    # we pass Vehicle Class as direct base class
+    pass
+
+class Car(GroundVehicle):
+     # we pass GroundVehicle Class as direct parent class
+    pass
+
+class Motorcycle(GroundVehicle):
+     # we pass GroundVehicle Class as direct parent class
+    pass
