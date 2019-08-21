a932f5309df90c544a87461ea0c7515eb48043e4
Witanday <witanday.cd@gmail.com>
2019-08-20 20:08:01 -0400

    Completed Sprint Minimum Requirement

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -7,7 +7,7 @@ class GroundVehicle():
     def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
     def drive(self):
-        print('vroooom')
+        return 'vroooom'
 
     # TODO
 
@@ -25,7 +25,7 @@ class Motorcycle(GroundVehicle):
        self.num_wheels=num_wheels
 
     def drive(self):
-        print('BRAAAP!!')
+        return 'BRAAAP!!'
 
 print(Motorcycle().num_wheels)
 
