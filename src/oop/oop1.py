e0ccca210c0ee720bcf1b8eceab09b818dd58a91
ThaDocIzN <rodan72@msn.com>
2019-09-06 10:27:22 -0500

    oop1.py: class hierarchies

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,37 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class is Vehicle class
+
+
+class Vehicle:
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
+
+
+
+
