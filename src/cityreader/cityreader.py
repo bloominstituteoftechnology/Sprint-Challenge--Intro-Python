0d7792262cf13568750c3f5420cac0adfe9bbf93
[your <leigh.tm@gmail.com>
2019-09-06 14:07:31 -0400

    first push still errors

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,7 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+        def __init__(self, name, lat,lon):
+              self.name = name
+              self.lat = lat
+              self.lon = lon
 
+        def __repr__(self):
+              return f'{self.name}: ({self.lat}, {self.lon})'
+
+              
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -17,6 +26,22 @@
 cities = []
 
 def cityreader(cities=[]):
+    
+    with open('cities.csv') as f:
+        data = csv.DictReader(f)
+        for row in data:
+            cities.append(City(row['city'], float(
+                row['lat']), float(row['lng'])))
+
+    return cities
+
+
+cityreader(cities)
+
+for c in cities:
+    print(c)
+
+
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
