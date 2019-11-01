04619a5a88b15f163a0057e6bf5bacf657269247
ndoshi83 <49622764+ndoshi83@users.noreply.github.com>
2019-11-01 14:38:32 -0700

    Fixed CityReader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -31,7 +31,7 @@ def cityreader(cities=[]):
     reader = csv.reader(f)
     next(reader)
     for row in reader:
-      cities.append(City(row[0], row[3], row[4]))
+      cities.append(City(row[0], float(row[3]), float(row[4])))
     return cities
 
 cityreader(cities)
