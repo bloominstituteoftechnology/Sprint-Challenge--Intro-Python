53eabd0d8c13009159da645f17fae9b926461504
Michael Harris <mhguitar1997@gmail.com>
2019-11-01 08:53:03 -0700

    List comp names and ages

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -44,11 +44,11 @@ print("Ages plus 10:")
 d = [person.age + 10 for person in humans]
 print(d)
 
-# # Write a list comprehension that creates a list of strings which are the name
-# # joined to the age with a hyphen, for example "David-31", for all humans.
-# print("Name hyphen age:")
-# e = []
-# print(e)
+# Write a list comprehension that creates a list of strings which are the name
+# joined to the age with a hyphen, for example "David-31", for all humans.
+print("Name hyphen age:")
+e = [f"{person.name}-{person.age}" for person in humans]
+print(e)
 
 # # Write a list comprehension that creates a list of tuples containing name and
 # # age, for example ("David", 31), for everyone between the ages of 27 and 32,
