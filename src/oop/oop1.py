69c2ca6e1a650864be482a9ee5d45b31f549c83d
Christopher Tolbert <cmtolbert1@gmail.com>
2019-11-01 08:59:40 -0700

    Added oop1 files

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# base class
+class Vehicle:
+    pass
+
+#Vehicle based
+class FlightVehicle(Vehicle):
+    pass
+
+#FlightVehicle based
+class Starship(FlightVehicle):
+    pass
+
+#Vehicle based
+class GroundVehicle(Vehicle):
+    pass
+
+#FlightVehicle based
+class Airplane(FlightVehicle):
+    pass
+
+#GroundVehicle based
+class Car(GroundVehicle):
+    pass
+
+#Groundvehicle based
+class Motorcycle(GroundVehicle):
+    pass
