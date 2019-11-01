a388edb54710981850d9e15b819156ee1d11246b
Jason Aviles <sandman2k18@outlook.com>
2019-11-01 13:44:33 -0400

    finnshed reader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,10 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
-
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,14 +24,20 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  csvfile = open('cities.csv', 'r') #file and read only
+  reader = csv.reader(csvfile) #create reader
+  next(reader) #this is the iterator 
+  
+  for row in reader:
+    city = City(row[0], float(row[3]), float(row[4])) #create new city
+    cities.append(city)
     return cities
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(c.name, c.lat, c.lon)
 
 # STRETCH GOAL!
 #
