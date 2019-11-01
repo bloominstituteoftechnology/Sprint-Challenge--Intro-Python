904de24012992cbfe3c889a74a7901cc4cb8a7db
Michael Harris <mhguitar1997@gmail.com>
2019-11-01 08:17:16 -0700

    Create subclass motorcycle

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -7,7 +7,7 @@ class GroundVehicle():
     def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
-    def drive():
+    def drive(self):
         return "vroooom"
 
 
@@ -19,6 +19,12 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels)
+
+    def drive(self):
+        return "BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
