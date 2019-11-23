ca2af9c0f37ac64bd67b14597dda276702787cae
Reyaad Rafeek <reyaad510@gmail.com>
2019-11-23 12:18:04 -0500

    Completed oop1.py questions

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,48 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+
+# base class is Vehicle
+class FlightVehicle(Vehicle):
+    def __init__(self, name, description, airtime):
+        super().__init__(name, description)
+        self.airtime = airtime
+
+# base class is FlightVehicle
+
+
+class Starship(FlightVehicle):
+    def __init__(self, name, description, airtime, model):
+        super().__init__(name, description, airtime)
+        self.model = model
+
+# base class is Vehicle
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self, name, description, tires):
+        super().__init__(name, description)
+        self.tires = tires
+
+# base class is GroundVehicle
+
+
+class Car(GroundVehicle):
+    def __init__(self, name, description, tires, model):
+        super().__init__(name, description, tires)
+        self.model = model
+
+# base class is groundvehicle
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name, description, tires, cc):
+        super().__init__(name, description, tires)
+        self.cc = cc
