84cf43840ca07aa3f16093abb8a004cb026f4e65
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-12-06 11:22:40 -0700

    Did all problems in comp.py. Debbuggin failed tests.

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -66,11 +66,12 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+# person.name.uppercase() person.age + 5
+g = [f"{person.name.upper()}, {person.age + 5}" for person in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(person.age) for person in humans]
 print(h)
