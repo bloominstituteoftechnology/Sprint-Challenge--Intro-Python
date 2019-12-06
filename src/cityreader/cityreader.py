c14285962c70f93ea5ded384f3ca059d9aaf648f
chikechris <cchike2017@gmail.com>
2019-12-06 13:26:08 -0500

    updated cityreader.py

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,11 +1,18 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
-
+import csv
+class City:
+  def __init__(self , name , lat, lon):
+    self.name = name
+    self.lat = float(lat)
+    self.lon = float(lon)
+  def __str__(self):
+    return f'city:{self.name}, lat:{self.lat} ,long:{self.lon}'
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module
+# In the body of the `cityreader` function, use Python's built-in "csv" module 
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -14,24 +21,23 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
-
-
 cities = []
 
-
 def cityreader(cities=[]):
-    import csv
+  with open('cities.csv', newline='') as csv_file:
+    reader = csv.DictReader(csv_file, delimiter=',')
+    for details in reader:
+      newCity = City(details['city'],details['lat'],details['lng'])
+      cities.append(newCity)
+    
 
-    with open('/c/Users/cchik/lambdaPython/Sprint-Challenge--Intro-Python/src/cityreader/cities.csv', 'rt') as f:
-        data = csv.reader(f)
-        for row in data:
-            cities.append({"city": row[0], "lat": row[3], "long": row[4]})
+  # TODO Implement the functionality to read from the 'cities.csv' file
+  # For each city record, create a new City instance and add it to the 
+  # `cities` list
+    
+    return cities
 
-        return cities
-
-
-cities = cityreader()
-print(cities)
+cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
