a36a936696f6eafab59f05940ae6589b5e6ca1e4
chikechris <cchike2017@gmail.com>
2019-12-06 14:17:55 -0500

    updated oop2.py

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,22 +4,25 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels =4):
-        self.num_wheels = num_wheels
+    def __init__(self, num_wheels =4): # initialized method; w/ num_wheels defaults to 4
+        self.num_wheels = num_wheels  # setting the instance variable
 
     # TODO
-    def drive(self):
-        return 'vroooom'
+    def drive(self): # initialized drive() method
+        return 'vroooom' # returns "vroooom".
 
 
 # Subclass Motorcycle from GroundVehicle.
 #
-class Motorcycle(GroundVehicle):
+
+class Motorcycle(GroundVehicle): #inherits from GroundVehicle
     def __init__(self, num_wheels=None):
-        super(). __init__(num_wheels = 2)
+        super(). __init__(num_wheels = 2) # super, sets num_wheels defaults to 2
         pass
-    def drive(self):
-        return "BRAAAP!!"
+    def drive(self):  # initialized drive() method
+        return "BRAAAP!!"  # returns "BRAAAP"
+
+
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
@@ -34,9 +37,8 @@ vehicles = [
     GroundVehicle(),
     Motorcycle(),
 ]
-for i in vehicles:
+
+for i in vehicles: # goes thru vehicles list and print the result of calling drive() on each.
     print(i.drive())
 
-# Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
