e7bf5f5b5c90c23a8eda722ca0f802bfefc8a85d
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-12-06 09:29:31 -0700

    Completed oop1.py and ran successful test!

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,33 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# [x] 1 Parent --> Vehicle
+# [x] 2 Children --> GroundVehicle &  FlightVehicle
+# [x] 4 Grandchildren --> Car, Motorcycle, Airplane, Starship
+
+class Vehicle:   #Base Class
+    pass
+
+
+# Group A
+class GroundVehicle(Vehicle):   # Child 1
+    pass
+
+class Car(GroundVehicle):    # Grandchild 1
+    pass
+
+class Motorcycle(GroundVehicle):    # Grandchild 2
+    pass
+
+
+# Group B
+class FlightVehicle(Vehicle):   # Child 2
+    pass
+
+class Airplane(FlightVehicle):    # Grandchild 1
+    pass
+
+class Starship(FlightVehicle):    # Grandchild 2
+    pass
+
