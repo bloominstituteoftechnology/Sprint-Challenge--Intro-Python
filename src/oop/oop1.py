7f96ef44d836cea3a981f450be37c5f73bf33d4e
Scott Renz <scottrenz@scottrenz.com>
2019-12-06 13:05:17 -0500

    pass oop1 and oop2

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,35 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle is base class
+class Vehicle():
+    def __init__(self):
+        return
+    pass
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+    pass
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+    pass
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+    pass
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+    pass
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+    pass
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+    pass
+
