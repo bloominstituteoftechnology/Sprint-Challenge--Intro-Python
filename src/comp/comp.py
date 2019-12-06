c095f47e75a18d3c87903b854205772b7d6f7723
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-12-06 10:53:48 -0700

    Did first problem in comp

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,7 +24,7 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [letter.name for letter in humans if letter.name[0] == 'D']
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
