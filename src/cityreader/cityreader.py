0065e7e33112ba6744651a93d32fa4b7fdc64dbd
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2019-12-06 12:58:57 -0500

    Completed Sprint Challenge with stretch goals.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,27 +1,46 @@
+import csv
+import os
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+
+
+class City():
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
 
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
-# Google "python 3 csv" for references and use your Google-fu for other examples.
+# Google "python 3 csv" for references and use your Google-fu for other
+# examples.
 #
 # Store the instances in the "cities" list, below.
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+
 cities = []
 
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+    directory = os.path.dirname(os.path.abspath(__file__))
+    with open(directory + '/cities.csv') as csvfile:
+        cities_reader = csv.reader(csvfile)
+        next(cities_reader)
+        for row in cities_reader:
+            cities.append(City(row[0], float(row[3]), float(row[4])))
     return cities
+
 
 cityreader(cities)
 
@@ -32,17 +51,18 @@ for c in cities:
 # STRETCH GOAL!
 #
 # Allow the user to input two points, each specified by latitude and longitude.
-# These points form the corners of a lat/lon square. Pass these latitude and 
+# These points form the corners of a lat/lon square. Pass these latitude and
 # longitude values as parameters to the `cityreader_stretch` function, along
-# with the `cities` list that holds all the City instances from the `cityreader`
-# function. This function should output all the cities that fall within the 
-# coordinate square.
+# with the `cities` list that holds all the City instances from the
+# `cityreader` function. This function should output all the cities that fall
+# within the coordinate square.
 #
 # Be aware that the user could specify either a lower-left/upper-right pair of
-# coordinates, or an upper-left/lower-right pair of coordinates. Hint: normalize
-# the input data so that it's always one or the other, then search for cities.
-# In the example below, inputting 32, -120 first and then 45, -100 should not
-# change the results of what the `cityreader_stretch` function returns.
+# coordinates, or an upper-left/lower-right pair of coordinates. Hint:
+# normalize the input data so that it's always one or the other, then search
+# for cities. In the example below, inputting 32, -120 first and then 45, -100
+# should not change the results of what the `cityreader_stretch` function
+# returns.
 #
 # Example I/O:
 #
@@ -60,12 +80,21 @@ for c in cities:
 
 # TODO Get latitude and longitude values from the user
 
+
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+    min_lat = min([lat1, lat2])
+    max_lat = max([lat1, lat2])
+    min_lon = min([lon1, lon2])
+    max_lon = max([lon1, lon2])
 
-  return within
+    # within will hold the cities that fall within the specified region
+    within = [city for city in cities if
+              city.lat > min_lat and city.lat < max_lat and
+              city.lon > min_lon and city.lon < max_lon]
+
+    # TODO Ensure that the lat and lon valuse are all floats
+    # Go through each city and check to see if it falls within
+    # the specified coordinates.
+
+    return within
--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,41 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    pass
+
+
+# GroundVehicle is derived from the base class Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+
+# Car is derived from the class GroundVehicle, which is derived from the
+# base class Vehicle
+class Car(GroundVehicle):
+    pass
+
+
+# Motorcycle is derived from the class GroundVehicle, which is derived from
+# the base class Vehicle
+class Motorcycle(GroundVehicle):
+    pass
+
+
+# FlightVehicle is derived from the base class Vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+
+# Starship is derived from the class FlightVehicle, which is derived from the
+# base class Vehicle
+class Starship(FlightVehicle):
+    pass
+
+
+# Airplane is derived from the class FlightVehicle, which is derived from the
+# base class Vehicle
+class Airplane(FlightVehicle):
+    pass
