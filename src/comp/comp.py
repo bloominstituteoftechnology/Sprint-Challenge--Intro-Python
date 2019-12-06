7fd75a08cea7a0ed3e214b8138a1c30e7d70046e
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-12-06 10:54:08 -0700

    Did second  problem in comp

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -30,7 +30,7 @@ print(a)
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [letter.name for letter in humans if letter.name[-1] == 'e']
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
