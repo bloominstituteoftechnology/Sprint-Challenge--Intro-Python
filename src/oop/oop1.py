1e22359ebf05b627898224855c2bf8a0f8fcbb7b
Marlon <marmoneymar@icloud.com>
2020-01-17 13:20:30 -0500

    Corrected comments

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,30 +1,24 @@
 # Write classes for the following class hierarchy:
 
-# No base class. It is the base class.
+# Vehicle is the base class. **
 class Vehicle:
     pass
 
-# Base class is Vehicle
 class FlightVehicle(Vehicle):
     pass
 
-# Base class  is FlightVehicle
 class Starship(FlightVehicle):
     pass
 
-# Base class is FlightVehicle
 class Airplane(FlightVehicle):
     pass
 
-# Base class is Vehicle
 class GroundVehicle(Vehicle):
     pass
 
-# Base class is GroundVehicle
 class Car(GroundVehicle):
     pass
 
-# Base class is GroundVehicle
 class Motorcycle(GroundVehicle):
     pass
 
