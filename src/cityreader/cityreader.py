d4ea13556187666f817501da362cc9057d1ff3a8
johnpitts <jgpitts@gmail.com>
2020-01-17 17:23:26 -0500

    debugged probem operating VS Code regarding having proper directory in terminal.  Terminal was in the wrong src file.  Wrote MVP for cityReader and it gives proper output but the test isn't sat.  might have to do with header, or test might be using full City objects whereas I printed out a string of the objects

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -6,7 +6,8 @@ class City(object):
     self.name = name
     self.lat = lat
     self.lon = lon
-
+    
+print("\n")
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -20,27 +21,27 @@ class City(object):
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 
-import csv
-with open('cities.csv') as csvfile:
-  cityreader = csv.reader(csvfile, delimiter=' ')
-  for row in cityreader:
-    print(row)
-  csvfile.close()
-
 cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  import csv
+  with open('cities.csv', newline='') as csvfile:
+    all_cities_reddit = csv.reader(csvfile, delimiter=',')
+    city = City("nothing", 40, 179)
+    for row in all_cities_reddit:
+      city = City(row[0], row[3], row[4])
+      cities.append(city)
     return cities
+  csvfile.close()
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(f"({c.name}, {c.lat}, {c.lon})")
 
 # STRETCH GOAL!
 #
