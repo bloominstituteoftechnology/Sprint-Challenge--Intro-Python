06ca5815e3f00f3c32d51be3108f6779e6ee9ca4
Louis-Charles Carrier <louis-charlescarrier@Louis-Charless-MacBook-Pro.local>
2020-01-17 12:17:29 -0500

    oop and cityreader

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -19,3 +19,52 @@
 # Put a comment noting which class is the base class
 
 # this is a test
+
+# Write classes for the following class hierarchy:
+#
+#  [Vehicle]->[FlightVehicle]->[Starship]
+#      |                |
+#      v                v
+# [GroundVehicle]      [Airplane]
+#   |       |
+#   v       v
+# [Car]  [Motorcycle]
+#
+# Each class can simply "pass" for its body. The exercise is about setting up
+# the hierarchy.
+#
+# e.g.
+#
+# class Whatever:
+#     pass
+#
+# Put a comment noting which class is the base class
+
+
+class Vehicle:
+    # This is the base class
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
