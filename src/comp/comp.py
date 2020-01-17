e0e638a715b452142d2ce3b28e1eaa6576e8b6b2
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-01-17 11:31:16 -0500

    2 more list comprehensions done, 4 left

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -39,12 +39,12 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [human.name for human in humans if human.name[0] in 'CDEFG']
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [(human.age + 10) for human in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
