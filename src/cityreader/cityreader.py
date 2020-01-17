15af5a1d4c59527393c120be8269be3635ea301b
Scott-Huston <scottphuston@gmail.com>
2020-01-17 10:04:27 -0800

    Finished SC

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
+import csv
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City():
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+  
+  def __repr__(self):
+    return(f'{self.name}: ({self.lat}, {self.lon})')
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -14,13 +24,28 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+
 cities = []
 
+# Implement the functionality to read from the 'cities.csv' file
+# For each city record, create a new City instance and add it to the 
+# `cities` list
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+  """
+  Method to read from cities.csv and return a list of cities
+  """
+  with open('cities.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    # count is used to skip the header line
+    count = 0
+    for row in reader:
+        if count>0:
+          city_name = row[0]
+          lat = row[3]
+          lon = row[4]
+          cities.append(City(city_name, lat, lon))
+        count+=1
     return cities
 
 cityreader(cities)
@@ -58,14 +83,42 @@ for c in cities:
 # Tucson: (32.1558,-110.8777)
 # Salt Lake City: (40.7774,-111.9301)
 
-# TODO Get latitude and longitude values from the user
-
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
+  """
+  Method to return what cities are within a rectangle created by
+  two latitude and longitude pairs
+  """
+  # normalize inputs
+  min_lat = min([lat1, lat2])
+  min_lon = min([lon1, lon2])
+  max_lat = max([lat1, lat2])
+  max_lon = max([lon1, lon2])
+
+  # get list of cities
+  cities = cityreader()
+
   # within will hold the cities that fall within the specified region
   within = []
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+  for city in cities:
+    lat = float(city.lat)
+    lon = float(city.lon)
+    # if city lat and lon are within their respective max and min values
+    # then add the city to within
+    if  lat >= min_lat and lat <= max_lat and \
+        lon >= min_lon and lon <= max_lon:
+        within.append(city)
 
   return within
+
+# get latitude and longitude values from the user
+print('This program allows you to search for major U.S. cities in the ', \
+        'rectangle between two latitude and longitude coordinate pairs')
+
+lat1 = float(input('Please input the latitude of the first point\n'))
+lon1 = float(input('Please input the longitude of the first point\n'))
+
+lat2 = float(input('Please input the latitude of the second point\n'))
+lon2 = float(input('Please input the longitude of the second point\n'))
+
+print(cityreader_stretch(lat1, lon1, lat2, lon2))
--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,19 +1,29 @@
-# Write classes for the following class hierarchy:
-#
-#  [Vehicle]->[FlightVehicle]->[Starship]
-#      |                |
-#      v                v
-# [GroundVehicle]      [Airplane]
-#   |       |
-#   v       v
-# [Car]  [Motorcycle]
-#
-# Each class can simply "pass" for its body. The exercise is about setting up
-# the hierarchy.
-#
-# e.g.
-#
-# class Whatever:
-#     pass
-#
-# Put a comment noting which class is the base class
+from abc import ABC
+
+# This is base class
+class Vehicle(ABC):
+    pass
+
+# GroundVehicle inherits from Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+# Car inherits from GroundVehicle
+class Car(GroundVehicle):
+    pass
+
+# Motorcycle inherits from GroundVehicle
+class Motorcycle(GroundVehicle):
+    pass
+
+# FlightVehicle inherits from Vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+# Starship inherits from FlightVehicle
+class Starship(FlightVehicle):
+    pass
+
+# Airplane inherits from FlightVehicle
+class Airplane(FlightVehicle):
+    pass
