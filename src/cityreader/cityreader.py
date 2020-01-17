87be0dd68c6968907c0fa6f00e26b41e45172395
Jeffrey <SantanaJny@gmail.com>
2020-01-17 14:26:28 -0500

    cityreader complete + tested

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,11 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City():
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -14,20 +19,24 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
+
 cities = []
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
+  with open('/Users/santana/Desktop/Lambda/CS/Python I/Sprint-Challenge--Intro-Python/src/cityreader/cities.csv') as csvDataFile:
+    csvReader = csv.reader(csvDataFile)
+    next(csvReader)
+    for row in csvReader:
+      cities.append(City(row[0], float(row[3]), float(row[4])))
     
-    return cities
+  return cities
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(c.name, c.lat, c.lon)
 
 # STRETCH GOAL!
 #
@@ -59,6 +68,13 @@ for c in cities:
 # Salt Lake City: (40.7774,-111.9301)
 
 # TODO Get latitude and longitude values from the user
+
+# lat1 = None
+# long2 = None
+# while lat1 == None || long2 == None:
+#   userInput = input('Enter in the starting coordinate (comma seperated): ').split(',')
+
+#   if userInput[0]
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
