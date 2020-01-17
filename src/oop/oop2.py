a3b66687867d7de89685b9f6077c75291d45b347
Ben <bigriffin94@outlook.com>
2020-01-17 09:08:29 -0800

    MVP MET

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,11 +4,16 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
     # TODO
+    def drive(self):
+        return 'vroooom'
 
+## My tests
+# lambo = GroundVehicle()
+# print(f'The Lambo with {lambo.num_wheels} wheels goes {lambo.drive()}')
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -18,6 +23,13 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 4):
+        super().__init__(num_wheels = 2)
+    
+    def drive(self):
+        return 'BRAAAP!!'
+
 
 vehicles = [
     GroundVehicle(),
@@ -30,3 +42,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for i in vehicles:
+    print(f' I am the drive function loop: {i.drive()}')
+
