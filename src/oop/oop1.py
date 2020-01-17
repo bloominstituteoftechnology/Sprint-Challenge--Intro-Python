0195d504e49bf95cbdc76d059375358292599cf3
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-01-17 11:26:55 -0500

    oop1 done.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,40 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    """Base class of all vehicles"""
+    pass
+
+
+class FlightVehicle(Vehicle):
+    '''Parent class for flying vehicles.
+       Base class is Vehicle'''
+    pass
+
+
+class Starship(FlightVehicle):
+    '''Starship class - base is Vehicle'''
+    pass
+
+
+class Airplane(FlightVehicle):
+    '''Airplane class - base is Vehicle'''
+    pass
+
+
+class GroundVehicle(Vehicle):
+    '''Parent class for ground vehicles.
+       Base class is Vehicle'''
+    pass
+
+
+class Car(GroundVehicle):
+    '''Car class - base is Vehicle'''
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    '''Motorcycle class - base is Vehicle'''
+    pass
