6da1035a26ebc08300f5f71a0428f2eb1d1301b5
johnpitts <jgpitts@gmail.com>
2020-01-17 14:27:37 -0500

    debugged and tested oop1 & 2 SAT

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -14,6 +14,8 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+Class City():
+
 cities = []
 
 def cityreader(cities=[]):
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,14 +3,12 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
-class GroundVehicle():
+class GroundVehicle(object):
     def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
-
-    def drive():
+    def drive(self):
         return "vroooom"
 
-    # TODO
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -20,15 +18,13 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
-# TODO
 
 class Motorcycle(GroundVehicle):
     def __init__(self, num_wheels = 2):
+        GroundVehicle.__init__(self, num_wheels)
         self.num_wheels = num_wheels
-        GroundVehicle.__init__(self)
-
-    def drive():
-        return 'BRAAAP!!'
+    def drive(self):
+        return "BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
@@ -39,9 +35,7 @@ vehicles = [
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
+    # print(callable(each_vehicle))
 
 for each_vehicle in vehicles:
-    vechicle = each_vehicle()
-    print(vehicle.drive())
-
-# TODO
+    each_vehicle.drive()
