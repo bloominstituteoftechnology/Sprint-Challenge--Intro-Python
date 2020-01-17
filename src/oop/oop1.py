22f1cf54a9f07e930a4341c57156f93eda65be07
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-01-17 11:38:02 -0600

    Update oop1.py
    Sub Classes have been created

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -18,17 +18,35 @@
 #
 # Put a comment noting which class is the base class
 
+
+## Vehicle class is the main Parent class
 class Vehicle: 
-    def __init__(self, vehicle, flight, ship):
+    def __init__(self, vehicle, flight_vehicle, ship):
         self.vehicle = vehicle
-        self.flight = flight
+        self.flight_vehicle = FlightVehicle(flight_vehicle)
         self.ship = ship
         
-        
+# Inhereted by Vehicle class, Parent of Starship Class
 class FlightVehicle: 
-    def __init__(self, airplane):
-        self.airplane = airplane
-        
+    def __init__(self, starship):
+        self.starship = Starship(starship) 
+
+# Inhereted by FlightVehicle Class
 class Starship:
     def __init__(self, starship):
         self.starship = starship
+        
+
+# Vehicle inherets all Sub classes below 
+class GroundVehicle: 
+    def __init__(self, car, motorcycle):
+        self.car = Car(car)
+        self.motorcycle = Motorcycle(motorcycle)
+        
+class Car: 
+    def __init__(self, car):
+        self.car = car
+        
+class Motorcycle: 
+    def __init__(self, motorcycle):
+        self.motor_cycle = motorcycle
