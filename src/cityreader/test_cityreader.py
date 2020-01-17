6f4252386e6b25ab2576e02502fc30a5c9e83f9c
Nick <nicholasinterest1@icloud.com>
2020-01-17 12:33:36 -0500

    Sprint Complete

--- src/cityreader/test_cityreader.py
+++ src/cityreader/test_cityreader.py
@@ -78,6 +78,7 @@ class CityreaderTests(unittest.TestCase):
     
   def test_cityreader_correctness(self):
     for i in range(len(self.cities)):
+      print(self.cities[i])
       self.assertTrue(check_city(self.cities[i], self.expected[i]))
 
 
