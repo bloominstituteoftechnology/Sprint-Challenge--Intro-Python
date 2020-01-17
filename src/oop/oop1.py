148f0709c82b04d972728ba1b0f382a770a96615
Joshua-Shockley <thelumbashockley@gmail.com>
2020-01-17 13:13:26 -0600

    turning in the spirnt.. not sure why test files dont pass when i get same answers

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,54 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    # def __init_(self, name):
+    #     self.name = name
+    pass
+
+# groundvehicles from vehicle
+# t_type specifies travel type ex: ground, water, air, space
+
+
+class GroundVehicle(Vehicle):
+    # def __init__(self, name, t_type):
+    #     super().__init__(self, name)
+    pass
+
+# car from ground vehicle
+
+
+class Car(Vehicle):
+    # def __init__(self, name, speed, t_type, make, model, engine, seats):
+    #     super().__init__(self, name, speed, t_type)
+    pass
+# motorcycle from ground vehicle
+
+
+class Motorcycle(Vehicle):
+    # def __init__(self, name, speed, t_type, make, model, engine, seats, helmet):
+    #     super().__init__(self, name, speed, t_type)
+    pass
+# flightvehile from vehicle
+
+
+class FlightVehicle(Vehicle):
+    # def __init__(self, name, t_type):
+    #     super().__init__(self, name)
+    pass
+# airplane from flightvehicle
+
+
+class Airplane(FlightVehicle):
+    # def __init__(self, name, t_type, speed, make, model, engine, seats, wingspan):
+    #     super().__init__(self, name, t_type)
+    pass
+# starship from flightvehicle
+
+
+class Starship(FlightVehicle):
+    # def __init__(self, name, t_type, speed,  make, model, power_drive, occupancy):
+    #     super().__init__(self, name, t_type)
+    pass
