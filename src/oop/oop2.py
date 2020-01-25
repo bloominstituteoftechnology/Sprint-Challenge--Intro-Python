9a8a250a98cde18137230c5faa2f78e317b684f3
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2020-01-25 12:03:44 -0800

    i dont know about oop 2

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,23 +1,33 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
-#
-# Also change it so the num_wheels defaults to 4 if not specified when the
-# object is constructed.
+# # To the GroundVehicle class, add method drive() that returns "vroooom".
+# #
+# # Also change it so the num_wheels defaults to 4 if not specified when the
+# # object is constructed.
 
-class GroundVehicle():
-    def __init__(self, num_wheels):
-        self.num_wheels = num_wheels
+# class GroundVehicle():
+#     def __init__(self, num_wheels):
+#         self.num_wheels = num_wheels
 
-    # TODO
+#     def drive(self):
+#         print(self, "vrooom")
 
 
-# Subclass Motorcycle from GroundVehicle.
-#
-# Make it so when you instantiate a Motorcycle, it automatically sets the number
-# of wheels to 2 by passing that to the constructor of its superclass.
-#
-# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+# # Subclass Motorcycle from GroundVehicle.
+# #
+# # Make it so when you instantiate a Motorcycle, it automatically sets the number
+# # of wheels to 2 by passing that to the constructor of its superclass.
+# #
+# # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
-# TODO
+# class Motorcycle(GroundVehicle):
+#     def __init__(self, name, model, price,):
+#         self.name = name
+#         self.model = model
+#         self.price = price
+    
+#     def drive(self):
+#         return f"BRAAAP! !"
+
+
 
 vehicles = [
     GroundVehicle(),
@@ -28,5 +38,4 @@ vehicles = [
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
-
-# TODO
+print (*vehicles)
