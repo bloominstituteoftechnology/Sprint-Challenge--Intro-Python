77533ed596d9d802b31b3929af4caeef56457df6
unknown <jjamal95@gmail.com>
2020-01-26 20:57:08 -0600

    finished comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -55,18 +55,18 @@ print(sorted(e))
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
-print(f)
+f = [(person.name, person.age) for person in humans if person.age >= 27 and person.age <= 32]
+print(sorted(f))
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [Human((person.name).upper(), (person.age +5)) for person in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(person.age) for person in humans]
 print(h)
