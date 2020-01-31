429970e0e3973af274a048ee54faecfd062c14ca
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2020-01-30 23:13:46 -0500

    passes all the tests now with the addition of float to the creation of te city reader lat and lon

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -52,7 +52,7 @@ def cityreader(cities=[]):
     reader = csv.reader(csvfile)
     next(reader)
     for row in reader:
-      cities.append(City(row[0],row[3],row[4]))
+      cities.append(City(row[0],float(row[3]),float(row[4])))
 
 
     
