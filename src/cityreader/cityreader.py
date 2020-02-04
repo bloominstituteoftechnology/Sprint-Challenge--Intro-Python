2e6979268b6ac0c3cd17010f97e80e86e3b6e00a
Scott Schindler <scottschindler29@gmail.com>
2020-02-04 16:15:05 -0500

    new changes

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat 
+    self.lon = lon
+
+  def __str__(self):
+    return (f'{self.name}, {self.lat}, {self.lon} ')
+    
+
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -16,10 +26,19 @@
 # should not be loaded into a City object.
 cities = []
 
+import csv 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+
+  with open('cities.csv', newline= '') as csvfile:
+    cityList = csv.reader(csvfile, delimiter = ',')
+    for item in cityList:
+      if item[0] != 'city':
+        cities.append(City(item[0], float(item[3]), float(item[4])))
+
+
     
     return cities
 
@@ -28,6 +47,7 @@ cityreader(cities)
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
     print(c)
+
 
 # STRETCH GOAL!
 #
--- src/comp/comp.py
+++ src/comp/comp.py
@@ -23,49 +23,50 @@ humans = [
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
-print("Starts with D:")
-a = []
+# print("Starts with D:")
+a = [item.name for item in humans if item.name[0] == 'D']
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [item.name for item in humans if item.name[-1] == "e"]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [item.name for item in humans if item.name[0] in ["C", "D", "E", "F", "G"]]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [item.age + 10 for item in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f"{item.name}-{item.age}" for item in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(item.name, item.age) for item in humans if item.age >= 27 and item.age <= 32] # check on this one
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [Human(item.name.upper(), item.age + 5) for item in humans]
 print(g)
+
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(item.age) for item in humans]
 print(h)
