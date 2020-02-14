4d3951245c32b1d23858f258bba6a71c0d8e9913
ElijahMcKay <elijahmckay10@gmail.com>
2020-02-14 12:50:35 -0600

    still working on stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -41,14 +41,12 @@ def cityreader(cities=[]):
 				# print('lon', type(city[4]))
 				cities.append(City(city[0], float(city[3]), float(city[4])))
 
-	print("Finished!")
+	# print("Finished!")
 
 	return cities
-
-
 # Print the list of cities (name, lat, lon), 1 record per line.
-for c in cities:
-	print(c)
+# for c in cities:
+# 	print(c)
 
 # STRETCH GOAL!
 #
@@ -82,11 +80,16 @@ for c in cities:
 # TODO Get latitude and longitude values from the user
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+	print(type(lat1), type(lat2), type(lon1), type(lon2))
+  	# within will hold the cities that fall within the specified region
+	# within = [f"{city.name}: ({city.lat}, {city.lon})" for city in cities if city.lon in range(lon2, lon1) and city.lat in range(lat2, lat1)]
+  	# TODO 
+	# Ensure that the lat and lon valuse are all floats
+  	# Go through each city and check to see if it falls within
+ 	# the specified coordinates.
+	for city in cities:
+		print(city.lon in range(lon2, lon1) and city.lat in range(lat2, lat1))
 
-  return within
+	return within
+
