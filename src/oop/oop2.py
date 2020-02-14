10a2c99f70962012c52dcae58127a5bbf48d02f3
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2020-02-14 10:35:31 -0600

    finished oop 2

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,8 +4,12 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
+    
+    def drive(self):
+        return "vroooom"
+    
 
     # TODO
 
@@ -16,6 +20,14 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels=num_wheels)
+    
+    def drive(self):
+        return "BRAAAP!!"
+    
 
 # TODO
 
@@ -30,3 +42,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for vehicle in vehicles:
+    vehicle.drive()
