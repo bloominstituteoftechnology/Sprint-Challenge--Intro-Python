c46364a57bd3e82e1fd8a507c68b9165736f5dc8
kenna <kennaalastair@gmail.com>
2020-02-14 11:02:32 -0800

    still working on this challenge, terminal isn't returning any errors or city info

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,14 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City():
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
 
+    def __str__(self):
+      return f"The city of {self.name} is located at lat: {self.lat}, lon: {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,8 +28,12 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+  with open("cities.csv", "r") as csvfile:
+      cityreader = csv.DictReader(csvfile)
+      for row in cityreader:
+          cities.append(City(name=row['city'], long=row['lon'], lat=row['lat']))
     
-    return cities
+  return cities
 
 cityreader(cities)
 
