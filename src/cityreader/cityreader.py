4ec16b05f19bb83db975f85ec0bc3deabcbf3fb2
zeleke <zzdd9251@gmail.com>
2020-02-14 10:33:26 -0800

    2st commit..

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -2,6 +2,16 @@
 # fields for name, lat and lon (representing latitude and longitude).
 
 
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __repr__(self):
+        return f'{self.name}: ({self.lat}, {self.lon})'
+
+
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
