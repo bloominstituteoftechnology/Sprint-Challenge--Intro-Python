33b4308b59ace46253fe51d7494f285964aac6f5
Hira Khan <sunshine9.r8@gmail.com>
2020-02-14 12:48:15 -0500

    SC done. 1 test error

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,55 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    """Base class for all the other class """
+#    def __init__(self, motor, engine):
+#        self.motor = motor
+#        self.engine = engine
+    pass
+
+class FlightVehicle(Vehicle):
+    """ Child class of Parent Vehicle """
+#    def __init__(self, canfly):
+#        super().__init__(motor, engine)
+#        self.canfly = canfly
+    pass
+
+class Starship(FlightVehicle):
+#    """Child class of Parent Vehicle class """
+#    def __init__(self, gridfins, engines, booster, nozzle):
+#        super().__init__(self, canfly)
+#        self.gridfins = gridfins
+#        self.engines = engines
+#        self.booster = booster
+#        self.nozzle = nozzle
+    pass
+
+class GroundVehicle(Vehicle):
+    """Sub-child of Parent Vehicle class """
+#    def __init__(self, tires, steeringwheel):
+#        super().__init__(self, motor, engine)
+#        self.tires = tires
+#        self.steeringwheel = steeringwheel
+    pass
+
+class Car(GroundVehicle):
+    """Sub-child of GroundVehicle parent class """
+#    def __init__(self, fourtires):
+#        super().__init__(self, tires, steeringwheel)
+#        self.fourtires = fourtires
+    pass
+
+class Motorcycle(GroundVehicle):
+#    """ child of GroundVehicle Parent"""
+#    def __init__(self, twotires):
+##        self.twotires = twotires
+    pass
+
+class Airplane(FlightVehicle):
+    """Child of FlightVehicle Class """
+#    def __init__(self, hasfins):
+#        super().__init__(self, canfly)
+#        self.hasfins = hasfins
+    pass
