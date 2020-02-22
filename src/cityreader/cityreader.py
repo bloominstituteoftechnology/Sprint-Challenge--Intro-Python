9c4b5e876606ce8c53e4a387b92d965abd49a6af
Ilgar Ilyasov <iilyasov@awarex.com>
2020-02-21 21:47:07 -0500

    Complete cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -5,7 +5,7 @@
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -14,14 +14,32 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import os
+import csv
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f'{self.name}, {self.lat}, {self.lon}'
+
 cities = []
 
-def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
+  # For each city record, create a new City instance and add it to the
   # `cities` list
-    
-    return cities
+path = "/Users/ilqarilyasov/Desktop/Edu/LambdaCS/Sprint-Challenge--Intro-Python/src/cityreader/cities.csv"
+
+def cityreader(cities=[]):
+  with open(path) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    for (index, row) in enumerate(csvreader):
+      if index != 0:
+        city = City(row[0], float(row[3]), float(row[4]))
+        cities.append(city)
+  return cities
 
 cityreader(cities)
 
