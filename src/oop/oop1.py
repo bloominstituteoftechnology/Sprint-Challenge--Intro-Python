fde378fc2917264ba69b52d8e5220c933f4966eb
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2020-02-23 20:18:54 -0600

    opp1 all good!

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,33 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+# Vehicle is the base class
+class Vehicle():
+    # base
+    pass
+
+
+class FlightVehicle(Vehicle):
+    # base
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    # base
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
