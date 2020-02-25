fea0b7d99558c7a885d98ae5dbebd50b42d06d9f
heuzin <matheusqs.arq@gmail.com>
2020-02-24 19:40:12 -0500

    test for oop2.py

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -8,7 +8,7 @@ class GroundVehicle():
         self.num_wheels = num_wheels
 
     def drive(self):
-        print('vroooom')
+        return("vroooom")
     # TODO
 
 
@@ -26,7 +26,7 @@ class Motorcycle(GroundVehicle):
         super().__init__(num_wheels)
 
     def drive(self):
-        print("BRAAAP!!")
+        return("BRAAAP!!")
 
 vehicles = [
     GroundVehicle(),
