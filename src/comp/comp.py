3c5a6d7d038edbfee56b9b825581cc6cb326371c
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-03-13 11:13:10 -0600

    Update comp.py
    list comprehentions complete

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -81,11 +81,15 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [
+    (i.name.upper(), i.age+5) for i in humans 
+]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [
+    math.sqrt(i.age) for i in humans
+]
 print(h)
