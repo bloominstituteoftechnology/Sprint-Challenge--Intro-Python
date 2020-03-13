c2fd87578f5372c9351c2e9070e17e71b82f07d9
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-03-13 10:19:09 -0600

    Update comp.py
    ends with e

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -33,7 +33,10 @@ print(a)
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [
+    i.name for i in humans
+    if(i.name[-1] == "e")
+]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
