ba0532025f77b44d00ca0964b65e295922f5d9c7
adeoye adela <adeoye.adela@gmail.com>
2020-03-13 17:24:13 +0100

    oop2 test pass

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,16 +4,13 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
     # TODO
     def drive(self):
-        if not self.num_wheels:
-            self.num_wheels=4
-        else:
-            self.num_wheels
-        return "vroom"
+        
+        return "vroooom"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -24,7 +21,11 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
-
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(num_wheels=2)
+    def drive(self):    
+        return "BRAAAP!!"
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -36,3 +37,5 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for n in vehicles:
+   print( n.drive())
