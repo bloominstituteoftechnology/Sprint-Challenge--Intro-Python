66d2c801ad123c9cfa75fa98f7baabae544e68dc
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-03-13 15:30:58 -0700

    got g done in comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -68,7 +68,7 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [for person in humans humans.slice]
+g = [Human(person.name.upper(), person.age + 5) for person in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
