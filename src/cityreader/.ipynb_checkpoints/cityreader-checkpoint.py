2257dab92d5693b7547a2cb768bc1932402d1d79
mefrem <maxim.efremov@gmail.com>
2020-03-13 12:24:05 -0400

    finished comp and cityreader

--- src/cityreader/.ipynb_checkpoints/cityreader-checkpoint.py
+++ src/cityreader/.ipynb_checkpoints/cityreader-checkpoint.py
@@ -18,21 +18,16 @@ class City():
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
-class City():
-    def __init__(self, name, lat, lon):
-        self.name = name
-        self.lat = float(lat)
-        self.lon = float(lon)
-
 cities = []
+
 def cityreader(cities=[]):
-    import csv
-    with open('cities.csv') as c:
-        city_generator = csv.reader(c, delimiter=',')
-        next(city_generator)
-        for row in city_generator:
-            cities.append(City(row[0], row[3], row[4]))
-        return cities            
+  # TODO Implement the functionality to read from the 'cities.csv' file
+  # For each city record, create a new City instance and add it to the 
+  # `cities` list
+    
+    return cities
+
+cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
@@ -69,12 +64,12 @@ for c in cities:
 
 # TODO Get latitude and longitude values from the user
 
-# def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-#   # within will hold the cities that fall within the specified region
-#   within = []
+def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
+  # within will hold the cities that fall within the specified region
+  within = []
 
-#   # TODO Ensure that the lat and lon valuse are all floats
-#   # Go through each city and check to see if it falls within 
-#   # the specified coordinates.
+  # TODO Ensure that the lat and lon valuse are all floats
+  # Go through each city and check to see if it falls within 
+  # the specified coordinates.
 
-#   return within
+  return within
