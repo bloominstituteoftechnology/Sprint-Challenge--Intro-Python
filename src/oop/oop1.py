2d85bc6363eb6f33207878a2ecd56e01b04dbca9
Jerry Haaser <jerry.haaser@hotmail.com>
2020-04-10 11:02:49 -0500

    set up class hierarchy

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,23 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
