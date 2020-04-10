af43d6e5cf42a7b2d94082a6316f0bebcbc3abdb
PercivalN <percival.ngan@gmail.com>
2020-04-10 17:41:42 -0500

    Completed comp.py and FINALLY passed test

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -14,6 +14,16 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+class City:
+      
+      def __init__(self, name, lat, lon):
+            self.name = name
+            self.lat = lat
+            self.lon = lon
+            
+      def __str__(self):
+            return f"{self.name}, {self.lat}, {self.lon}"
+
 cities = []
 
 def cityreader(cities=[]):
@@ -21,6 +31,12 @@ def cityreader(cities=[]):
   # For each city record, create a new City instance and add it to the 
   # `cities` list
     
+    with open('cities.csv') as csvfile:
+      read = csv.reader(csvfile, delimiter = ',')
+      next(read)
+      for row in read:
+            cities.append(City(row[0], float(row[3]), float(row[4])))
+            
     return cities
 
 cityreader(cities)
--- src/comp/comp.py
+++ src/comp/comp.py
@@ -53,7 +53,7 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [i.name for i in humans if i.name[0] in ['C', 'D', 'E', 'F', 'G']]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
@@ -71,7 +71,7 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = [f"({i.name}, {i.age})" for i in humans if i.age in range(27, 32+1)]
+f = [(i.name, i.age) for i in humans if i.age in range(27, 32+1)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
@@ -84,5 +84,6 @@ print(g)
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = [for i in humans]
+
+h = [math.sqrt(i.age) for i in humans]
 print(h)
