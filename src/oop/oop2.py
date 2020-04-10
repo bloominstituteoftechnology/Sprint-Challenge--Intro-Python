04d03b0ce0997a5457013369273dceb5d406dd60
Bryan <17adamsbryk@washk12.org>
2020-04-10 11:01:07 -0600

    MVP finished

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,11 +3,23 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
+    def drive(self):
+        return("vroooom")
+
     # TODO
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(2)
+
+    def drive(self):
+        return("BRAAAP!!")
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -18,7 +30,6 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
-
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -26,6 +37,9 @@ vehicles = [
     GroundVehicle(),
     Motorcycle(),
 ]
+for vehicle in vehicles:
+    print(vehicle.drive())
+    print(vehicle.num_wheels)
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
---
+++ src/oop/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+for vehicle in vehicles:
+#     # print(vehicle.drive())
+#     # print(vehicle.num_wheels)
