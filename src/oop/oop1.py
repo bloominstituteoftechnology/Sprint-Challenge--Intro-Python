237489b8c18fbb114909d1da11df73e14bd496c4
Josh <josh_hill15@me.com>
2020-04-10 13:54:16 -0400

    almost finished with last sprint problem

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,38 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    # base
+    pass
+
+
+class FlightVehicle(Vehicle):
+    # 2nd level
+    pass
+
+
+class Starship(FlightVehicle):
+    # 2nd level
+    pass
+
+
+class GroundVehicle(Vehicle):
+    # 2nd level
+    pass
+
+
+class Airplane(FlightVehicle):
+    # 2nd level
+    pass
+
+
+class Car(GroundVehicle):
+    # 3rd
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    # 3rd
+    pass
