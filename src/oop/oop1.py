ce26ddbfa9e862a43c6a3cec543e46a660610b66
Robert Gordon <Komeuppance@gmail.com>
2020-04-10 08:55:02 -0700

    Oop1.py 6 tests passing

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -20,3 +20,26 @@
 
 
 #Base Class:
+class Vehicle:
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
