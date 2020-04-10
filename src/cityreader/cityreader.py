8bea09cb09fa7ecd46dfd5ec07b3ffb3d5b145fd
imxande <alerodriguezlarosa@gmail.com>
2020-04-10 13:36:22 -0400

    city class created and implemented

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City:
+  # constructor
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+  def __str__(self):
+    return (f"{self.name}, {self.lat}, {self.lon}")
 
+  # sanity check
+  print("La Havana, Cuba", 23.1136, 82.3666)
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
