71a5f0d61f5fe1e41aeaeee00664c79a0a278787
Chad Rutherford <chad_rutherford@outlook.com>
2020-05-08 11:32:30 -0400

    Name starts with 'D' test passing

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,7 +24,7 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [human.name for human in humans if human.name[0:1] == 'D']
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
