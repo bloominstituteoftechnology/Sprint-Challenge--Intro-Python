b598f2029601799ef2c2505ef6edb60c1efe6d5b
Chad Rutherford <chad_rutherford@outlook.com>
2020-05-08 11:36:07 -0400

    Name begins with letter between 'C' and 'G' (exclusive) test passing.

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -36,7 +36,7 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [human.name for human in humans if human.name[0:1] in ['C', 'D', 'E', 'F', 'G']]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
