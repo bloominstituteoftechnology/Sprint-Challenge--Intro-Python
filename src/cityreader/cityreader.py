bf636c27ac5afca653a2357b6b2b15b26f5f543c
jwross24 <jwross24@users.noreply.github.com>
2020-05-08 12:37:44 -0400

    complete city reader and stretch goals

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,27 +1,72 @@
+import csv
+import os
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
 
-# We have a collection of US cities with population over 750,000 stored in the
-# file "cities.csv". (CSV stands for "comma-separated values".)
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return f"({self.name}, {self.lat}, {self.lon})"
+
+
+# We have a collection of US cities with population over 750,000 stored in
+# the file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
-# to read this file so that each record is imported into a City instance. Then
-# return the list with all the City instances from the function.
-# Google "python 3 csv" for references and use your Google-fu for other examples.
+# In the body of the `cityreader` function, use Python's built-in "csv"
+# module to read this file so that each record is imported into a City
+# instance. Then return the list with all the City instances from the
+# function.
+# Google "python 3 csv" for references and use your Google-fu for other
+# examples.
 #
 # Store the instances in the "cities" list, below.
 #
-# Note that the first line of the CSV is header that describes the fields--this
-# should not be loaded into a City object.
+# Note that the first line of the CSV is header that describes the fields--
+# this should not be loaded into a City object.
 cities = []
 
+
+def find_in_subdirectory(filename, subdirectory=''):
+    """
+    Helper function to find the cities.csv file in the subdirectory.
+    """
+    if subdirectory:
+        path = subdirectory
+    else:
+        path = os.getcwd()
+    for root, dirs, names in os.walk(path):
+        if filename in names:
+            return os.path.join(root, filename)
+    raise 'File not found'
+
+
+def strip_zeros(num):
+    """
+    Helper function to strip trailing zeros from coordinates.
+    """
+    return float(str(num).rstrip('0'))
+
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+    with open(find_in_subdirectory('cities.csv'), 'r', newline='') as csvfile:
+        citiesreader = csv.reader(csvfile)
+
+        # Skip the header row
+        next(citiesreader)
+
+        for row in citiesreader:
+            cities.append(
+                City(row[0], strip_zeros(row[3]), strip_zeros(row[4])))
     return cities
+
 
 cityreader(cities)
 
@@ -32,15 +77,16 @@ for c in cities:
 # STRETCH GOAL!
 #
 # Allow the user to input two points, each specified by latitude and longitude.
-# These points form the corners of a lat/lon square. Pass these latitude and 
+# These points form the corners of a lat/lon square. Pass these latitude and
 # longitude values as parameters to the `cityreader_stretch` function, along
-# with the `cities` list that holds all the City instances from the `cityreader`
-# function. This function should output all the cities that fall within the 
-# coordinate square.
+# with the `cities` list that holds all the City instances from the
+# `cityreader` function. This function should output all the cities that fall
+# within the coordinate square.
 #
 # Be aware that the user could specify either a lower-left/upper-right pair of
-# coordinates, or an upper-left/lower-right pair of coordinates. Hint: normalize
-# the input data so that it's always one or the other, then search for cities.
+# coordinates, or an upper-left/lower-right pair of coordinates. Hint:
+# normalize the input data so that it's always one or the other, then search
+# for cities.
 # In the example below, inputting 32, -120 first and then 45, -100 should not
 # change the results of what the `cityreader_stretch` function returns.
 #
@@ -59,13 +105,27 @@ for c in cities:
 # Salt Lake City: (40.7774,-111.9301)
 
 # TODO Get latitude and longitude values from the user
+lat1, lon1 = map(float, input('Enter lat1,lon1: ').split(','))
+lat2, lon2 = map(float, input('Enter lat2,lon2: ').split(','))
+
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
+    # within will hold the cities that fall within the specified region
+    within = []
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+    # TODO Ensure that the lat and lon values are all floats
+    # Go through each city and check to see if it falls within
+    # the specified coordinates.
+    for coord in (lat1, lon1, lat2, lon2):
+        if not isinstance(coord, (float, int)):
+            raise ValueError('You entered a lat/lon incorrectly!')
 
-  return within
+    # Sort lats and lons in ascending order
+    lats = sorted((lat1, lat2))
+    lons = sorted((lon1, lon2))
+
+    for c in cities:
+        if ((lats[0] <= c.lat <= lats[1]) and (lons[0] <= c.lon <= lons[1])):
+            within.append(c)
+
+    return within
