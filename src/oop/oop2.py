07f6602f74dec66681ef38212900190860a5ec4e
Jud Taylor <jud.taylor@protonmail.com>
2020-05-08 12:35:21 -0400

    Added Class Vehicle so it could be inherited from.

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,6 +3,9 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+class Vehicle:
+    pass
+
 class GroundVehicle(Vehicle):
     def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
