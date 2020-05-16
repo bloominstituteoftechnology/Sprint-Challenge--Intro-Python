540fb9538d6c3a089c6807bf223cfaff25fe1d78
Mayankho <yankhot96@gmail.com>
2020-05-16 11:22:47 -0400

    Finishied oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,46 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# This is the base class
+class Vehicle:
+    def __init__(self, vehicle_name, is_Flight = False, is_Star = False):
+        self.vehicle_name = vehicle_name
+        self.is_Flight = is_Flight
+        self.is_Star = is_Star
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self, vehicle_name):
+        super().__init__(vehicle_name)
+        pass
+
+class Car(GroundVehicle):
+    def __init__(self, vehicle_name):
+        super().__init__(vehicle_name)
+        pass
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, vehicle_name):
+        super().__init__(vehicle_name)
+        pass
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self, vehicle_name):
+        super().__init__(vehicle_name, is_Flight = True) 
+        pass
+
+class Airplane(FlightVehicle):
+    def __init__(self, vehicle_name):
+        super().__init__(vehicle_name)
+        pass
+
+
+class Starship(Vehicle):
+    def __init__(self, vehicle_name): 
+        super().__init__(vehicle_name, is_Star = True)
+        pass
+
