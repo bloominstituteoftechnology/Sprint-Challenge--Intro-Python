3fc1dd142f33f698725c9d5c0d9a64a88315e5d4
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-05 10:51:01 +0300

    file oop1 test ok

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,28 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    pass
+# subclass of vehicle
+class GroundVehicle(Vehicle):
+    pass
+# subclass of GroundVehicle
+class Car(GroundVehicle):
+    pass
+# subclass of GroundVehicle
+class Motorcycle(GroundVehicle):
+    pass
+# subclass of Vehicle
+class FlightVehicle(Vehicle):
+    pass
+# subclass of FlightVehicle
+class Starship(FlightVehicle):
+    pass
+# subclass of FlightVehicle
+class Airplane(FlightVehicle):
+    pass
+
+
+
+
