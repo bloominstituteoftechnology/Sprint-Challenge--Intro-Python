74fb8363d0e6ad193d73243a8eb38c690f4bf9f8
Mario Rivera <mario@Marios-MacBook-Pro.local>
2020-06-05 10:06:29 -0700

    periodic save point, quick break

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -31,24 +31,24 @@ print(a)
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [h.name for h in humans if h.name[-1] == "e"]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [h.name for h in humans if h.name[0] in ["C", "D", "E", "F", "G"]]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [(h.age + 10) for h in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f'{h.name}-{h.age}' for h in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
