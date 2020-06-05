97f183dab669044ebd364054c278426155f6517a
Lydia Zhang <art4everyan@gmail.com>
2020-06-05 12:54:13 -0400

    comp finished

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -71,11 +71,12 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+
+g = [Human(x.name.upper(), x.age + 5) for x in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [pow(x.age,3) for x in humans]
 print(h)
