bc62b17ebee45948cdd3d69f1da08a5824db90a6
Hector <hectoremil21@hotmail.com>
2020-06-05 13:04:04 -0400

    Fix line 64. Mistakenly subtracted 5 instead of adding 5

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -61,7 +61,7 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [Human(str(person.name).upper(), person.age - 5) for person in humans]
+g = [Human(str(person.name).upper(), person.age + 5) for person in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
