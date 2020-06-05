be2696faa44426f1625f2bfad0989ad3dd2f7cd9
Shawn Gee <shawngee@gmail.com>
2020-06-05 12:07:39 -0400

    fixed bug where lat and lon were passed as strings - I miss Swift

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -32,7 +32,7 @@ def cityreader(cities=[]) -> List[City]:
     reader = csv.reader(cities_file)
     next(reader)
     for row in reader:
-      cities.append(City(row[0], row[3], row[4]))
+      cities.append(City(row[0], float(row[3]), float(row[4])))
     
     return cities
 
