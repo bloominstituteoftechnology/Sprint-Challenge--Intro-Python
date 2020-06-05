3e95a71bfdf9b1f0df75c9e707ef45befc8c4d53
Mark Gerrior <github@gerrior.com>
2020-06-05 09:03:17 -0700

    test_square_root_of_ages done

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -67,5 +67,5 @@ print(g)
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(individual.age) for individual in humans]
 print(h)
