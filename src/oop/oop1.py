615bbc82b890ee9de0ea04d859045e801ee6723d
CC <ciarabeitel@gmail.com>
2020-06-05 11:14:04 -0400

    Complete opp1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,25 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle: #this is the base class
+    pass
+
+class FlightVehicle(Vehicle): # this is the parent class for airplane
+    pass
+
+class Starship(FlightVehicle): 
+    pass
+
+class GroundVehicle(Vehicle): # this is the parent class for Car & Motorcycle
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
