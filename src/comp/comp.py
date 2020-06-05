0a31d15445ecbd77012e55938ca919db29e68325
ph5500 <philfives@gmail.com>
2020-06-05 12:09:07 -0600

    added ability to read csv file, completed almost all list comprehension questions

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -25,29 +25,42 @@ humans = [
 # whose name starts with 'D':
 print("Starts with D:")
 a = []
+for i in humans:
+    if i.name[0] == 'D':
+        a.append(i.name)
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
 b = []
+for i in humans:
+    if i.name[-1] == 'e':
+        b.append(i.name)
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
 c = []
+for i in humans:
+    if i.name[0].upper() >= 'C' and i.name[0].upper() <= 'G':
+        c.append(i.name)
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
 d = []
+for i in humans:
+    d.append(i.age + 10)
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
 e = []
+for i in humans:
+    e.append(f"{i.name}-{i.age}")
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
@@ -55,6 +68,10 @@ print(e)
 # inclusive.
 print("Names and ages between 27 and 32:")
 f = []
+for i in humans:
+    if i.age >= 27 and i.age <= 32:
+        human_tuple = (i.name, i.age)
+        f.append(human_tuple)
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
@@ -62,6 +79,9 @@ print(f)
 # The "humans" list should be unmodified.
 print("All names uppercase:")
 g = []
+for i in humans:
+    new_human = (Human(i.name.upper(), i.age + 5))
+    g.append(new_human)
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
