0af496a5191531530cf1f1e24cdaa5fe4cfe3afe
Pete1701 <pete.pertongonas@pirron.one>
2020-06-05 21:42:54 +0200

    comp

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -50,31 +50,40 @@ print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [i.age + 10 for i in humans]
+# for i in humans:
+#     d.append(i.age + 10)
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f"{i.name}-{i.age}" for i in humans]
+# for i in humans:
+#     e.append(f"{i.name}-{i.age}")
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(i.name, i.age) for i in humans if i.age >= 27 and i.age <= 32]
+# for i in humans:
+#     if i.age >= 27 and i.age <= 32:
+#         f.append((i.name, i.age))
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [Human(i.name.upper(), i.age + 5) for i in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(i.age) for i in humans]
+# for i in humans:
+#     h.append(math.sqrt(i.age))
 print(h)
