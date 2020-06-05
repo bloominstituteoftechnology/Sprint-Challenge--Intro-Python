c8acedbad3dce8577ee979a5b452fb06e2c2312b
Wyatt Harrell <wyattaharrell@gmail.com>
2020-06-04 22:29:09 -0400

    Created Motorcycle subclass in oop2.py

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,23 +1,18 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
-#
-# Also change it so the num_wheels defaults to 4 if not specified when the
-# object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return f"vrooom"
 
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        num_wheels = 2
+        super().__init__(num_wheels)
 
-# Subclass Motorcycle from GroundVehicle.
-#
-# Make it so when you instantiate a Motorcycle, it automatically sets the number
-# of wheels to 2 by passing that to the constructor of its superclass.
-#
-# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
-
-# TODO
+    def drive(self):
+        return f"BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
@@ -27,6 +22,5 @@ vehicles = [
     Motorcycle(),
 ]
 
-# Go through the vehicles list and print the result of calling drive() on each.
-
-# TODO
+for item in vehicles:
+    print(item.drive())
