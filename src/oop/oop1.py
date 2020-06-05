95ea6b286201f8e1a6959397c1f6d5c037957cd2
Doina Langille <doina.jitoreanu@gmail.com>
2020-06-05 13:45:52 -0400

    solutions

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    # The base class
+    pass
+
+class FlightVehicle(Vehicle):
+    # Inherits the class Vehicle
+    pass
+
+class Starship(FlightVehicle):
+    # Inherits the class FlightVehicle
+    pass
+
+class Airplane(FlightVehicle):
+    # Inherits the class FlightVehicle
+    pass
+
+class GroundVehicle(Vehicle):
+    # Inherits the class Vehicle
+    pass
+
+class Car(GroundVehicle):
+    # Inherits the class GroundVehicle
+    pass
+
+class Motorcycle(GroundVehicle):
+    # Inherits the class GroundVehicle
+    pass
