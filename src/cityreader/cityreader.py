e054ba870c70e189dcfd43fd29c35787a72a04c9
AustinPotts <pottsevolvements@gmail.com>
2020-06-05 12:17:19 -0400

    Configured CityReader + Ran Test for OK result

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,3 +1,4 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 class City:
@@ -5,6 +6,9 @@ class City:
     self.name = name 
     self.lat = lat
     self.lon = lon
+  def __str__(self):
+     return f"\"{self.name}, {self.lat}, {self.lon}\""
+
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -24,6 +28,15 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+
+  #For some reason I'm getting a "No Such File cities.csv" error
+  with open("cities.csv") as citiesFile:
+    reader = csv.reader(citiesFile, delimiter=',')
+    lines = 0
+    for data in reader:
+      if lines !=0:
+        cities.append(City(data[0], float(data[3]), float(data[4])))
+        lines += 1
     
     return cities
 
