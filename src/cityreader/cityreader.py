02d1b3d6c49c6284a9bbb7da4abda5a683aab47e
Hector <hectoremil21@hotmail.com>
2020-06-05 13:51:50 -0400

    USe csv reader to parse through file

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -21,16 +21,26 @@ class City:
 # should not be loaded into a City object.
 cities = []
 
+import csv
+
 
 def cityreader(cities=[]):
     # TODO Implement the functionality to read from the 'cities.csv' file
     # For each city record, create a new City instance and add it to the
     # `cities` list
-
+    with open('cities.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                cities.append(City(row[0], row[3], row[4]))
     return cities
 
-
 cityreader(cities)
+
+# cityreader()
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
