41d462b43a8f1fe23183644a3a9792deb6386853
Mark Gerrior <github@gerrior.com>
2020-06-05 08:52:06 -0700

    test_between_C_and_G done

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -36,7 +36,7 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [individual.name for individual in humans if ord(individual.name[0]) >= ord('C') and ord(individual.name[0]) <= ord('G')]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
