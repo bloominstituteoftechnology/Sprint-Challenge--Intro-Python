2f01f0e8b05e8a3df2d2a7ccfe76d6283f6ff5ad
Christopher Hernandez <cerberean661@gmail.com>
2020-06-07 20:50:11 -0700

    finished oop

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,8 +4,10 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
+    def drive(self):
+        return "vroooom"
 
     # TODO
 
@@ -19,6 +21,12 @@ class GroundVehicle():
 
 # TODO
 
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(2)
+    def drive(self):
+        return "BRAAAP!!"
+
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -30,3 +38,5 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for x in vehicles:
+    print(x.drive())
