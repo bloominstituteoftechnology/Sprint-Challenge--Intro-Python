6969ffcbf1b80492b5e9a047ae08473afb032f4d
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-06-13 12:40:10 -0500

    Finished oop2.

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,20 +4,31 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
     # TODO
 
+    def drive(self):
+        return "vroooom"
 
 # Subclass Motorcycle from GroundVehicle.
-#
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
+
+    def drive(self):
+        return 'BRAAAP!!'
+
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
+
 
 vehicles = [
     GroundVehicle(),
@@ -30,3 +41,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for vehicle in vehicles:
+    print(vehicle.drive())
