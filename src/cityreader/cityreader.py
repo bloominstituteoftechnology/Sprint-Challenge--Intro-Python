31b65f982f70e050df81500724bd2b2d8386efeb
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-06-13 12:52:05 -0500

    Attempting stretch.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -81,13 +81,26 @@ for c in cities:
 
 # TODO Get latitude and longitude values from the user
 
+user_input = input(
+    "Input coordinates as demonstrated in the example (lat1, lon1, lat2, lon2): ")
+print(user_input.split(','))
+
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
     # within will hold the cities that fall within the specified region
     within = []
 
-    # TODO Ensure that the lat and lon valuse are all floats
+    # TODO Ensure that the lat and lon values are all floats
     # Go through each city and check to see if it falls within
     # the specified coordinates.
+
+    lat1 = float(lat1)
+    lon1 = float(lon1)
+
+    lat2 = float(lat2)
+    lon2 = float(lon2)
+
+    greater = []
+    lesser = []
 
     return within
