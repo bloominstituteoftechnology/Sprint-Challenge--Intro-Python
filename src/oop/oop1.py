39dad114e1dc65060905aa00897f3ea3b73fb041
Harry Merritt <harry@merritt.es>
2020-06-15 00:17:43 +0100

    completed oop1.py: all tests pass

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    def __init__(self):
+        self.name = ""
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        self.name = ""
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        self.name = ""
+
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
