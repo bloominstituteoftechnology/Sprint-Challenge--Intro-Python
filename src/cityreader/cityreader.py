11fdbd183f4a11511e3bf56809313a2eb8f04f59
Michael Lowe <mrlowememphis@yahoo.com>
2020-06-15 17:36:13 -0500

    Opps ! Did not correct cityreader.py to print list of cities!

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -41,7 +41,8 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-     print(c)
+      # print(c)
+      print(f'{c.name}, {c.lat}, {c.lon} \n')
 
 # STRETCH GOAL!
 #
