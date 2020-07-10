e05fd9f8e66ed73b445092b5d9a01f496cfacc15
mtoce <mtoce@villanova.edu>
2020-07-10 13:16:46 -0400

    sprint completed

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,51 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+# Write classes for the following class hierarchy:
+#
+#  [Vehicle]->[FlightVehicle]->[Starship]
+#      |                |
+#      v                v
+# [GroundVehicle]      [Airplane]
+#   |       |
+#   v       v
+# [Car]  [Motorcycle]
+#
+# Each class can simply "pass" for its body. The exercise is about setting up
+# the hierarchy.
+#
+# e.g.
+#
+# class Whatever:
+#     pass
+#
+# Put a comment noting which class is the base class
+
+# base class
+class Vehicle:
+    def __init__(self):
+        pass
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        pass
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        pass
+
+class Car(GroundVehicle):
+    def __init__(self):
+        pass
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        pass
