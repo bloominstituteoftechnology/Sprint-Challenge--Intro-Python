d2f4a83b25f592d19f9953224828b6f305a06b7f
Tim Shaker <tim@tmshkr.com>
2020-07-10 08:11:21 -0700

    complete oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,38 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle
+# base class
+class Vehicle:
+    pass
+
+
+# FlightVehicle
+# inherits from Vehicle
+# parent class to Starship and Airplane
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+# GroundVehicle
+# inherits from Vehicle
+# parent class to Car and Motorcycle
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
