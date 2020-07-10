ba6eb903ee3a7dc4878fcdfd050493ca2f13f150
Trey Lloyd <58454082+HUGHIII@users.noreply.github.com>
2020-07-10 15:22:40 -0500

    mvp

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -6,6 +6,9 @@ class City:
        self.name = name
        self.lat = lat
        self.lon = lon
+    
+    def __str__(self):
+      return f"{self.name}, {self.lat}, {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -27,13 +30,13 @@ def cityreader(cities=[]):
   # `cities` list
   
 
-  with open('src\cityreader\cities.csv', 'r') as csv_file:
+  with open('/Users/Trey/lambda/computerScience/introToPythonOOP/csSprint1real/Sprint-Challenge--Intro-Python/src/cityreader/cities.csv', 'r') as csv_file:
       csv_reader = csv.reader(csv_file)
 
       next(csv_reader)
 
-      for line in csv_reader:
-          print(line)
+      for i in csv_reader:
+          cities.append(City(i[0], float(i[3]), float(i[4])))
     
   return cities
 
