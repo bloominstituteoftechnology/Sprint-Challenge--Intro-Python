6cfb1fc0a951e1e563226064b3e1b176d5444f54
Krishna Dahal <krishna.dahal2017@gmail.com>
2020-07-10 12:58:05 -0400

    cityreader coded but still error in printing out data --- working on it

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,11 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -20,14 +26,18 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+  with open("/Users/krishnadahal/Sprint-Challenge--Intro-Python/src/cityreader/cities.csv", "r+") as myfile:
+    city_access = csv.reader(myfile, delimiter=",")
+    next(city_access) #to skip header - fields
+    for x in city_access:
+      cities.append(City(x[0], x[3], x[4]))
+  return cities
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+  print(c)
 
 # STRETCH GOAL!
 #
