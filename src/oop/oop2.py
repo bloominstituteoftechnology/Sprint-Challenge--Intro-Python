1811f66578f43b6a7de500d4a37f9abb9cf95ea4
Christine Fletcher <chrismis79@gmail.com>
2020-07-10 10:26:56 -0500

    oop2 MVP completed and both oop1 and 2 all tests passing

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,8 +4,11 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
+    
+    def drive(self):
+        return "vroooom"
 
     # TODO
 
@@ -19,6 +22,13 @@ class GroundVehicle():
 
 # TODO
 
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        self.num_wheels = num_wheels
+
+    def drive(self):
+        return "BRAAAP!!"
+
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -30,3 +40,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for vehicle in vehicles:
+    print(vehicle.drive())
