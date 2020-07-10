5d0b39bab8ab0154f5ca89ca450663c4b22832b8
chrishuskey <chrishuskey@users.noreply.github.com>
2020-07-10 12:29:35 -0400

    Add completed comp.py and oop1.py files.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,51 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class: Definition for our base class Vehicle, which represents a generic 
+# vehicle object:
+class Vehicle():
+    def __init__(self):
+        pass
+
+# Definition for class GroundVehicle, which represents a specialized type of Vehicle 
+# object that only operates on the ground:
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# Definition for class Car, which represents a specialized type of GroundVehicle 
+# object that carries passengers on roads (usually) and has 4 wheels:
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# Definition for class Motorcycle, which represents a specialized type of GroundVehicle 
+# object that carries passengers and has 2 wheels:
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# Definition for class FlightVehicle, which represents a specialized type of Vehicle 
+# object that flies;
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# Definition for class Airplane, which represents a specialized type of FlightVehicle 
+# object that carries passengers or cargo:
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# Definition for class Starship, which represents a specialized type of FlightVehicle 
+# object that goes to space! (and/or one built by SpaceX that might go to Mars...):
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        pass
