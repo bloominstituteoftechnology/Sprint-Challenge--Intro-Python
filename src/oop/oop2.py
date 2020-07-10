2e61cd13102aadedc096109469c4d8155e18d253
Krishna Dahal <krishna.dahal2017@gmail.com>
2020-07-10 11:34:34 -0400

    oop2 done - test passed

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -21,7 +21,7 @@ class GroundVehicle():
 # TODO
 class Motorcycle(GroundVehicle):
     def __init__(self, num_wheels = 2):
-        super().__init__()
+        super().__init__(num_wheels)
     
     def drive(self):
         return "BRAAAP!!"
