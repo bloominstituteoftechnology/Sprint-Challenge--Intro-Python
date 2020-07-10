5c5dcb408539ef3d355bef8b10f0d002dbf54ae9
stevedole808 <stevedolelambda@gmail.com>
2020-07-10 07:54:09 -1000

    mvp

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,23 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle: # Base class
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
