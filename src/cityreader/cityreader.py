e95c36be55ee6a42d12b4c36ae2ee127c482aa91
austiezr <austinziech@gmail.com>
2020-07-10 10:12:17 -0600

    finished cityreader stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,7 +1,6 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
-
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -80,12 +79,12 @@ print(len(cities))
 
 # TODO Get latitude and longitude values from the user
 
-def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
-
-  return within
+def cityreader_stretch(lat1, lon1, lat2, lon2, cities_list=[]):
+    # within will hold the cities that fall within the specified region
+    within = []
+    for x in cities_list:
+        if float(min(lat1, lat2)) < float(x.lat) < float(max(lat1, lat2)) and \
+                float(min(lon1, lon2)) < float(x.lon) < float(max(lon1, lon2)):
+            within.append(x)
+    return within
