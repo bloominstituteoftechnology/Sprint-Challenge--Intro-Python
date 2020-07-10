e286e6a00d53516b7017f134b0863dce98dc701c
Shawn James <shawn.james@me.com>
2020-07-10 12:25:52 -0400

    update cityreader

---
+++ src/cityreader/city.py
@@ -0,0 +1,8 @@
+class City:
+    def __init__(self, name, latitude, longitude):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        return f"{self.name}, {self.latitude}, {self.longitude}"    
