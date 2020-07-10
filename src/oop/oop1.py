58b380b5257214b84ee2544c2809784155c6d285
wesley.moody <wgm3190@gmail.com>
2020-07-10 10:30:57 -0500

    vehicle classes in oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,6 +1,35 @@
 # Write classes for the following class hierarchy:
 #
 #  [Vehicle]->[FlightVehicle]->[Starship]
+class Vehicle: 
+  # Vehicle is base class
+  pass
+
+# Inheriting methods from Vehicle
+
+class FlightVehicle(Vehicle):
+  pass
+
+class GroundVehicle(Vehicle):
+  pass
+
+# Inheriting methods from FlightVehicle => Vehicle
+class Starship(FlightVehicle):
+  pass
+
+class Airplane(FlightVehicle):
+  pass
+
+# Inheriting methods from GroundVehicle => Vehicle
+
+class Car(GroundVehicle):
+  pass
+
+class Motorcycle(GroundVehicle):
+  pass
+
+
+  
 #      |                |
 #      v                v
 # [GroundVehicle]      [Airplane]
