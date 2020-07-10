ca18db76c010f8ed27094fe4db1125158931b435
Jing Qian <qianjing2020@users.noreply.github.com>
2020-07-10 11:48:37 -0400

    "init commit"

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,5 +1,4 @@
 # Write classes for the following class hierarchy:
-#
 #  [Vehicle]->[FlightVehicle]->[Starship]
 #      |                |
 #      v                v
@@ -7,13 +6,34 @@
 #   |       |
 #   v       v
 # [Car]  [Motorcycle]
-#
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
-#
-# e.g.
-#
 # class Whatever:
-#     pass
-#
+#     pass#
 # Put a comment noting which class is the base class
+class Vehicle:
+    pass
+
+class FlightVehicle(Vehicle):
+    # base class: Vehicle
+    pass
+
+class Starship(FlightVehicle):
+    # base class: FlightVehicle
+    pass
+
+class Airplane(FlightVehicle):
+    # base class: FlightVehicle
+    pass
+
+class GroundVehicle(Vehicle):
+    # base class: Vehicle
+    pass
+
+class Car(GroundVehicle):
+    # base class: GroundVehicle
+    pass
+
+class Motorcycle(GroundVehicle):
+    # base class: GroundVehicle
+    pass
