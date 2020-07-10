e73b657a62757261a11c87c5d2d6f3519b9facd3
Patrick Millet <lohs79seahawk@gmail.com>
2020-07-10 10:41:47 -0500

    OOP1 Complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,38 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# Parent Class
+class Vehicle:
+    def __init__(self, name, engine):
+        self.name = name
+        self.engine = engine
+
+# Inherits from Vehicle
+class GroundVehicle(Vehicle):
+    def __init__(self, name, engine, num_tires):
+        super.__init__(name, engine)
+        self.num_tires = num_tires
+
+# Inherits from GroundVehicle
+class Car(GroundVehicle):
+    def __init__(self, name, engine, num_tires, num_doors):
+        super.__init__(name, engine, num_tires)
+        self.num_doors = num_doors
+
+# Inherits from Vehicle
+class FlightVehicle(Vehicle):
+    def __init__(self, name, engine, num_engines):
+        super.__init__(name, engine)
+        self.num_engines = num_engines
+
+class Airplane(FlightVehicle):
+    def __init__(self, name, engine, num_engines, num_seats):
+        super.__init__(name, engine, num_engines)
+        self.num_seats = num_seats
+
+class Starship(FlightVehicle):
+    def __init__(self, name, engine, num_engines, num_blasters):
+        super.__init__(name, engine, num_engines)
+        self.num_blasters = num_blasters
