ec2ea47f8a80dbfb5e8dffa270f4b9cba2bf16f5
Poenomaly <djbenson741@gmail.com>
2020-07-10 09:55:27 -0600

    Finished oop/ and comp/

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,56 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle: # Vehicle is the base class for all other classes
+
+    def __init__(self):
+
+        pass
+
+class FlightVehicle(Vehicle): # FlightVehicle is the base class for Airplane and Starship
+
+    def __init__(self):
+
+        super(FlightVehicle, self).__init__()
+
+        pass
+
+class Airplane(FlightVehicle):
+
+    def __init__(self):
+
+        super(Airplane, self).__init__()
+
+        pass
+        
+class Starship(FlightVehicle):
+
+    def __init__(self):
+
+        super(Starship, self).__init__()
+
+        pass
+
+class GroundVehicle(Vehicle): # GroundVehicle is the base class for Car and Motorcycle
+
+    def __init__(self):
+
+        super(GroundVehicle, self).__init__()
+
+        pass
+
+class Car(GroundVehicle):
+
+    def __init__(self):
+
+        super(Car, self).__init__()
+
+        pass
+
+class Motorcycle(GroundVehicle):
+
+    def __init__(self):
+
+        super(Motorcycle, self).__init__()
+
+        pass
