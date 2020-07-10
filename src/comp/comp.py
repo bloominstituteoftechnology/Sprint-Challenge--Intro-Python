386c00eb7729d3cf395a83b574089b314ac486e8
juarezfrench <markedfrench@gmail.com>
2020-07-10 15:48:19 -0500

    Corrections to comp and cityreader

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,7 +24,7 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = [x.name for x in humans if x.name[0].lower == 'd']
+a = [x.name for x in humans if x.name[0] == 'D']
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
