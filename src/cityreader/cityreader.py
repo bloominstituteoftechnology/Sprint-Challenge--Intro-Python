2489c6f2621577062b26b2e0b94fd9a601fdd212
john <jgreenberg@runbox.com>
2020-07-10 15:13:27 -0400

    completed

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -9,6 +9,12 @@ class City:
     self.lat = lat
     self.lon = lon
 
+  #def __str__(self):
+    #return str(self.name + self.lat + self.lon)
+
+  def __repr__(self):
+    return (f'{self.__class__.__name__}('f'{self.name!r}, {self.lat}, {self.lon})')
+
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -31,13 +37,11 @@ def cityreader(cities=[]):
   reader = csv.reader(file)
   next(reader, None)
   for row in reader:
-    cities.append(City(row[0], row[3], row[4]))
+    cities.append(City(str(row[0]), float(row[3]), float(row[4])))
 
   return cities
 
-cities = cityreader(cities)
-for i in cities:
-  print(i)
+print(cityreader(cities))
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -20,7 +20,7 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 class Motorcycle(GroundVehicle): 
-    def __init__(self, num_wheels=2):
+    def __init__(self):
         super().__init__(num_wheels=2)
 
     def drive(self):
