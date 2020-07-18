7db5b9bfc976962be8cf06881637ab908d577564
Dan Anderson <danfanderson@gmail.com>
2020-07-17 21:25:52 -0500

    complete oop1 class hierarchy definition

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,36 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class: Vehicle
+class Vehicle:
+    pass
+
+# Class FlightVehicle is a type of Vehicle (child of Vehicle)
+class FlightVehicle(Vehicle):
+    pass
+
+# Class Airplane is a type of FlightVehicle 
+#   (child of FlightVehicle, grandchild of Vehicle)
+class Airplane(FlightVehicle):
+    pass
+
+# Class Starship is a type of FlightVehicle 
+#   (child of FlightVehicle, grandchild of Vehicle)
+class Starship(FlightVehicle):
+    pass
+
+# GroundVehicle is a type of Vehicle
+#   (child of Vehicle)
+class GroundVehicle(Vehicle):
+    pass
+
+# Car is a type of GroundVehicle
+#   (child of GroundVehicle; grandchild of Vehicle)
+class Car(GroundVehicle):
+    pass
+
+# Motorcycle is a type of GroundVehicle
+#   (child of GroundVehicle; grandchild of Vehicle)
+class Motorcycle(GroundVehicle):
+    pass
