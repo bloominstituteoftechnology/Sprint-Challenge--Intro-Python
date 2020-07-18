e88ad1febc421f637ab05936fb2b221d83f5ad84
Veto Ramirez <vetorobert3@gmail.com>
2020-07-18 10:37:36 -0700

    Working on classes. Getting some errors.

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,14 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, drive, num_wheels):
         self.num_wheels = num_wheels
+        self.drive = "vroooom"
+        self.num_wheels = 4
 
     # TODO
+    def __str__(self):
+        return self.drive
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -18,6 +22,10 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        self.num_wheels = 2
+        self.drive = "BRAAAP!"
 
 vehicles = [
     GroundVehicle(),
@@ -28,5 +36,8 @@ vehicles = [
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
+
+my_vehicle = Vehicle({self.drive})
+print(my_vehicle)
 
 # TODO
