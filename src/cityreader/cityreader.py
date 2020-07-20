458b70cb891f291aac0719f0b3df1a09a048d976
Aaroneld <merrifield48@gmail.com>
2020-07-19 21:47:57 -0400

    MVP

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,17 @@
+import csv
+import os 
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+
+class City():
+
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon= lon 
+
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -14,6 +26,7 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+
 cities = []
 
 def cityreader(cities=[]):
@@ -21,10 +34,14 @@ def cityreader(cities=[]):
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
 
-cityreader(cities)
+    with open('./src/cityreader/cities.csv', newline="") as csvfile:
+      reader = csv.reader(csvfile, delimiter=",", quotechar='"')
+      cities = [City(x[0], float(x[3]), float(x[4])) for x in reader if x[0] != 'city']
+      
+      return cities
+
+cities = cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,13 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
     # TODO
+    def drive(self):
+        return "vroooom"
+
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -16,6 +19,15 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+
+class Motorcycle(GroundVehicle):
+
+    def __init__(self, num_wheels = 2):
+        super().__init__()
+        self.num_wheels = num_wheels
+    
+    def drive(self):
+        return "BRAAAP!!"
 
 # TODO
 
@@ -30,3 +42,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for vehicle in vehicles:
+    print(vehicle.drive())
