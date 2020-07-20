433a154f80e0e652de102bc5cbb8fbe53589e9e8
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-07-19 20:36:38 -0700

    Got oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,28 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# base is itself
+class Vehicle:
+    pass
+
+# base is Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+# base is Vehicle
+class Motorcycle(GroundVehicle):
+    pass
+
+# base is GroundVehicle
+class Car(GroundVehicle):
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
