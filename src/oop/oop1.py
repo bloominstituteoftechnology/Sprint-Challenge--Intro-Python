b4815dedd7dedb62f66a4d710515aa7b8bc6e6da
Neha-Kumari31 <Neha-kumari31@user.noreply.github.com>
2020-07-19 22:50:27 -0400

    Finish first part

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,24 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    '''Base class'''
+    pass
+class GroundVehicle (Vehicle):
+    '''Subclass of Parent class Vehicle'''
+    pass
+class FlightVehicle (Vehicle):
+    '''Subclass of Parent class Vehicle'''
+    pass
+class Starship (FlightVehicle):
+    '''Subclass of Parent class F lightVehicle'''
+    pass
+class Airplane (FlightVehicle):
+    '''Subclass of Parent class FlightVehicle'''
+    pass
+class Car (GroundVehicle):
+    '''Subclass of GroundVehicle'''
+    pass
+class Motorcycle (GroundVehicle):
+    '''subclass of GroundVehicle'''
+    pass
