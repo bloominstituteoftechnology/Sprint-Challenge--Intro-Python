8a485a6e06df8e83efef427b10356565f43d18f8
Cody Morley <codymorleyinc@gmail.com>
2020-09-04 08:52:18 -0700

    City Reader
    Finished w/ Stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,7 +1,11 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
-
+class City:
+	def __init__(self, name, lat, lon):
+		self.name = name
+		self.lat = lat
+		self.lon = lon
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -17,18 +21,22 @@
 cities = []
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # Ensure that the lat and lon valuse are all floats
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
-    return cities
+		# TODO Implement the functionality to read from the 'cities.csv' file
+		# Ensure that the lat and lon valuse are all floats
+		# For each city record, create a new City instance and add it to the 
+		# `cities` list
+	with open("cities.csv", newline="") as csvfile:
+    next(csvfile)
+    for line in csvfile:
+      city_info = line.split(",")
+      cities.append(City(city_info[0], float(city_info[3]), float(city_info[4])))			
+	return cities
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+				print(c)
 
 # STRETCH GOAL!
 #
@@ -60,12 +68,56 @@ for c in cities:
 # Salt Lake City: (40.7774,-111.9301)
 
 # TODO Get latitude and longitude values from the user
+coordinate1 = input("Enter lat and long values for the first co-ordinate separated by commas: ").split(",")
+coordinate2 = input("Enter lat and long values for the second co-ordinate separated by commas: ").split(",")
+
+try:
+  lat_first = coordinate1[0]
+  lon_first = coordinate1[1]
+  lat_second = coordinate2[0]
+  lon_second = coordinate2[1]
+except IndexError:
+  print("Error: Improper data for coordinate.")
+  exit(-1)
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
-  
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+		# within will hold the cities that fall within the specified region
+		within = []
+		
+    lat1 = float(lat1)
+    lon1 = float(lon1)
+    lat2 = float(lat2)
+    lon2 = float(lon2)
 
-  return within
+		# Go through each city and check to see if it falls within 
+		# the specified coordinates.
+    if lon1 < lon2:
+      left_lat = lat1
+      left_lon = lon1
+      right_lat = lat2
+      right_lon = lon2
+    else:
+      left_lat = lat2
+      left_lon = lon2
+      right_lat = lat1
+      right_lon = lon1
+
+    if left_lat < right_lat:
+      for city in cities:
+          city_lat = city.lat
+          city_lon = city.lon
+          if left_lat < city_lat < right_lat and left_lon < city_lon < right_lon:
+            within.append(city)
+    else:
+      for city in cities:
+        city_lat = city.lat
+        city_lon = city.lon
+        if left_lat > city_lat > right_lat and left_lon < city_lon < right_lon:
+          within.append(city)
+
+		return within
+
+included_cities = cityreader_stretch(first_lat, first_lon, second_lat, second_lon, cities)
+
+for city in included_cities:
+  print(city)
