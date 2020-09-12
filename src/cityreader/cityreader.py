1e1d594de48db2310bb8a75cda0892b4efac1d81
OliverAbreu <oliverabreu@unknown40ec99bd3cab.attlocal.net>
2020-09-12 14:54:56 -0400

    feat(cityreader.py): Implements read csv functionality and prints list

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,11 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
-
+class City():
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -21,8 +26,12 @@ def cityreader(cities=[]):
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+	with open("./src/cityreader/cities.csv", newline="") as csvfile:
+		next(csvfile)
+		for line in csvfile:
+			city_info = line.split(",")
+			cities.append(City(city_info[0], float(city_info[3]), float(city_info[4])))			
+	return cities
 
 cityreader(cities)
 
