ef2ba6c562a715ebdb1c4fe7a1fd759239c79478
OliverAbreu <oliverabreu@unknown40ec99bd3cab.attlocal.net>
2020-09-12 14:23:37 -0400

    feat(comp.py): Creates list comprehension 6 of 8

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -55,7 +55,7 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(human.name, human.age) for human in humans if (human.age >= 27 and human.age <= 32)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
