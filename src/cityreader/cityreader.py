dffbf95dbb89e842e459270cd91fb88b673559d1
Brian <bri2210433@yahoo.com>
2020-09-12 11:04:01 -0700

    mvp

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,15 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
+
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"  
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -21,7 +31,13 @@ def cityreader(cities=[]):
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  rows = []
+  with open("./src/cityreader/cities.csv") as csvfile:
+    reader = csv.reader(csvfile, delimiter = ",")
+    for row in reader:
+      rows.append(row)
+    for row in rows[1:]:
+      cities.append(City(row[0], float(row[3]), float(row[4])))
     return cities
 
 cityreader(cities)
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,9 +4,11 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
-
+        
+    def drive(self):
+        return "vroooom"    
     # TODO
 
 
@@ -19,6 +21,13 @@ class GroundVehicle():
 
 # TODO
 
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels)
+
+    def drive(self):
+        return "BRAAAP!!"    
+
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -28,5 +37,8 @@ vehicles = [
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
+
+for item in vehicles:
+    print(item.drive())
 
 # TODO
