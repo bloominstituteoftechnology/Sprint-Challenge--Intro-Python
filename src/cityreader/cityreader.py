f7cb271e061a069c126d40d5c374fbbd8acd9d6b
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-09-11 21:00:24 -0600

    Met MVP and finished stretch.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,11 +1,19 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
+
+
+class City:
+    def __init__(self, name, lat: float, lon: float):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
 
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -16,27 +24,33 @@
 # should not be loaded into a City object.
 cities = []
 
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # Ensure that the lat and lon valuse are all floats
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+   # TODO Implement the functionality to read from the 'cities.csv' file
+    # Ensure that the lat and lon valuse are all floats
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+    with open('cities.csv') as file:
+        file_data = csv.reader(file)
+        manage_data = list(file_data)
+        for row in manage_data[1:]:
+            cities.append(City(row[0], float(row[3]), float(row[4])))
     return cities
+
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print((c.name, c.lat, c.lon))
 
 # STRETCH GOAL!
 #
 # Allow the user to input two points, each specified by latitude and longitude.
-# These points form the corners of a lat/lon square. Pass these latitude and 
+# These points form the corners of a lat/lon square. Pass these latitude and
 # longitude values as parameters to the `cityreader_stretch` function, along
 # with the `cities` list that holds all the City instances from the `cityreader`
-# function. This function should output all the cities that fall within the 
+# function. This function should output all the cities that fall within the
 # coordinate square.
 #
 # Be aware that the user could specify either a lower-left/upper-right pair of
@@ -61,11 +75,52 @@ for c in cities:
 
 # TODO Get latitude and longitude values from the user
 
-def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
-  
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
 
-  return within
+def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
+    upperlat = 0
+    lowerlat = 0
+    upperlon = 0
+    lowerlon = 0
+    # getting lat bounds
+    if abs(lat1) >= abs(lat2):
+        upperlat = abs(lat1)
+        lowerlat = abs(lat2)
+        # print("upperlat", upperlat)
+        # print("lowerlat", lowerlat)
+    else:
+        upperlat = abs(lat2)
+        lowerlat = abs(lat1)
+        # print("upperlat", upperlat)
+        # print("lowerlat", lowerlat)
+
+    # getting lon bounds
+    if abs(lon1) >= abs(lon2):
+        upperlon = abs(lon1)
+        lowerlon = abs(lon2)
+        # print("upperlon", upperlon)
+        # print("lowerlon", lowerlon)
+    else:
+        upperlon = abs(lon2)
+        lowerlon = abs(lon1)
+        # print("upperlon", upperlon)
+        # print("lowerlon", lowerlon)
+
+    # within will hold the cities that fall within the specified region
+    within = []
+    for i in cities:
+        latitude = abs(int(i.lat))
+        longitude = abs(int(i.lon))
+        if latitude in range(lowerlat, upperlat) and longitude in range(lowerlon, upperlon):
+            print(i.name, i.lat, i.lon)
+            within.append(i)
+
+            # Go through each city and check to see if it falls within
+            # the specified coordinates.
+
+    return within
+
+
+answer_list = cityreader_stretch(45, -100, 32, -120, cities)
+# cityreader_stretch(32, -120, 45, -100, cities)
+for i in answer_list:
+    print(i.name, (i.lat, i.lon))
--- src/comp/comp.py
+++ src/comp/comp.py
@@ -1,5 +1,8 @@
-# The following list comprehension exercises will make use of the 
-# defined Human class. 
+# The following list comprehension exercises will make use of the
+# defined Human class.
+import math
+
+
 class Human:
     def __init__(self, name, age):
         self.name = name
@@ -7,6 +10,7 @@ class Human:
 
     def __repr__(self):
         return f"<Human: {self.name}, {self.age}>"
+
 
 humans = [
     Human("Alice", 29),
@@ -24,48 +28,47 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [i.name for i in humans if i.name[0] == "D"]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [i.name for i in humans if i.name[-1] == "e"]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [i.name for i in humans if i.name[0] in ["C", "D", "E", "F", "G"]]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [i.age + 10 for i in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f"{i.name}-{i.age}" for i in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(i.name, i.age) for i in humans if i.age in range(27, 33)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [Human(i.name.upper(), i.age+5) for i in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
-import math
-h = []
+h = [i.age**0.5 for i in humans]
 print(h)
