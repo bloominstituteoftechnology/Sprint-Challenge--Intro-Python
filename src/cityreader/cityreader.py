67d3c19d0cfe5d39ba11c0e5e7458952113f13f0
Anthony <chitowncoder@gmail.com>
2020-09-13 22:29:23 -0500

    MVP + Sprint

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,14 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
-
+class City: 
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = float(lat)
+    self.lon = float(lon)
+    
+  def __str__(self):
+    return f'{self.name} {self.lat} {self.lon}'
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -18,10 +26,14 @@ cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
-  # Ensure that the lat and lon valuse are all floats
+  # Ensure that the lat and lon values are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  with open('src/cityreader/cities.csv') as f:
+    read_obj = csv.reader(f)
+    next(read_obj)
+    for row in read_obj: 
+      cities.append(City(row[0], row[3], row[4]))
     return cities
 
 cityreader(cities)
@@ -60,12 +72,31 @@ for c in cities:
 # Salt Lake City: (40.7774,-111.9301)
 
 # TODO Get latitude and longitude values from the user
+coord1 = input('Enter lat1, lon1\n[Make sure to separate with a comma]: ').strip().split(",")
+coord2 = input('Enter lat2, lon2\n[Make sure to separate with a comma]: ').strip().split(',')
+
+lat1 = float(coord1[0])
+lon1 = float(coord1[1])
+lat2 = float(coord2[0])
+lon2 = float(coord2[1])
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
   within = []
+  minlat = min([lat1, lat2])
+  maxlat = max([lat1, lat2])
+  minlon = min([lon1, lon2])
+  maxlon = max([lon1, lon2])
   
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+  # Go through each city and check to see if it falls within the specified coordinates.
+  for city in cities: 
+    if city.lat >= minlat and city.lat <= maxlat:
+      if city.lon >= minlon and city.lon <= maxlon:
+        within.append(city)
 
   return within
+
+city_area = cityreader_stretch(lat1, lon1, lat2, lon2, cities)
+
+for city in city_area: 
+  print(city)
--- src/comp/comp.py
+++ src/comp/comp.py
@@ -47,14 +47,14 @@ print(d)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = [f'{human.name}--{human.age}' for human in humans]
+e = [f'{human.name}-{human.age}' for human in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = [(human.age, human.age) for human in humans if human.age>= 27 and human.age<= 32]
+f = [(human.name, human.age) for human in humans if human.age>= 27 and human.age<= 32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
