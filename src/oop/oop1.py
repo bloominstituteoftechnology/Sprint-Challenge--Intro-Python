72436a94e3f588f10f81ef57ea73e26f7039ba46
FabiolaSaga <saga.fabiola@gmail.com>
2020-09-13 20:12:34 -0500

    Finished oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,43 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#Base class
+class Vehicle:
+    def __init__(self, year, make):
+        self.year = year
+        self.make = make
+
+#Inherits from Vehicle
+class FlightVehicle(Vehicle):
+    def __init__(self, year, make, wings):
+        super.__init__(year, make)
+        self.wings = wings
+
+#Inherits from FlightVehicle
+class Starship(FlightVehicle):
+    def __init__(self, year, make, wings):
+        super.__init__(year, make, wings)
+
+#Inherits from vehicle
+class GroundVehicle(Vehicle):
+    def __init__(self, year, make, wheels):
+        super.__init__(year, make)  
+        self.wheels = wheels
+
+#Inherits from flight vehicle
+class Airplane(FlightVehicle):
+    def __init__(self, year, make, wings, cabin):
+        super.__init__(year, make, wings)
+        self.cabin = cabin              
+
+#Inherits from GroundVehicle
+class Car(GroundVehicle):
+    def __init__(self, year, make, wheels):
+        super.__init__(year, make, wheels)  
+
+#Inherits from GroundVehicle
+class Motorcycle(GroundVehicle):
+    def __init__(self, year, make, wheels):
+        super.__init__(year, make, wheels)
+
