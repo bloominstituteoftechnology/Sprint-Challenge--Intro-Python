e2d56d3d2fd9b1ea7c30ac9e99a00a2881c24eb2
FabiolaSaga <saga.fabiola@gmail.com>
2020-09-13 20:28:11 -0500

    Updated code to pass tests

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -20,40 +20,47 @@
 
 #Base class
 class Vehicle:
-    def __init__(self, year, make):
-        self.year = year
-        self.make = make
+    # def __init__(self, year, make):
+    #     self.year = year
+    #     self.make = make
+    pass
 
 #Inherits from Vehicle
 class FlightVehicle(Vehicle):
-    def __init__(self, year, make, wings):
-        super().__init__(year, make)
-        self.wings = wings
+    # def __init__(self, year, make, wings):
+    #     super().__init__(year, make)
+    #     self.wings = wings
+    pass
 
 #Inherits from FlightVehicle
 class Starship(FlightVehicle):
-    def __init__(self, year, make, wings):
-        super().__init__(year, make, wings)
+    # def __init__(self, year, make, wings):
+    #     super().__init__(year, make, wings)
+    pass
 
 #Inherits from vehicle
 class GroundVehicle(Vehicle):
-    def __init__(self, year, make, wheels):
-        super().__init__(year, make)  
-        self.wheels = wheels
+    # def __init__(self, year, make, wheels):
+    #     super().__init__(year, make)  
+    #     self.wheels = wheels
+    pass
 
 #Inherits from flight vehicle
 class Airplane(FlightVehicle):
-    def __init__(self, year, make, wings, cabin):
-        super().__init__(year, make, wings)
-        self.cabin = cabin              
+    # def __init__(self, year, make, wings, cabin):
+    #     super().__init__(year, make, wings)
+    #     self.cabin = cabin 
+    pass             
 
 #Inherits from GroundVehicle
 class Car(GroundVehicle):
-    def __init__(self, year, make, wheels):
-        super().__init__(year, make, wheels)  
+    # def __init__(self, year, make, wheels):
+    #     super().__init__(year, make, wheels)
+    pass  
 
 #Inherits from GroundVehicle
 class Motorcycle(GroundVehicle):
-    def __init__(self, year, make, wheels):
-        super().__init__(year, make, wheels)
+    # def __init__(self, year, make, wheels):
+    #     super().__init__(year, make, wheels)
+    pass
 
