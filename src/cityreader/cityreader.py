b8ca7b577780fe33960b4de1c781ba380230e935
Vitaliy Mikhaylyuk <VitaliyMikhaylyuk@gmail.com>
2020-09-13 22:39:57 -0400

    comps

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,9 +1,18 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
+
+import csv
+
 #
 # In the body of the `cityreader` function, use Python's built-in "csv" module 
 # to read this file so that each record is imported into a City instance. Then
--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,36 +24,40 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [i.name for i in humans if i.name[0] == "D"]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [i.name for i in humans if i.name[-1] == "e"]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
+# c = [i.name for i in humans if i.name[0] == "C" or "D" or "E" or "F" or "G"]
+# c = [i.name for i in humans if i.name[0] == filter(i.name[0]), ("C", "D", "E", "F", "G")]
 c = []
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [i.age + 10 for i in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f"{i.name}-{i.age}" for i in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
+# f = [(i.name, i.age) for i in humans if i.age >= 27 and <= 32]
+# f = [(i[0], i[1]) for i in humans]
 f = []
 print(f)
 
@@ -61,11 +65,12 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
+# g = [i.name.upper() for i in humans]
 g = []
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(i.age) for i in humans]
 print(h)
