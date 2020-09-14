3506e6c6c0e619aefc91f47356c3d9e815a550b7
Vitaliy Mikhaylyuk <VitaliyMikhaylyuk@gmail.com>
2020-09-13 21:59:18 -0400

    oop done

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,31 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
+    def drive(self):
+        return "vroooom"
+
+car = GroundVehicle()
+print(car.num_wheels)
+print(car.drive())
+
+
+
     # TODO
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
+        self.num_wheels = num_wheels
+
+    def drive(self):
+        return "BRAAAP!!"
+
+moto = Motorcycle()
+print(moto.num_wheels)
+print(moto.drive())
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -30,3 +51,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for i in vehicles:
+    print(i.drive())
