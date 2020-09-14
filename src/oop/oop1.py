983894b1b827cf535bf1e20064ec17ec6df39d20
FabiolaSaga <saga.fabiola@gmail.com>
2020-09-13 20:24:43 -0500

    Fixed oop1 errors, finished oop2

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -27,33 +27,33 @@ class Vehicle:
 #Inherits from Vehicle
 class FlightVehicle(Vehicle):
     def __init__(self, year, make, wings):
-        super.__init__(year, make)
+        super().__init__(year, make)
         self.wings = wings
 
 #Inherits from FlightVehicle
 class Starship(FlightVehicle):
     def __init__(self, year, make, wings):
-        super.__init__(year, make, wings)
+        super().__init__(year, make, wings)
 
 #Inherits from vehicle
 class GroundVehicle(Vehicle):
     def __init__(self, year, make, wheels):
-        super.__init__(year, make)  
+        super().__init__(year, make)  
         self.wheels = wheels
 
 #Inherits from flight vehicle
 class Airplane(FlightVehicle):
     def __init__(self, year, make, wings, cabin):
-        super.__init__(year, make, wings)
+        super().__init__(year, make, wings)
         self.cabin = cabin              
 
 #Inherits from GroundVehicle
 class Car(GroundVehicle):
     def __init__(self, year, make, wheels):
-        super.__init__(year, make, wheels)  
+        super().__init__(year, make, wheels)  
 
 #Inherits from GroundVehicle
 class Motorcycle(GroundVehicle):
     def __init__(self, year, make, wheels):
-        super.__init__(year, make, wheels)
+        super().__init__(year, make, wheels)
 
